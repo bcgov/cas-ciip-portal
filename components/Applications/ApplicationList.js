b818d0401852fc44cbc1a74b9bf699defb153ad2
Dylan Leard <dylan@button.is>
2019-08-30 14:56:29 -0700

    formatting

--- components/Applications/ApplicationList.js
+++ components/Applications/ApplicationList.js
@@ -1,10 +1,12 @@
 import React ,{ Component } from 'react'
+import ReactDOM from 'react-dom';
 import {graphql, QueryRenderer} from "react-relay";
 import initEnvironment from '../../lib/createRelayEnvironment';
 import ApplicationRowItem from "./ApplicationRowItem";
-import {Container, Dropdown, Button, Row, Col} from 'react-bootstrap';
+import {Container, Dropdown, Button, Row, Col, Form} from 'react-bootstrap';
 const environment = initEnvironment();
 
+// TODO: Dynamic options for filtering (ie: dropdown for status?), filtering with contains etc instead of just equals
 
 class ApplicationList extends Component {
 
@@ -13,7 +15,10 @@ class ApplicationList extends Component {
         this.state = {
             orderByField: "OPERATOR_NAME_",
             direction: "ASC",
-            orderByDisplay: "Operator Name"
+            orderByDisplay: "Operator Name",
+            filterField: "none",
+            filterValue: null,
+            filterDisplay: "No Filter"
         }
     }
 
@@ -42,13 +47,41 @@ class ApplicationList extends Component {
         this.state.direction === 'ASC' ? this.setState({direction: 'DESC'}) : this.setState({direction: 'ASC'})
     }
 
+    applyFilterField = (eventKey, event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        event.persist();
+        this.setState({filterField: eventKey, filterDisplay: event.target.text, filterValue: null});
+    }
+
+    applyFilterValue = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        event.persist();
+        if (this.state.filterField !== "none") {
+            this.setState({filterValue: event.target[0].value});
+        }
+    }
+
     render(){
-        console.log(this.state.orderByField);
-        console.log(this.state.direction);
+        let vars;
+        if (this.state.filterField !== 'none' && this.state.filterValue !== null) {
+           vars = {condition: { [this.state.filterField]: this.state.filterValue }, orderBy: `${this.state.orderByField}${this.state.direction}`};
+        } else {
+           vars = {orderBy: `${this.state.orderByField}${this.state.direction}`};
+        }
         return(
             <React.Fragment>
                 <Container style={{padding: 10, background: 'lightGrey'}}>
-                    <h5>Sort Applications</h5>
+                    <Row>
+                        <Col md={2}>
+                        <h5>Sort Applications</h5>
+                        </Col>
+                        <Col md={2}/>
+                        <Col md={3}>
+                            <h5>Filter Applications</h5>
+                        </Col>
+                    </Row>
                     <Row>
                         <Col md={2}>
                             <Dropdown style={{width: "100%"}}>
@@ -67,16 +100,45 @@ class ApplicationList extends Component {
                         <Col md={1}>
                             <Button style={{width: "100%"}}onClick={this.toggleDirection} variant='info'>{this.state.direction}</Button>
                         </Col>
+                        <Col md={1}/>
+                        <Col md={2}>
+                           <Dropdown style={{width: "100%"}}>
+                                <Dropdown.Toggle style={{width: "100%"}} variant='info' id='dropdown-filter'>
+                                    {this.state.filterDisplay}
+                                </Dropdown.Toggle>
+                                    <Dropdown.Menu style={{width: "100%"}}>
+                                        <Dropdown.Item eventKey='none' onSelect={this.applyFilterField}>No Filter</Dropdown.Item>
+                                        <Dropdown.Item eventKey='applicationId' onSelect={this.applyFilterField}>Application ID</Dropdown.Item>
+                                        <Dropdown.Item eventKey='operatorName' onSelect={this.applyFilterField}>Operator Name</Dropdown.Item>
+                                        <Dropdown.Item eventKey='facilityName' onSelect={this.applyFilterField}>Facility Name</Dropdown.Item>
+                                        <Dropdown.Item eventKey='certificationDate' onSelect={this.applyFilterField}>Submission Date</Dropdown.Item>
+                                        <Dropdown.Item eventKey='applicationStatus' onSelect={this.applyFilterField}>Status</Dropdown.Item>
+                                    </Dropdown.Menu>
+                            </Dropdown>
+                        </Col>
+                        <Col md={6}>
+                        <Form onSubmit={this.applyFilterValue}>
+                          <Form.Row>
+                            <Form.Group as={Col} md={8} controlId="filter">
+                                <Form.Control type="string" step="0.01"
+                                              ref='filter' />
+                            </Form.Group>
+                            <Form.Group as={Col} md={1}>
+                                <Button variant='info' type='submit'>Filter</Button>
+                            </Form.Group>
+                            </Form.Row>
+                        </Form>
+                        </Col>
                     </Row>
                 </Container>
                 <br/>
                 <br/>
                 <QueryRenderer
                     environment={environment}
-                    variables={{orderBy: `${this.state.orderByField}${this.state.direction}`}}
+                    variables={vars}
                     query={graphql`
-                        query ApplicationListQuery($orderBy: [ApplicationsOrderBy!]) {
-                            allApplications(orderBy: $orderBy){
+                        query ApplicationListQuery($condition: ApplicationCondition, $orderBy: [ApplicationsOrderBy!]) {
+                            allApplications(condition: $condition, orderBy: $orderBy){
                                 nodes{
                                   applicationId
                                   facilityName
@@ -93,7 +155,6 @@ class ApplicationList extends Component {
           </React.Fragment>
         )
     }
-
 }
 
 export default ApplicationList;
