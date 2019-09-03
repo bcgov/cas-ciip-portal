efba0d1d4897a741386013e625914f7e8cbfc3ce
Dylan Leard <dylan@button.is>
2019-09-03 13:24:33 -0700

    use Table component to list applications

--- components/Applications/ApplicationRowItem.js
+++ components/Applications/ApplicationRowItem.js
@@ -1,73 +1,53 @@
 import React ,{ Component } from 'react';
 import propTypes from 'prop-types';
-import {Form, Button, Badge, Col, Row, Modal, Container, Dropdown} from 'react-bootstrap';
+import {Form, Button, Badge, Col, Row, Modal, Container, Dropdown, Table} from 'react-bootstrap';
 
 class ApplicationRowItem extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {showDetails: 'none'}
-    }
-
-    toggleDetails = () => {
-        this.state.showDetails === 'none' ? this.setState({showDetails: 'block'}) : this.setState({showDetails: 'none'})
     }
 
     render(){
 
         const application = this.props.application;
-
         const statusBadgeColor = {
             pending: 'warning',
             declined: 'danger',
             approved: 'success'
         }
-        const url = `https://metabase-wksv3k-dev.pathfinder.gov.bc.ca/public/dashboard/bb6a4b75-3a7f-4fab-9268-cb013ecfcb7b?application_id=${application.applicationId}`;
-        const nextUrl = `/application-details?application_id=${application.applicationId}`
+        const applicationDetails = `/application-details?application_id=${application.applicationId}`
+
         return(
             <React.Fragment>
-              <div key={this.props.application.applicationId} >
-                <Container>
-                    <Row className="justify-content-md-center">
-                        <Col md={2} style={{textAlign: 'center'}}>
-                            <h5>Application ID</h5>
-                            <small>{application.applicationId}</small>
-                        </Col>
-                        <Col md={2}>
-                            <h5>Operator Name</h5>
-                            <small>{application.operatorName}</small>
-                        </Col>
-                        <Col md={2}>
-                            <h5>Facility Name</h5>
-                            <small>{application.facilityName}</small>
-                        </Col>
-                        <Col md={2}>
-                            <h5>Submitted</h5>
-                            <small>{application.certificationDate}</small>
-                        </Col>
-                        <Col md={1}>
-                            <h5>Status</h5>
-                            <Badge pill variant={statusBadgeColor[application.applicationStatus]} >{application.applicationStatus}</Badge>
-                        </Col>
-                        <Col md={3} style={{paddingLeft: 100}}>
-                            <Button style={{display: "table-cell"}} href={nextUrl} target="_blank" variant='primary'>{this.state.showDetails === 'none' ? 'View Application' : 'Hide Application'}</Button>
-                        </Col>
-                    </Row>
-                    <Row id={this.props.application.applicationId} className="justify-content-md-center" style={{display: this.state.showDetails}}>
-                        <br/>
-                        <Col md={12}>
-                            <iframe
-                                src={url}
-                                frameBorder="0"
-                                width="100%"
-                                height="1000"
-                                allowtransparency="true">
-                            </iframe>
-                        </Col>
-                    </Row>
-                </Container>
-                <hr/>
-            </div>
+                <div key={this.props.application.applicationId} >
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Application ID</th>
+                                <th>Operator Name</th>
+                                <th>Facility Name</th>
+                                <th>Submitted</th>
+                                <th>Status</th>
+                                <th/>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>{application.applicationId}</td>
+                                <td>{application.operatorName}</td>
+                                <td>{application.facilityName}</td>
+                                <td>{application.certificationDate}</td>
+                                <td>
+                                    <Badge pill style={{width: "100%"}} variant={statusBadgeColor[application.applicationStatus]} >{application.applicationStatus}</Badge>
+                                </td>
+                                <td>
+                                    <Button style={{display: "table-cell"}} href={applicationDetails} target="_blank" variant='primary'>View Application</Button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
           </React.Fragment>
         )
     }
