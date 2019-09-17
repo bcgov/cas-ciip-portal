72964c5ec6fd6ed7f030e6f15279a3505d32ff43
Dylan Leard <dylan@button.is>
2019-09-17 14:01:03 -0700

    removed unused imports

--- app/tests/pages/application-details.test.js
+++ app/tests/pages/application-details.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {shallow, mount, render} from 'enzyme';
+import {mount} from 'enzyme';
 import ApplicationDetails from '../../pages/application-details';
 
 const router = {
