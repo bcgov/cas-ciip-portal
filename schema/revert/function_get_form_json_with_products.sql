dae24076ae99784e5fba1cf8470bc20b103d0e8a
Dylan Leard <dylan@button.is>
2019-09-30 13:05:35 -0700

    removed unnecessary function from sqitch

--- schema/revert/function_get_form_json_with_products.sql
+++
@@ -1,8 +0,0 @@
--- Revert ggircs-portal:function_get_form_json_with_products from pg
-
-BEGIN;
-
-drop function ggircs_portal.get_form_json_with_products;
-drop type form_json_product_type;
-
-COMMIT;
--- schema/verify/function_get_form_json_with_products.sql
+++
@@ -1,7 +0,0 @@
--- Verify ggircs-portal:function_get_form_json_with_products on pg
-
-BEGIN;
-
-select pg_get_functiondef('ggircs_portal.get_form_json_with_products(numeric)'::regprocedure);
-
-ROLLBACK;
