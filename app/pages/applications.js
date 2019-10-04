8007ae4a389bbc8b59d76a28ef5d22e43867e2ee
Alec Wenzowski <alec@button.is>
2019-10-04 16:42:08 -0700

    define handlers directly on class

--- app/pages/applications.js
+++ app/pages/applications.js
@@ -41,30 +41,38 @@ class Applications extends Component {
     }
   `;
 
-  actions = {
-    toggleDirection: () => {
-      this.state.direction === 'ASC'
-        ? this.setState({direction: 'DESC'})
-        : this.setState({direction: 'ASC'});
-    },
-    sortApplications: (event, eventKey) => {
-      this.setState({
-        orderByField: eventKey,
-        orderByDisplay: event.target.text
-      });
-    },
-    applySearchField: (event, eventKey) => {
-      this.setState({
-        searchField: eventKey,
-        searchDisplay: event.target.text,
-        searchValue: null
-      });
-    },
-    applySearchValue: event => {
-      if (this.state.searchField !== 'none') {
-        this.setState({searchValue: event.nativeEvent.target[0].value});
-      }
+  toggleDirection = () => {
+    this.state.direction === 'ASC'
+      ? this.setState({direction: 'DESC'})
+      : this.setState({direction: 'ASC'});
+  };
+
+  sortApplications = (event, eventKey) => {
+    this.setState({
+      orderByField: eventKey,
+      orderByDisplay: event.target.text
+    });
+  };
+
+  applySearchField = (event, eventKey) => {
+    this.setState({
+      searchField: eventKey,
+      searchDisplay: event.target.text,
+      searchValue: null
+    });
+  };
+
+  applySearchValue = event => {
+    if (this.state.searchField !== 'none') {
+      this.setState({searchValue: event.nativeEvent.target[0].value});
     }
+  };
+
+  actions = {
+    toggleDirection: this.toggleDirection,
+    sortApplications: this.sortApplications,
+    applySearchField: this.applySearchField,
+    applySearchValue: this.applySearchValue
   };
 
   handleEvent = (event, eventKey) => {
