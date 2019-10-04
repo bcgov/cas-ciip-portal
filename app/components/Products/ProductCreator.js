4e07b58f9e3bb981f5f8b642aa7e758ba8a369a9
Dylan Leard <dylan@button.is>
2019-10-04 15:30:02 -0700

    convert productCreator to functional component

--- app/components/Products/ProductCreator.js
+++ app/components/Products/ProductCreator.js
@@ -1,23 +1,20 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {graphql, createFragmentContainer, commitMutation} from 'react-relay';
 import {Form, Button, Col} from 'react-bootstrap';
 
-class ProductCreator extends Component {
-  constructor(props) {
-    super(props);
-    this.createProductFromRef = React.createRef();
-    this.createProduct = graphql`
-      mutation ProductCreatorMutation($input: CreateProductInput!) {
-        createProduct(input: $input) {
-          product {
-            rowId
-          }
+const ProductCreator = props => {
+  const createProductFromRef = React.createRef();
+  const createProduct = graphql`
+    mutation ProductCreatorMutation($input: CreateProductInput!) {
+      createProduct(input: $input) {
+        product {
+          rowId
         }
       }
-    `;
-  }
+    }
+  `;
 
-  saveProduct = event => {
+  const saveProduct = event => {
     event.preventDefault();
     event.stopPropagation();
     const date = new Date().toUTCString();
@@ -34,45 +31,39 @@ class ProductCreator extends Component {
       }
     };
 
-    const saveMutation = this.createProduct;
-    const {environment} = this.props.relay;
+    const saveMutation = createProduct;
+    const {environment} = props.relay;
     commitMutation(environment, {
       mutation: saveMutation,
       variables: saveVariables,
       onCompleted: response => {
         console.log(response);
-        this.createProductFromRef.current.reset();
+        createProductFromRef.current.reset();
         window.location.reload();
       },
       onError: err => console.error(err)
     });
   };
 
-  render() {
-    return (
-      <>
-        <div>
-          <Form ref={this.createProductFromRef} onSubmit={this.saveProduct}>
-            <Form.Row>
-              <Form.Group as={Col} md="4" controlId="product_name">
-                <Form.Label>Product Name</Form.Label>
-                <Form.Control required="required" type="text" placeholder="" />
-              </Form.Group>
-              <Form.Group as={Col} md="8" controlId="product_description">
-                <Form.Label>Product Description</Form.Label>
-                <Form.Control
-                  required="required"
-                  type="textbox"
-                  placeholder=""
-                />
-              </Form.Group>
-            </Form.Row>
-            <Button type="submit">Create Product</Button>
-          </Form>
-        </div>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <div>
+        <Form ref={createProductFromRef} onSubmit={saveProduct}>
+          <Form.Row>
+            <Form.Group as={Col} md="4" controlId="product_name">
+              <Form.Label>Product Name</Form.Label>
+              <Form.Control required="required" type="text" placeholder="" />
+            </Form.Group>
+            <Form.Group as={Col} md="8" controlId="product_description">
+              <Form.Label>Product Description</Form.Label>
+              <Form.Control required="required" type="textbox" placeholder="" />
+            </Form.Group>
+          </Form.Row>
+          <Button type="submit">Create Product</Button>
+        </Form>
+      </div>
+    </>
+  );
+};
 
 export default createFragmentContainer(ProductCreator, {});
