5733df0e9b11081d041dd019fa8b1b6c036812f9
Dylan Leard <dylan@button.is>
2019-10-09 17:15:09 -0700

    a little bit of cleanup

--- app/components/Incentives/BenchmarkChart.js
+++ app/components/Incentives/BenchmarkChart.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
 import propTypes from 'prop-types';
-import MathJax from 'react-mathjax2';
 
 class BenchmarkChart extends Component {
   constructor(props) {
     super(props);
     this.canvasRef = React.createRef();
   }
+
+  // Proptype Validations
+  static propTypes = (BenchmarkChart.propTypes = {
+    quantity: propTypes.number,
+    benchmark: propTypes.number,
+    eligibilityThreshold: propTypes.number,
+    carbonTaxPaid: propTypes.number,
+    incentiveSegment: propTypes.number,
+    fuelPercentage: propTypes.number
+  }.isRequired);
 
   componentDidMount() {
     const canvas = this.canvasRef.current;
@@ -47,15 +56,5 @@ class BenchmarkChart extends Component {
     );
   }
 }
-
-// Proptype Validations
-BenchmarkChart.propTypes = {
-  quantity: propTypes.number,
-  benchmark: propTypes.number,
-  eligibilityThreshold: propTypes.number,
-  carbonTaxPaid: propTypes.number,
-  incentiveSegment: propTypes.number,
-  fuelPercentage: propTypes.number
-};
 
 export default BenchmarkChart;
