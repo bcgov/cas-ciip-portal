5b1250d6ec6f7a83600da1c563f96b6d2a1d261e
Alec Wenzowski <alec@button.is>
2019-10-09 17:06:55 -0700

    console log for error boundary

--- app/lib/error-boundary.js
+++ app/lib/error-boundary.js
@@ -8,12 +8,14 @@ class ErrorBoundary extends Component {
 
   static getDerivedStateFromError(error) {
     // Update state so the next render will show the fallback UI.
+    console.error(error);
     return {hasError: true};
   }
 
   componentDidCatch(error, errorInfo) {
     // You can also log the error to an error reporting service
     // logErrorToMyService(error, errorInfo);
+    console.error(error, errorInfo);
   }
 
   render() {
