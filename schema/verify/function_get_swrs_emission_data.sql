91055d0f983967c903134402b1385617bcfa35fc
Hamza Javed <hamza@button.is>
2019-10-11 11:31:02 -0700

    Update schema/verify/function_get_swrs_emission_data.sql
    Co-Authored-By: Matthieu Foucault <matthieu@button.is>

--- schema/verify/function_get_swrs_emission_data.sql
+++ schema/verify/function_get_swrs_emission_data.sql
@@ -1,6 +1,6 @@
 -- Verify ggircs-portal:function_get_swrs_emission_data on pg
 
-BEGIN;
+begin;
 
 select pg_get_functiondef('ggircs_portal.get_swrs_emission_data(integer, varchar)'::regprocedure);
 
