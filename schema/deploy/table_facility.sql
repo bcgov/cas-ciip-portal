bbf6038b74fde3ac211f1902aad4a987ee38a534
Matthieu Foucault <matthieu@button.is>
2019-10-11 14:51:28 -0700

    Use foreign key in facility table instead of organization_facility table

--- schema/deploy/table_facility.sql
+++ schema/deploy/table_facility.sql
@@ -5,6 +5,7 @@ begin;
 
   create table ggircs_portal.facility (
       id integer not null,
+      organisation_id integer not null references ggircs_portal.organisation(id),
       report_id integer ,
       swrs_report_id integer ,
       swrs_facility_id integer ,
@@ -23,8 +24,8 @@ begin;
       facility_postal_code varchar(1000),
       facility_country varchar(1000)
   );
-  
-  
+
+
   create unique index facility_id_index
     on ggircs_portal.facility (id);
 
@@ -35,7 +36,8 @@ begin;
 
   --Todo: refactor to add address table in CIIP
 
-    comment on column ggircs_portal.facility.id is 'unique if for the facility';
+    comment on column ggircs_portal.facility.id is 'unique id for the facility';
+    comment on column ggircs_portal.facility.organisation_id is 'the id of the organization responsible for that facility';
     comment on column ggircs_portal.facility.report_id is 'report id from swrs';
     comment on column ggircs_portal.facility.swrs_report_id is 'swrs report id from swrs';
     comment on column ggircs_portal.facility.swrs_organisation_id is 'swrs organisation id';
@@ -58,6 +60,7 @@ begin;
 
     insert into ggircs_portal.facility (
       id,
+      organisation_id,
       report_id,
       swrs_report_id,
       swrs_facility_id ,
@@ -77,6 +80,7 @@ begin;
       facility_country
     ) values (
       1,
+      1,
       1192,
       15736,
       1766,
@@ -94,30 +98,9 @@ begin;
       'Alberta',
       't2p4j8',
       'Canada'
-    );
-
-
-    insert into ggircs_portal.facility (
-      id,
-      report_id,
-      swrs_report_id,
-      swrs_facility_id ,
-      swrs_organisation_id,
-      reporting_year,
-      facility_name,
-      facility_type ,
-      bcghgid,
-      naics_code,
-      naics_classification,
-      latitude,
-      longitude,
-      facility_mailing_address,
-      facility_city,
-      facility_province,
-      facility_postal_code,
-      facility_country
-    ) values (
+    ), (
       2,
+      1,
       4000,
       15736,
       1767,
@@ -135,29 +118,9 @@ begin;
       'Alberta',
       't2p4j8',
       'Canada'
-    );
-
-      insert into ggircs_portal.facility (
-      id,
-      report_id,
-      swrs_report_id,
-      swrs_facility_id ,
-      swrs_organisation_id,
-      reporting_year,
-      facility_name,
-      facility_type ,
-      bcghgid,
-      naics_code,
-      naics_classification,
-      latitude,
-      longitude,
-      facility_mailing_address,
-      facility_city,
-      facility_province,
-      facility_postal_code,
-      facility_country
-    ) values (
+    ), (
       3,
+      2,
       2218,
       16289,
       2251,
--- schema/verify/table_organisation_facility.sql
+++
@@ -1,7 +0,0 @@
--- Verify ggircs-portal:table_organisation_facility on pg
-
-begin;
-
-select pg_catalog.has_table_privilege('ggircs_portal.organisation_facility', 'select');
-
-rollback;
