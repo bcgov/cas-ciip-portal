2a525675861fe3f8c09c51a3c097b444515dc8da
Hamza Javed <hamza@button.is>
2019-10-11 13:43:11 -0700

    fix user table uuid

--- schema/deploy/table_user.sql
+++ schema/deploy/table_user.sql
@@ -6,7 +6,7 @@ begin;
 create table ggircs_portal.user
 (
   id int not null,
-  uuid uuid
+  uuid uuid,
   first_name varchar(1000),
   last_name varchar(1000),
   email_address varchar(1000),
@@ -42,11 +42,11 @@ comment on column ggircs_portal.user.created_by is 'The person who updated the u
 comment on column ggircs_portal.user.updated_at is 'The date the user was updated';
 comment on column ggircs_portal.user.updated_by is 'The person who updated the user';
 
-insert into ggircs_portal.user (id, first_name, last_name, email_address)
+insert into ggircs_portal.user (id, uuid, first_name, last_name, email_address)
         values (1, '6c01258f-6ad8-4790-8ccc-485163f122a5' , 'Hamza', 'Javed', 'hamza@button.is');
-insert into ggircs_portal.user (id, first_name, last_name, email_address)
+insert into ggircs_portal.user (id, uuid, first_name, last_name, email_address)
         values (2, 'ca716545-a8d3-4034-819c-5e45b0e775c9' , 'Alec', 'Wenzowski', 'alec@button.is');
-insert into ggircs_portal.user (id, first_name, last_name, email_address)
-        values (3, '11bc5fb8-88f6-4b6e-92e3-581622ad4804',. 'Kawhi', 'Leonard', 'kawhi@button.is');
+insert into ggircs_portal.user (id, uuid, first_name, last_name, email_address)
+        values (3, '11bc5fb8-88f6-4b6e-92e3-581622ad4804', 'Kawhi', 'Leonard', 'kawhi@button.is');
 
 commit;
