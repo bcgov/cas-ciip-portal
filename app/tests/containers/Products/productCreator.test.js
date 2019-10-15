3fc93998d5c8896884779b8a7cec5d6d525b5dc8
Alec Wenzowski <alec@button.is>
2019-10-15 15:58:24 -0700

    make a few tests pass again

--- app/tests/containers/Products/productCreator.test.js
+++ app/tests/containers/Products/productCreator.test.js
@@ -3,25 +3,25 @@ import {render} from '@testing-library/react';
 import {ProductCreatorContainer} from '../../../containers/Products/ProductCreatorContainer';
 
 describe('Product Creator', () => {
-  it.skip('should render the product name field', async () => {
+  it('should render the product name field', async () => {
     // This will replace the query in ProductList with the one above and wait till Milk is rendered
     const {getByLabelText} = render(<ProductCreatorContainer />);
     expect(getByLabelText(/Product Name/i)).toBeDefined();
   });
 
-  it.skip('should render the product desc field', async () => {
+  it('should render the product desc field', async () => {
     // This will replace the query in ProductList with the one above and wait till Milk is rendered
     const {getByLabelText} = render(<ProductCreatorContainer />);
     expect(getByLabelText(/Product Description/i)).toBeDefined();
   });
 
-  it.skip('should render the Create Product button', async () => {
+  it('should render the Create Product button', async () => {
     // This will replace the query in ProductList with the one above and wait till Milk is rendered
     const {getByText} = render(<ProductCreatorContainer />);
     expect(getByText(/Create Product/i)).toBeDefined();
   });
 
-  it.skip('should match the last usable snapshot', async () => {
+  it('should match the last usable snapshot', async () => {
     // This will replace the query in ProductList with the one above and wait till Milk is rendered
     const container = render(<ProductCreatorContainer />);
     expect(container).toMatchSnapshot();
