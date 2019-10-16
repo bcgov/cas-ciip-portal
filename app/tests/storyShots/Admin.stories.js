15e498e767ca37ceb7af181847f0dfbe2fae2fac
Alec Wenzowski <alec@button.is>
2019-10-15 17:23:17 -0700

    $ xo --fix

--- app/tests/storyShots/Admin.stories.js
+++ app/tests/storyShots/Admin.stories.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { QueryRenderer } from "react-relay";
-import { storiesOf } from "@storybook/react";
+import React from 'react';
+import {QueryRenderer} from 'react-relay';
+import {storiesOf} from '@storybook/react';
 import {createMockEnvironment} from 'relay-test-utils';
-import Admin from "../../containers/pageContainers/Admin";
+import Admin from '../../containers/pageContainers/Admin';
 
-const environment=createMockEnvironment();
+const environment = createMockEnvironment();
 
 const query = graphql`
-query AdminTestQuery {
-  query {
-    ...ProductListContainer_query
+  query AdminTestQuery {
+    query {
+      ...ProductListContainer_query
+    }
   }
-}
 `;
 
 const renderStory = (query, environment, variables = {}) => (
@@ -19,31 +19,54 @@ const renderStory = (query, environment, variables = {}) => (
     environment={environment}
     query={query}
     variables={variables}
-    render={({ props, error }) => {
+    render={({props, error}) => {
       if (error) {
         console.error(error);
       } else if (props) {
-        console.log(props)
-        return <Admin {...props} query={query}/>;
+        console.log(props);
+        return <Admin {...props} query={query} />;
       }
-      console.log(props)
-      return <Admin {...props} query={{active:{
-      edges:
-        [{node: {
-          id: 1, name:'buddy', state: 'active', benchmarksByProductId: {edges: []}
-        }}]}
-      , archived: {
-        edges:
-          [{node: {
-            id: 2, name:'holly', state: 'archived', benchmarksByProductId: {edges: []}
-          }}]}}}/>;
+
+      console.log(props);
+      return (
+        <Admin
+          {...props}
+          query={{
+            active: {
+              edges: [
+                {
+                  node: {
+                    id: 1,
+                    name: 'buddy',
+                    state: 'active',
+                    benchmarksByProductId: {edges: []}
+                  }
+                }
+              ]
+            },
+            archived: {
+              edges: [
+                {
+                  node: {
+                    id: 2,
+                    name: 'holly',
+                    state: 'archived',
+                    benchmarksByProductId: {edges: []}
+                  }
+                }
+              ]
+            }
+          }}
+        />
+      );
       return null;
     }}
   />
 );
- 
-storiesOf("containers/pageContainers/Admin", module)
-  .add("Render Admin page", () => {
+
+storiesOf('containers/pageContainers/Admin', module).add(
+  'Render Admin page',
+  () => {
     return renderStory(query, environment);
-  });
-  
+  }
+);
