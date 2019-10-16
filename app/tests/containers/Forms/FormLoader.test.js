0726487df3428296c399a77943085780fe5c15e8
Alec Wenzowski <alec@button.is>
2019-10-16 09:50:57 -0700

    formatting

--- app/tests/containers/Forms/FormLoader.test.js
+++ app/tests/containers/Forms/FormLoader.test.js
@@ -1,11 +1,10 @@
+import {create} from 'domain';
 import React from 'react';
 import {wait, render} from '@testing-library/react';
 import {shallow, mount} from 'enzyme';
 import * as Survey from 'survey-react';
+import {createMockEnvironment} from 'relay-test-utils';
 import {FormLoaderContainer} from '../../../containers/Forms/FormLoaderContainer';
-import {createMockEnvironment} from 'relay-test-utils'
-import { create } from 'domain';
-
 
 describe('Form Loader', () => {
   let survey;
@@ -20,21 +19,33 @@ describe('Form Loader', () => {
     Survey.Survey.cssType = 'bootstrap';
     query = {
       json: {
-        edges: [{node: {id: 'form-1', rowId: 1, name: 'testForm', formJson: '{"elements":[{"name":"customerName","type":"text","title":"What is your name?","isRequired":true}]}'}}]
+        edges: [
+          {
+            node: {
+              id: 'form-1',
+              rowId: 1,
+              name: 'testForm',
+              formJson:
+                '{"elements":[{"name":"customerName","type":"text","title":"What is your name?","isRequired":true}]}'
+            }
+          }
+        ]
       }
     };
   });
 
   it('should render the form', async () => {
-    console.log(query)
-    const r = shallow(<FormLoaderContainer query={query} relay={{environment}}/>);
+    console.log(query);
+    const r = shallow(
+      <FormLoaderContainer query={query} relay={{environment}} />
+    );
     r.update();
     expect(r).toMatchSnapshot();
   });
 
-    it.skip('should render the complete button', async () => {
+  it.skip('should render the complete button', async () => {
     const r = shallow(<FormLoaderContainer formJson={survey} />);
-    // await wait(() => r.getAllByText(/Complete/i));
+    // Await wait(() => r.getAllByText(/Complete/i));
     expect(r.getAllByText(/Complete/i)).toBeDefined();
   });
 });
