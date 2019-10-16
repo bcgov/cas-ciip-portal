f2039ebf4935ccb2432f65871b381b3098ec6f1d
Dylan Leard <dylan@button.is>
2019-10-16 08:58:22 -0700

    wip Formloader test fix

--- app/tests/containers/Forms/FormLoader.test.js
+++ app/tests/containers/Forms/FormLoader.test.js
@@ -1,27 +1,40 @@
 import React from 'react';
 import {wait, render} from '@testing-library/react';
+import {shallow, mount} from 'enzyme';
 import * as Survey from 'survey-react';
 import {FormLoaderContainer} from '../../../containers/Forms/FormLoaderContainer';
+import {createMockEnvironment} from 'relay-test-utils'
+import { create } from 'domain';
+
 
 describe('Form Loader', () => {
   let survey;
+  let query;
+  let environment;
   beforeEach(() => {
+    environment = createMockEnvironment();
     const model = new Survey.Model(
       '{"elements":[{"name":"customerName","type":"text","title":"What is your name?","isRequired":true}]}'
     );
     survey = <Survey.Survey model={model} onComplete={jest.fn()} />;
     Survey.Survey.cssType = 'bootstrap';
+    query = {
+      json: {
+        edges: [{node: {id: 'form-1', rowId: 1, name: 'testForm', formJson: '{"elements":[{"name":"customerName","type":"text","title":"What is your name?","isRequired":true}]}'}}]
+      }
+    };
   });
 
-  it.skip('should render the form', async () => {
-    const r = render(<FormLoaderContainer formJson={survey} />);
-    await wait(() => r.getAllByText(/What/i));
+  it('should render the form', async () => {
+    console.log(query)
+    const r = shallow(<FormLoaderContainer query={query} relay={{environment}}/>);
+    r.update();
     expect(r).toMatchSnapshot();
   });
 
-  it.skip('should render the complete button', async () => {
-    const r = render(<FormLoaderContainer formJson={survey} />);
-    await wait(() => r.getAllByText(/Complete/i));
+    it.skip('should render the complete button', async () => {
+    const r = shallow(<FormLoaderContainer formJson={survey} />);
+    // await wait(() => r.getAllByText(/Complete/i));
     expect(r.getAllByText(/Complete/i)).toBeDefined();
   });
 });
