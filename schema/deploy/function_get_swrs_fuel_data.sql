1dad1101d6366f7bc9d9d21ae2711752ca767665
Matthieu Foucault <matthieu@button.is>
2019-10-16 12:14:46 -0700

    Add fuel_table and normalize fuel names imported from SWRS

--- schema/deploy/function_get_swrs_fuel_data.sql
+++ schema/deploy/function_get_swrs_fuel_data.sql
@@ -2,56 +2,58 @@
 
 begin;
 
-  create type ggircs_portal.fuel_data as (
-    report_id integer,
-    fuel_type varchar(1000),
-    fuel_classification varchar(1000),
-    fuel_description varchar(1000),
-    fuel_units varchar(1000),
-    annual_fuel_amount numeric,
-    annual_weighted_avg_hhv numeric,
-    annual_weighted_avg_carbon_content numeric,
-    alternative_methodolody_description varchar(10000)
+create type ggircs_portal.fuel_data as (
+  report_id integer,
+  fuel_type varchar(1000),
+  fuel_classification varchar(1000),
+  fuel_description varchar(1000),
+  fuel_units varchar(1000),
+  annual_fuel_amount numeric,
+  annual_weighted_avg_hhv numeric,
+  annual_weighted_avg_carbon_content numeric,
+  alternative_methodolody_description varchar(10000)
   );
 
 
-  create or replace function ggircs_portal.get_swrs_fuel_data(
-    facility_id integer ,
-    reporting_year varchar(1000)
-  )
+create or replace function ggircs_portal.get_swrs_fuel_data(facility_id integer,
+                                                            reporting_year varchar(1000))
   returns setof ggircs_portal.fuel_data
-  as
-  $body$
-    begin
-      if exists (
-        select 1
-        from   information_schema.tables
-        where  table_schema = 'swrs'
-        and    table_name = 'fuel'
+as
+$body$
+begin
+  if exists(
+    select 1
+    from information_schema.tables
+    where table_schema = 'swrs'
+      and table_name = 'fuel'
+    )
+  then
+    return query (
+      with selected_report as (
+        select *
+        from swrs.report
+        where swrs_facility_id = facility_id
+          and reporting_period_duration = reporting_year
       )
-      then
-      return query (
-         with selected_report as (
-            select * from swrs.report where swrs_facility_id = facility_id
-            and reporting_period_duration = reporting_year
-          )
-          select
-            report_id,
-            fuel_type,
-            fuel_classification,
-            fuel_description,
-            fuel_units ,
-            annual_fuel_amount,
-            annual_weighted_avg_hhv,
-            annual_weighted_avg_carbon_content,
-            alternative_methodology_description
-          from swrs.fuel as _fuel
-          inner join selected_report as _rep on _fuel.report_id = _rep.id
-          where _fuel.fuel_type is not null
-      );
-      end if;
-    end;
-  $body$
+      select report_id,
+             regexp_replace(_details.normalized_fuel_type, '\s+\([^\)]+\)$', '')::varchar(1000) as fuel_type,
+             fuel_classification,
+             fuel_description,
+             fuel_units,
+             annual_fuel_amount,
+             annual_weighted_avg_hhv,
+             annual_weighted_avg_carbon_content,
+             alternative_methodology_description
+      from swrs.fuel as _fuel
+             inner join selected_report as _rep on _fuel.report_id = _rep.id
+             inner join swrs.fuel_mapping _fuel_mapping on _fuel.fuel_mapping_id = _fuel_mapping.id
+             inner join swrs.fuel_carbon_tax_details _details on _fuel_mapping.fuel_carbon_tax_details_id = _details.id
+
+      where _fuel.fuel_type is not null
+    );
+  end if;
+end;
+$body$
   language 'plpgsql' stable;
 
 commit;
