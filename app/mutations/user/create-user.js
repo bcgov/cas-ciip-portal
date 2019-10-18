bd1dab7bc0466dee4e104d1b96d1eba4973f00a8
dimak1 <dima1086@hotmail.com>
2019-10-18 16:39:18 -0700

    Fix lint issues

--- app/mutations/user/create-user.js
+++ app/mutations/user/create-user.js
@@ -1,30 +1,33 @@
-import { commitMutation, graphql } from 'react-relay';
+import {commitMutation, graphql} from 'react-relay';
 
 const mutation = graphql`
-    mutation createUserMutation($input: CreateUserInput!) {
-        createUser(input: $input) {
-            user {
-                firstName
-                lastName
-                emailAddress
-            }
-        }
+  mutation createUserMutation($input: CreateUserInput!) {
+    createUser(input: $input) {
+      user {
+        firstName
+        lastName
+        emailAddress
+      }
     }
+  }
 `;
 
-export default function createUser(environment, variables, onCompletedCallback, onErrorCallback) {
-
-    commitMutation(environment, {
-        mutation,
-        variables,
-        onCompleted: async (response, errors) => {
-            console.log(response);
-            onCompletedCallback;
-        },
-        onError: err => {
-            console.error(err);
-            onErrorCallback;
-        }
-    });
-    
+export default function createUser(
+  environment,
+  variables,
+  onCompletedCallback,
+  onErrorCallback
+) {
+  commitMutation(environment, {
+    mutation,
+    variables,
+    onCompleted: async response => {
+      console.log(response);
+      onCompletedCallback();
+    },
+    onError: async err => {
+      console.error(err);
+      onErrorCallback();
+    }
+  });
 }
