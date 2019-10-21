c4ff8d5bf9224ffd91d9819d76ebe881adb6a655
dimak1 <dima1086@hotmail.com>
2019-10-21 11:07:14 -0700

    Use default layout for Registration page

--- app/containers/pageContainers/Registration.js
+++ app/containers/pageContainers/Registration.js
@@ -1,25 +1,14 @@
 import React, {Component} from 'react';
-import {Container, Row, Col} from 'react-bootstrap';
-import Header from '../../components/Header';
 import FormCreateUser from '../Forms/FormCreateUser';
+import DefaultLayout from '../../layouts/default-layout';
 
 class Registration extends Component {
   render() {
     return (
       <>
-        <Header />
-        <Container className="w-50">
-          <Row>
-            <Col>
-              <h2 className="mb-5">Registration</h2>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <FormCreateUser />
-            </Col>
-          </Row>
-        </Container>
+        <DefaultLayout title="Registration">
+          <FormCreateUser />
+        </DefaultLayout>
       </>
     );
   }
