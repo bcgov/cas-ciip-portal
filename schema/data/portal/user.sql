dd8ee44226262b55fba27cef05daf6baa7c83684
Matthieu Foucault <matthieu@button.is>
2019-10-25 16:13:03 -0700

    add occupation and phone_number to user test data

--- schema/data/portal/user.sql
+++ schema/data/portal/user.sql
@@ -1,14 +1,16 @@
 begin;
 
-insert into ggircs_portal.user (id, uuid, first_name, last_name, email_address)
+insert into ggircs_portal.user (id, uuid, first_name, last_name, email_address, occupation, phone_number)
 overriding system value
 values
-  (1, '6c01258f-6ad8-4790-8ccc-485163f122a5' , 'Douglas', 'Fir', 'Douglas.Fir@hhry.xxx'),
-  (2, 'ca716545-a8d3-4034-819c-5e45b0e775c9' , 'Argus', 'Filch', 'argus@squibs.hp')
+  (1, '6c01258f-6ad8-4790-8ccc-485163f122a5' , 'Douglas', 'Fir', 'Douglas.Fir@hhry.xxx', 'Tree', '123456789'),
+  (2, 'ca716545-a8d3-4034-819c-5e45b0e775c9' , 'Argus', 'Filch', 'argus@squibs.hp', 'Custodian', '123456789')
 on conflict(id) do update set
   uuid=excluded.uuid,
   first_name=excluded.first_name,
   last_name=excluded.last_name,
-  email_address=excluded.email_address;
+  email_address=excluded.email_address,
+  occupation=excluded.occupation,
+  phone_number=excluded.phone_number;
 
 commit;
