69d4967459c0a994f988abb68cf29138b0b5313c
Hamza Javed <hamza@button.is>
2019-10-25 10:23:17 -0700

    fix to user-profile ci test

--- app/pages/user-profile.js
+++ app/pages/user-profile.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Button, Jumbotron} from 'react-bootstrap';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from '../components/Layout/Header';
+import Footer from '../components/Layout/Footer';
 
 class UserProfile extends Component {
   render() {
