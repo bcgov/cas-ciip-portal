66428a36f87bd018ffbc0f572689ecf60efe3add
Dylan Leard <dylan@button.is>
2019-11-05 15:39:40 -0800

    I don't know why I removed this export. It broke the tests. I put it back

--- app/containers/Forms/Form.tsx
+++ app/containers/Forms/Form.tsx
@@ -5,7 +5,7 @@ import SurveyWrapper from '../../components/Survey/SurveyWrapper';
 import FormWithProductUnits from './FormWithProductUnits';
 import FormWithFuelUnits from './FormWithFuelUnits';
 
-const FormComponent = ({
+export const FormComponent = ({
   query,
   initialData,
   initialDataSource,
