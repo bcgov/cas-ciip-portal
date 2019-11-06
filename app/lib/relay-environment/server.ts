6b8818ac52e1a919b7e8f3444b101c9ae9464d79
Dylan Leard <dylan@button.is>
2019-11-06 13:26:54 -0800

    reimplement relay network modern

--- app/lib/relay-environment/server.ts
+++ app/lib/relay-environment/server.ts
@@ -2,23 +2,7 @@ import {
   RelayNetworkLayer,
   urlMiddleware
 } from 'react-relay-network-modern/node8';
-import {Network, Environment, RecordSource, Store} from 'relay-runtime';
-
-async function fetchQuery(operation, variables) {
-  const response = await fetch('/graphql', {
-    method: 'POST',
-    headers: {
-      // Add authentication and other headers here
-      'content-type': 'application/json'
-    },
-    body: JSON.stringify({
-      query: operation.text, // GraphQL text from input
-      variables
-    })
-  });
-
-  return response.json();
-}
+import {Environment, RecordSource, Store} from 'relay-runtime';
 
 export default {
   initEnvironment: () => {
@@ -33,7 +17,9 @@ export default {
           urlMiddleware({
             // TODOx: set $RELAY_ENDPOINT
             // url: req => process.env.RELAY_ENDPOINT
-            url: _ => 'http://localhost:3004/graphql'
+            url: _ =>
+              'http://localhost:3004/graphql' ||
+              'http://localhost:3004/graphql/batch'
           })
         ])
       })
@@ -44,7 +30,16 @@ export default {
     const store = new Store(source);
     return new Environment({
       store,
-      network: Network.create(fetchQuery)
+      // @ts-ignore
+      network: new RelayNetworkLayer([
+        urlMiddleware({
+          // TODOx: set $RELAY_ENDPOINT
+          // url: req => process.env.RELAY_ENDPOINT
+          url: _ =>
+            'http://localhost:3004/graphql' ||
+            'http://localhost:3004/graphql/batch'
+        })
+      ])
     });
   }
 };
