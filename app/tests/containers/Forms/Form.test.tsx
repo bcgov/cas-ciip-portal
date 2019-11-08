54cd11b2de26ce85c6753e83c89e95ac57327225
Dylan Leard <dylan@button.is>
2019-11-08 12:51:32 -0800

    added a test for production form

--- app/tests/containers/Forms/Form.test.tsx
+++ app/tests/containers/Forms/Form.test.tsx
@@ -4,15 +4,22 @@ import {FormComponent} from '../../../containers/Forms/Form';
 import adminForm from '../../../../schema/data/portal/form_json/administration.json';
 import fuelForm from '../../../../schema/data/portal/form_json/fuel.json';
 import electricityAndHeatForm from '../../../../schema/data/portal/form_json/electricity_and_heat.json';
+import productionForm from '../../../../schema/data/portal/form_json/production.json';
 
 describe('Form', () => {
   it.skip('should match the snapshot with the administration form', async () => {
     // TODO: radio button input name is a random number. Figure out why that is.
+    /** (Dylan): In node_modules/react-jsonSchema-form/lib/components/widgets/RadioWidget there is a 'var name = Math.random().toString();'
+        The comment above this line says: // Generating a unique field name to identify this set of radio buttons.
+    */
     const r = render(
       <FormComponent
         query={{
           ' $refType': 'Form_query',
-          ' $fragmentRefs': {FuelFields_query: true},
+          ' $fragmentRefs': {
+            FuelFields_query: true,
+            ProductionFields_query: true
+          },
           result: {
             formResult: {},
             formJsonByFormId: {
@@ -31,7 +38,10 @@ describe('Form', () => {
       <FormComponent
         query={{
           ' $refType': 'Form_query',
-          ' $fragmentRefs': {FuelFields_query: true},
+          ' $fragmentRefs': {
+            FuelFields_query: true,
+            ProductionFields_query: true
+          },
           result: {
             formResult: [],
             formJsonByFormId: {
@@ -50,11 +60,35 @@ describe('Form', () => {
       <FormComponent
         query={{
           ' $refType': 'Form_query',
-          ' $fragmentRefs': {FuelFields_query: true},
+          ' $fragmentRefs': {
+            FuelFields_query: true,
+            ProductionFields_query: true
+          },
           result: {
             formResult: {},
             formJsonByFormId: {
               formJson: electricityAndHeatForm
+            }
+          }
+        }}
+      />
+    );
+    expect(r).toMatchSnapshot();
+  });
+
+  it('should match the snapshot with the production form', async () => {
+    const r = render(
+      <FormComponent
+        query={{
+          ' $refType': 'Form_query',
+          ' $fragmentRefs': {
+            FuelFields_query: true,
+            ProductionFields_query: true
+          },
+          result: {
+            formResult: {},
+            formJsonByFormId: {
+              formJson: productionForm
             }
           }
         }}
