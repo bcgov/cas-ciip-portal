c300168ae4190b46a237af22bb036acdb2d395e4
Dylan Leard <dylan@button.is>
2019-11-12 11:08:34 -0800

    passing materialized_view_test and schema_test

--- schema/test/style/materialized_view_test.sql
+++ schema/test/style/materialized_view_test.sql
@@ -7,9 +7,8 @@ reset client_min_messages;
 
 begin;
 
--- TODO: set search_path to change dynamically for each schema, I don't think this will work once
---       schemas other than swrs_transform become populated with tables
-set search_path to swrs_transform,public;
+-- TODO: set search_path to change dynamically for each schema
+set search_path to ggircs_portal,public;
 
 select * from no_plan();
 
@@ -17,7 +16,7 @@ select * from no_plan();
 
 -- GUIDELINE: All tables should have descriptions
 -- Check all tables for an existing description (regex '.+')
-with mvnames as (select matviewname from pg_matviews where schemaname like 'swrs%')
+with mvnames as (select matviewname from pg_matviews where schemaname like 'ggircs%')
 select matches(
                obj_description(mv::regclass, 'pg_class'),
                '.+',
@@ -28,7 +27,7 @@ from mvnames f(mv);
 -- --GUIDELINE GROUP: Enforce table naming conventions
 -- -- GUIDELINE: Names are lower-case with underscores_as_word_separators
 -- -- Check that all materialized view names do not return a match of capital letters or non-word characters
-with mvnames as (select matviewname from pg_matviews where schemaname like 'swrs%')
+with mvnames as (select matviewname from pg_matviews where schemaname like 'ggircs%')
 select doesnt_match(
                mv,
                '[A-Z]|\W',
@@ -49,7 +48,7 @@ create table csv_import_fixture
 \copy csv_import_fixture from './test/fixture/sql_reserved_words.csv' delimiter ',' csv;
 -- -- test that schema does not contain any table names that intersect with reserved words csv dictionary
 with reserved_words as (select csv_column_fixture from csv_import_fixture),
-mv_names as (select matviewname from pg_matviews where schemaname like 'swrs%')
+mv_names as (select matviewname from pg_matviews where schemaname like 'ggircs%')
 select hasnt_materialized_view(
                mv,
                res,
@@ -61,7 +60,7 @@ drop table csv_import_fixture;
 --
 -- GUIDELINE: All materialized views must have a primary key
 -- Get all materialized views in schema that do not have an index matching %primary%
-prepare null_pkey as select tablename from pg_indexes where not exists (select indexname from pg_indexes where indexname like '%primary%') and schemaname = 'swrs_transform';
+prepare null_pkey as select tablename from pg_indexes where not exists (select indexname from pg_indexes where indexname like '%primary%') and schemaname = 'ggircs';
 -- Test that the above query returns nothing, else throw an error
 select is_empty('null_pkey', 'All materialized views must have a primary key');
 
--- schema/test/style/schema_test.sql
+++ schema/test/style/schema_test.sql
@@ -4,18 +4,18 @@ reset client_min_messages;
 
 begin;
 
-select plan(6);
+select plan(2);
 
 /** Check schema compliance **/
 
--- Check schemas like swrs%  exist
-with schema_names as (select schema_name from information_schema.schemata where schema_name like 'swrs%')
+-- Check schemas like ggircs_portal%  exist
+with schema_names as (select schema_name from information_schema.schemata where schema_name like 'ggircs_portal%')
 select has_schema(sch)
 from schema_names f(sch);
 
 -- GUIDELINE: Schema has a description
 -- Check schema for an existing description (regex '.+')
-with schema_names as (select schema_name from information_schema.schemata where schema_name like 'swrs%')
+with schema_names as (select schema_name from information_schema.schemata where schema_name like 'ggircs_portal%')
 select matches(
                obj_description(sch::regnamespace, 'pg_namespace'),
                '.+',
