d3c5f1689cf0150cce071c522e09d61037913124
Matthieu Foucault <matthieu@button.is>
2019-11-12 10:49:56 -0800

    feature: load fuel data from swrs schema in mutation chain

--- schema/deploy/function_init_application_fuel_form_result.sql
+++ schema/deploy/function_init_application_fuel_form_result.sql
@@ -7,19 +7,33 @@ returns jsonb
 as $function$
 declare
   swrs_facility_id int;
-  fuel_data ggircs_portal.fuel_data[];
+  fuel_datum ggircs_portal.fuel_data;
   result text;
+  first_fuel boolean;
 
 begin
   select facility.swrs_facility_id from ggircs_portal.facility where id = facility_id into swrs_facility_id;
-  select * from ggircs_portal.get_swrs_fuel_data(swrs_facility_id, reporting_year) into fuel_data;
 
-  if fuel_data is null then
-    return '[]';
-  end if;
+  result := '[';
 
-  return concat(
-    '[]')::jsonb;
+  first_fuel := true;
+  for fuel_datum in (select * from ggircs_portal.get_swrs_fuel_data(swrs_facility_id, reporting_year))
+  loop
+    if first_fuel = false then
+      result := concat(result, ',');
+    end if;
+    first_fuel := false;
+    result := concat(result,
+      '{',
+      '"fuelType": "', fuel_datum.fuel_type, '",',
+      '"quantity": ', fuel_datum.annual_fuel_amount, ',',
+      '"fuelUnits": "', fuel_datum.fuel_units, '",',
+      '"methodology": "', fuel_datum.alternative_methodolody_description, '"',
+      '}'
+      );
+  end loop;
+  result := concat(result, ']');
+  return result;
 
 end;
 $function$ language plpgsql strict volatile;
