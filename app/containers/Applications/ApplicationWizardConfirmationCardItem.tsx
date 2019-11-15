d2990e95f32c77f4d1b2ef5b9586a73a98f72fdc
Dylan Leard <dylan@button.is>
2019-11-14 17:10:29 -0800

    disable submit buttons if in summary

--- app/containers/Applications/ApplicationWizardConfirmationCardItem.tsx
+++ app/containers/Applications/ApplicationWizardConfirmationCardItem.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
+import {Button} from 'react-bootstrap';
 import {createFragmentContainer, graphql} from 'react-relay';
 import JsonSchemaForm from 'react-jsonschema-form';
 import {FormJson} from 'next-env';
 import FormArrayFieldTemplate from '../Forms/FormArrayFieldTemplate';
 import FormFieldTemplate from '../Forms/FormFieldTemplate';
-import FuelFields from '../Forms/FuelFields';
 import EmissionGasFields from '../Forms/EmissionGasFields';
 import EmissionSourceFields from '../Forms/EmissionSourceFields';
-import ProductionFields from '../Forms/ProductionFields';
 import FormObjectFieldTemplate from '../Forms/FormObjectFieldTemplate';
 
 interface Props {
@@ -32,38 +31,38 @@ export const ApplicationWizardConfirmationCardItemComponent: React.FunctionCompo
   } = formJson as FormJson;
   console.log(formResult);
   const CUSTOM_FIELDS = {
-    fuel: props => <FuelFields query={props.formContext.query} {...props} />,
+    // Fuel: props => <FuelFields query={props.formContext.query} {...props} />,
     emissionSource: props => <EmissionSourceFields {...props} />,
-    emissionGas: props => <EmissionGasFields {...props} />,
-    production: props => (
-      <ProductionFields query={props.formContext.query} {...props} />
-    )
+    emissionGas: props => <EmissionGasFields {...props} />
+    // Production: props => (
+    // <ProductionFields query={props.formContext.query} {...props} />
+    // )
   };
 
   return (
     <>
       {/*
-        //@ts-ignore */}
-      {formResult.formJsonByFormId.name}
-      {/*
       //@ts-ignore JsonSchemaForm typedef is missing customFormats prop */}
       <JsonSchemaForm
         disabled
         omitExtraData
+        readonly
         liveOmit
-        showErrorList={false}
         ArrayFieldTemplate={FormArrayFieldTemplate}
         FieldTemplate={FormFieldTemplate}
         FormContext={{query}}
-        formData={query}
+        showErrorList={false}
         fields={CUSTOM_FIELDS}
         customFormats={customFormats}
         schema={schema}
         uiSchema={uiSchema}
         ObjectFieldTemplate={FormObjectFieldTemplate}
-        // TransformErrors={transformErrors}
-        // onSubmit={onComplete}
-      />
+        formData={query}
+      >
+        <div style={{display: 'none'}}>
+          <Button type="submit">Submit</Button>
+        </div>
+      </JsonSchemaForm>
     </>
   );
 };
