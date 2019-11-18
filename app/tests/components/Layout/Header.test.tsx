83ee44fae59029a98d1799ecb5797bcaad69d8b3
Matthieu Foucault <matthieu@button.is>
2019-11-18 13:48:24 -0800

    Header unit test should use shallow

--- app/tests/components/Layout/Header.test.tsx
+++ app/tests/components/Layout/Header.test.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import {render} from 'enzyme';
+import {shallow} from 'enzyme';
 import HeaderLayout from '../../../components/Layout/Header';
 
-// It renders the Header
-
 it('It matches the last accepted Snapshot', () => {
-  const wrapper = render(<HeaderLayout />);
+  const wrapper = shallow(<HeaderLayout />);
   expect(wrapper).toMatchSnapshot();
 });
