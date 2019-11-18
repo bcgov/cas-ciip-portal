5a4262316cc0b897c94c6a648c29636cfdd4a16d
Dylan Leard <dylan@button.is>
2019-11-18 13:47:00 -0800

    fixed production view and updated create_application_mutation_chain deploy file to conditionally create an empty object or an array containing an empty object

--- schema/deploy/view_ciip_production.sql
+++ schema/deploy/view_ciip_production.sql
@@ -7,7 +7,7 @@ begin;
       select * from
       (select
          form_result.application_id as id,
-         json_array_elements((form_result -> 'moduleThroughputAnd_productionData')::json) as production_data
+         json_array_elements((form_result)::json) as production_data
       from ggircs_portal.form_result
       join ggircs_portal.form_json
       on form_result.form_id = form_json.id
@@ -15,7 +15,7 @@ begin;
       inner join
       (select
          form_result.application_id as fid,
-         json_array_elements((form_result -> 'facilityInformation')::json) as facility_data
+         (form_result -> 'facility')::json as facility_data
       from ggircs_portal.form_result
       join ggircs_portal.form_json
       on form_result.form_id = form_json.id
@@ -26,8 +26,8 @@ begin;
        x.id,
        (x.facility_data ->> 'bcghgid')::numeric as bcghgid,
        (x.production_data ->> 'quantity')::numeric as quantity,
-       (x.production_data ->> 'processingUnit')::varchar(1000) as product,
-       (x.production_data ->> 'units')::varchar(1000) as fuel_units,
+       (x.production_data ->> 'product')::varchar(1000) as product,
+       (x.production_data ->> 'productUnits')::varchar(1000) as fuel_units,
        (x.production_data ->> 'comments')::varchar(10000) as comments,
        (x.production_data ->> 'associatedEmissions')::numeric as associated_emissions
     from x
@@ -45,6 +45,5 @@ comment on view ggircs_portal.ciip_production is E'@primaryKey id';
 -- comment on column ggircs_portal.ciip_production.fuel_units is 'The emission/fuel units for the production unit';
 -- comment on column ggircs_portal.ciip_production.comments is 'The comments';
 -- comment on column ggircs_portal.ciip_production.associated_emissions is 'The methodology details if other';
--- comment on column ggircs_portal.ciip_production.attributable_fuel_percentage is 'The fuel percentage that can be attributed to incentive';
 
 commit;
