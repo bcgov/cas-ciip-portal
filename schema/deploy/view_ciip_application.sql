4113361d7ff8a3b6113d4f93a5d03d45ee60b235
Dylan Leard <dylan@button.is>
2019-11-18 14:45:06 -0800

    fix application view

--- schema/deploy/view_ciip_application.sql
+++ schema/deploy/view_ciip_application.sql
@@ -31,7 +31,7 @@ create view ggircs_portal.ciip_application as (
     select
        x.id,
        (x.facility_data ->> 'facilityName')::varchar(1000) as facility_name,
-       (y.operator_data ->> 'operatorName')::varchar(1000) as operator_name,
+       (y.operator_data ->> 'name')::varchar(1000) as operator_name,
        x.submission_date as submission_date,
        (x.facility_data ->> 'bcghgid')::numeric as bcghgid,
        x.application_status as application_status,
