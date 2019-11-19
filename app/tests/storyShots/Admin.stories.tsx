ab36fbdfadefa6df98dc9d9c397c03b74ca02080
Matthieu Foucault <matthieu@button.is>
2019-11-19 15:49:53 -0800

    feature: add babel-plugin-module-resolver.  No ".." needed in imports.
    Relative imports can start from the root of the repository or from the `app` folder

--- app/tests/storyShots/Admin.stories.tsx
+++ app/tests/storyShots/Admin.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {QueryRenderer, graphql} from 'react-relay';
 import {storiesOf} from '@storybook/react';
 import {createMockEnvironment} from 'relay-test-utils';
-import Admin from '../../pages/admin';
+import Admin from 'pages/admin';
 
 const environment = createMockEnvironment();
 
