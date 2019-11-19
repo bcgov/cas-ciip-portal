144a136c9ac9e34f712e5f761cbad7aa0cfdf1a7
Helen <helen@button.is>
2019-11-19 14:01:39 -0800

    fixed syntax

--- schema/deploy/table_application.sql
+++ schema/deploy/table_application.sql
@@ -11,7 +11,7 @@ begin;
 create table ggircs_portal.application (
     id integer primary key generated always as identity,
     facility_id integer not null references ggircs_portal.facility(id),
-    foreign key reporting_year references ggircs_portal.reporting_year(reporting_year)
+    foreign key (reporting_year) references ggircs_portal.reporting_year(reporting_year)
 );
 
 comment on table ggircs_portal.application is 'The application data';
