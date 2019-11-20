72454c31f297bcaecc16cedd59b38ddad82ee3d6
dimak1 <dima1086@hotmail.com>
2019-11-20 12:45:42 -0800

    Add View Submitted Application page

---
+++ app/pages/view-application.tsx
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import {Button} from 'react-bootstrap';
+import {graphql} from 'react-relay';
+import Link from 'next/link';
+import {viewApplicationQueryResponse} from 'viewApplicationQuery.graphql';
+import DefaultLayout from '../layouts/default-layout';
+import ApplicationWizardConfirmationCardItem from '../containers/Applications/ApplicationWizardConfirmationCardItem';
+
+/*
+ * ViewApplication renders a summary of the data submitted in the application.
+ */
+
+interface Props {
+  query: viewApplicationQueryResponse['query'];
+}
+
+class ViewApplication extends Component<Props> {
+  static query = graphql`
+    query viewApplicationQuery($applicationId: ID!) {
+      query {
+        session {
+          ...defaultLayout_session
+        }
+        application(id: $applicationId) {
+          id
+          formResultsByApplicationId {
+            edges {
+              node {
+                id
+                ...ApplicationWizardConfirmationCardItem_formResult
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  render() {
+    const {session, application} = this.props.query;
+
+    return (
+      <DefaultLayout session={session} title="Summary of your application">
+        {application.formResultsByApplicationId.edges.map(({node}) => (
+          <ApplicationWizardConfirmationCardItem
+            key={node.id}
+            formResult={node}
+          />
+        ))}
+        <Link
+          href={{
+            pathname: '/user-dashboard'
+          }}
+        >
+          <Button variant="primary">Back to My Dashboard</Button>
+        </Link>
+      </DefaultLayout>
+    );
+  }
+}
+
+export default ViewApplication;
