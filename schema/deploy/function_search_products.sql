c9591b886f4a5e77a402b2d25dc6c1548318eeb9
Dylan Leard <dylan@button.is>
2019-11-22 12:46:04 -0800

    fixed logic of products search function

--- schema/deploy/function_search_products.sql
+++ schema/deploy/function_search_products.sql
@@ -9,13 +9,19 @@ returns setof ggircs_portal.product as
         $function$
             begin
                      if search_field is null or search_value is null
-                        then return query execute 'with tempTable as (select p.* from ggircs_portal.product as p join ggircs_portal.benchmark as b on p.id = b.product_id
-                        and p.state != ''deprecated'' and (CURRENT_TIMESTAMP between start_date and end_date or (CURRENT_TIMESTAMP > start_date and end_date is null)) order by ' || order_by_field || ' ' || direction ||  ')
+                        then return query execute 'with outerTable as (select p.*, benchmark, eligibility_threshold, start_date, end_date from ggircs_portal.product as p left join ggircs_portal.benchmark as b on p.id = b.product_id
+                        and p.state != ''deprecated''),
+                        tempTable as (select id, name, description, units, state, parent, created_at, created_by, updated_at, updated_by, deleted_at, deleted_by from outerTable
+                        where (CURRENT_TIMESTAMP between start_date and end_date or (CURRENT_TIMESTAMP > start_date and end_date is null) or benchmark is null)
+                        order by ' || order_by_field || ' ' || direction ||  ')
                         select * from tempTable';
                     else
                     return query execute
-                        'with tempTable as (select p.* from ggircs_portal.product as p join ggircs_portal.benchmark as b on p.id = b.product_id
-                        and '|| search_field || '::text ilike ''%' || search_value || '%'' and state != ''deprecated'' and (CURRENT_TIMESTAMP between start_date and end_date or (CURRENT_TIMESTAMP > start_date and end_date is null))
+                    'with outerTable as (select p.*, benchmark, eligibility_threshold, start_date, end_date from ggircs_portal.product as p left join ggircs_portal.benchmark as b on p.id = b.product_id
+                        and p.state != ''deprecated''),
+                        tempTable as (select id, name, description, units, state, parent, created_at, created_by, updated_at, updated_by, deleted_at, deleted_by from outerTable
+                        where '|| search_field || '::text ilike ''%' || search_value || '%'' and state != ''deprecated''
+                        and (  CURRENT_TIMESTAMP between start_date and end_date or (CURRENT_TIMESTAMP > start_date and end_date is null) or benchmark is null)
                         order by '|| order_by_field || ' ' || direction || ')
                         select * from tempTable';
                 end if;
