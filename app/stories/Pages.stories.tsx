323c659e0982500ca86219f97c9595f42309f74c
Alec Wenzowski <alec@button.is>
2019-12-06 12:20:42 -0800

    Feature(storybook): accessibility assessment of pages

---
+++ app/stories/Pages.stories.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {QueryRenderer} from 'react-relay';
+import {createMockEnvironment, MockPayloadGenerator} from 'relay-test-utils';
+import {withA11y} from '@storybook/addon-a11y';
+import './mockNextRouter';
+
+const render = Component => {
+  const {query} = Component;
+  const environment = createMockEnvironment();
+  environment.mock.queueOperationResolver(operation => {
+    const payload = MockPayloadGenerator.generate(operation);
+    // Console.log('mock', operation, payload);
+    return payload;
+  });
+  return (
+    <QueryRenderer
+      environment={environment}
+      query={query}
+      render={({props, error}) => {
+        if (error) {
+          console.error(error);
+          return 'Error';
+        }
+
+        // Console.log(Component.name, 'props', props);
+        return <Component {...props} router={{query: ''}} />;
+      }}
+    />
+  );
+};
+
+import Admin from 'pages/admin';
+// Import ApplicationReview from 'pages/application-review';
+// import Applications from 'pages/applications';
+import CiipApplicationSWRSImport from 'pages/ciip-application-swrs-import';
+// Import CiipApplication from 'pages/ciip-application';
+// import CompleteSubmit from 'pages/complete-submit';
+// import Index from 'pages/index';
+// import LoginRedirect from 'pages/login-redirect';
+// import OrganisationRequests from 'pages/organisation-requests';
+// import ProductsBenchmarks from 'pages/products-benchmarks';
+import Registration from 'pages/registration';
+import UserDashboard from 'pages/user-dashboard';
+import UserList from 'pages/user-list';
+// Import UserOrganisationFacilities from 'pages/user-organisation-facilities';
+import UserProfile from 'pages/user-profile';
+// Import ViewApplication from 'pages/view-application';
+
+export default {
+  title: 'Pages',
+  decorators: [withA11y]
+};
+
+export const admin = () => render(Admin);
+export const ciip_application_swrs_import = () =>
+  render(CiipApplicationSWRSImport);
+export const registration = () => render(Registration);
+export const user_dashboard = () => render(UserDashboard);
+export const user_list = () => render(UserList);
+export const user_profile = () => render(UserProfile);
