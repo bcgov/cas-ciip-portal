5a817a8a767b7be205772833b71d57a047518be9
Matthieu Foucault <matthieu@button.is>
2019-12-09 14:44:57 -0800

    feature: add SearchDropdownWidget to use SearchDropdown in forms

---
+++ app/components/Forms/SearchDropdownWidget.tsx
@@ -0,0 +1,47 @@
+import React, {useCallback} from 'react';
+import {WidgetProps} from 'react-jsonschema-form';
+import SearchDropdown from 'components/SearchDropdown';
+
+const SearchDropdownWidget: React.FunctionComponent<WidgetProps> = ({
+  onChange,
+  schema,
+  id,
+  placeholder,
+  value
+}) => {
+  const getOptions = useCallback(
+    () =>
+      schema.enum.map((e: string, index) => ({
+        id: e,
+        name: (schema as any).enumNames?.[index] ?? e
+      })),
+    [schema]
+  );
+
+  const getSelected = useCallback(() => {
+    if (value === null || value === undefined) return undefined;
+    const index = schema.enum.indexOf(value);
+    return [
+      {
+        id: value,
+        name: (schema as any).enumNames?.[index] ?? value
+      }
+    ];
+  }, [schema, value]);
+  const handleChange = (items: Array<{id: string | number}>) => {
+    onChange(items[0]?.id);
+  };
+
+  return (
+    <SearchDropdown
+      id={id}
+      options={getOptions()}
+      inputProps={{id}}
+      placeholder={placeholder}
+      selected={getSelected()}
+      onChange={handleChange}
+    />
+  );
+};
+
+export default SearchDropdownWidget;
