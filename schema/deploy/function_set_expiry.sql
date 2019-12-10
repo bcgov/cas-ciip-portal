1984b90b4af2ccd45ec1b60cf06df6b0d19c74b6
Dylan Leard <dylan@button.is>
2019-12-10 11:41:20 -0800

    create a function/trigger that sets an expires_at column

---
+++ schema/deploy/function_set_expiry.sql
@@ -0,0 +1,32 @@
+-- Deploy ggircs-portal:function_set_expiry to pg
+-- requires: schema_ggircs_portal
+
+begin;
+
+create function ggircs_portal.set_expiry()
+  returns trigger as $$
+declare
+  interval_value interval;
+begin
+  interval_value := TG_ARGV[0];
+  new.expires_at := now() + interval_value;
+  return new;
+end;
+$$ language plpgsql;
+
+comment on function ggircs_portal.set_expiry()
+  is $$
+  a trigger to set expires_at column.
+  example usage:
+
+  create table some_schema.some_table (
+    ...
+    expires_at timestamp with time zone not null
+  );
+  create trigger _set_expiry
+    before insert on some_schema.some_table
+    for each row
+    execute procedure ggircs_portal.set_expiry('7 days');
+  $$;
+
+commit;
--- schema/deploy/table_certification_url.sql
+++ schema/deploy/table_certification_url.sql
@@ -11,7 +11,9 @@ create table ggircs_portal.certification_url (
   updated_at timestamp with time zone not null default now(),
   updated_by varchar(1000),
   deleted_at timestamp with time zone,
-  deleted_by int references ggircs_portal.ciip_user
+  deleted_by int references ggircs_portal.ciip_user,
+  -- TODO(Dylan): revisit expiry / deprecation of generated URLs in the context of Authorization
+  expires_at timestamp with time zone not null
 );
 
 create trigger _random_id
@@ -24,6 +26,12 @@ create trigger _100_timestamps
   for each row
   execute procedure ggircs_portal.update_timestamps();
 
+-- Sets expires_at column for 7 days from creation
+create trigger _set_expiry
+  before insert on ggircs_portal.certification_url
+  for each row
+  execute procedure ggircs_portal.set_expiry('7 days');
+
 comment on table ggircs_portal.certification_url is 'Table containing the certification_url for an application';
 comment on column ggircs_portal.certification_url.id is 'Unique ID for the certification_url';
 comment on column ggircs_portal.certification_url.application_id is 'Foreign key to the application';
@@ -33,5 +41,6 @@ comment on column ggircs_portal.certification_url.updated_at is 'Last update dat
 comment on column ggircs_portal.certification_url.updated_by is 'The user who last updated the row';
 comment on column ggircs_portal.certification_url.deleted_at is 'Deletion date of row';
 comment on column ggircs_portal.certification_url.deleted_by is 'The user who deleted the row';
+comment on column ggircs_portal.certification_url.expires_at is 'The expiry date for the row';
 
 commit;
