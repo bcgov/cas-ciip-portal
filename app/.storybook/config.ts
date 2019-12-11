b96575ee76358c52a1aeb73ca0cac93a0edab207
Alec Wenzowski <alec@button.is>
2019-12-10 16:29:02 -0800

    Fix(style): linter teh fings

--- app/.storybook/config.ts
+++ app/.storybook/config.ts
@@ -1,17 +1,20 @@
-import { configure, addDecorator, addParameters } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
-import { withCssResources } from '@storybook/addon-cssresources';
+import {configure, addDecorator, addParameters} from '@storybook/react';
+import {withInfo} from '@storybook/addon-info';
+import {withCssResources} from '@storybook/addon-cssresources';
 import requireContext from 'require-context.macro';
 
 addDecorator(withInfo);
 addDecorator(withCssResources);
 addParameters({
-  cssresources: [{
-    id: 'bootstrap',
-    code: `<link rel="stylesheet" type="text/css" href="/static/bootstrap.min.css"></link>`,
-    picked: true,
-  }]
-})
+  cssresources: [
+    {
+      id: 'bootstrap',
+      code:
+        '<link rel="stylesheet" type="text/css" href="/static/bootstrap.min.css"></link>',
+      picked: true
+    }
+  ]
+});
 
-// automatically import all files ending in *.stories.js
+// Automatically import all files ending in *.stories.js
 configure(requireContext('../stories', true, /\.stories\.tsx?$/), module);
--- app/.storybook/webpack.config.js
+++ app/.storybook/webpack.config.js
@@ -1,14 +1,14 @@
-module.exports = ({ config, mode }) => {
+module.exports = ({config}) => {
   config.module.rules.push({
     test: /\.tsx?$/,
     use: [
       {
         loader: require.resolve('babel-loader'),
         options: {
-          presets: [require.resolve("babel-preset-react-app")],
-        },
+          presets: [require.resolve('babel-preset-react-app')]
+        }
       },
-      require.resolve("react-docgen-typescript-loader")
+      require.resolve('react-docgen-typescript-loader')
     ]
   });
   config.resolve.extensions.push('.ts', '.tsx');
