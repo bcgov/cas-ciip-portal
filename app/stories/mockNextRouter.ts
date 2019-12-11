b4b33b9ed201d15617ab52fb00afe61d7a87fa6e
Alec Wenzowski <alec@button.is>
2019-12-10 16:58:40 -0800

    Fix(style): linter acts differently on CI; just write decent types

--- app/stories/mockNextRouter.ts
+++ app/stories/mockNextRouter.ts
@@ -4,23 +4,22 @@ import {action} from '@storybook/addon-actions';
 
 declare type Url = string | UrlObject;
 
-const router: RouterType = {
-  ...Router.router,
-  replace: async (url: Url, as?: Url, options?: {}): Promise<boolean> => {
-    action('Router#replace')(url, as, options);
-    return true;
-  },
-  push: async (url: Url, as?: Url, options?: {}): Promise<boolean> => {
-    action('Router#push')(url, as, options);
-    return true;
-  },
-  prefetch: async (url: Url, as?: Url, options?: {}): Promise<void> => {
-    action('Router#prefetch')(url, as, options);
-  }
+const router: Partial<RouterType> = Router.router ? {...Router.router} : {};
+
+router.replace = async (url: Url, as?: Url, options?: {}): Promise<boolean> => {
+  action('Router#replace')(url, as, options);
+  return true;
 };
 
-// Monkeypatch the router instance
-Router._router = Router.router;
-Router.router = router;
+router.push = async (url: Url, as?: Url, options?: {}): Promise<boolean> => {
+  action('Router#push')(url, as, options);
+  return true;
+};
+
+router.prefetch = async (url: Url, as?: Url, options?: {}): Promise<void> => {
+  action('Router#prefetch')(url, as, options);
+};
+
+Router.router = router as RouterType;
 
 export {Router};
