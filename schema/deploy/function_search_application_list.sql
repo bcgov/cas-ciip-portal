87ed8844751160d25e903cff0968b6a1436b1793
Dylan Leard <dylan@button.is>
2019-12-13 14:25:21 -0800

    more search application list function refactor

--- schema/deploy/function_search_application_list.sql
+++ schema/deploy/function_search_application_list.sql
@@ -16,7 +16,8 @@ create or replace function ggircs_portal.search_application_list(search_field te
                                                 and ar.version_number = status.version_number
                                                 and status.application_revision_status != ''draft''
                                               join ggircs_portal.facility f on a.facility_id = f.id
-                                              join ggircs_portal.organisation o on f.organisation_id = o.id)
+                                              join ggircs_portal.organisation o on f.organisation_id = o.id),
+                                              y as (select max(status_id) as max_id from x group by application_id)
                                               select id,
                                                     application_id,
                                                     operator_name,
@@ -25,7 +26,7 @@ create or replace function ggircs_portal.search_application_list(search_field te
                                                     bcghgid,
                                                     submission_date,
                                                     application_revision_status
-                                                    from x where x.status_id = (select max(status_id) from x)
+                                                    from x inner join y on x.status_id = y.max_id
                                                     order by ' || order_by_field || ' ' || direction;
                 else
                     return query execute 'with x as (select row_number() over () as id, a.id as application_id, o.operator_name, f.facility_name, a.reporting_year, f.bcghgid, status.id as status_id, status.created_at as submission_date, status.application_revision_status
@@ -35,7 +36,8 @@ create or replace function ggircs_portal.search_application_list(search_field te
                                                 and ar.version_number = status.version_number
                                                 and status.application_revision_status != ''draft''
                                               join ggircs_portal.facility f on a.facility_id = f.id
-                                              join ggircs_portal.organisation o on f.organisation_id = o.id)
+                                              join ggircs_portal.organisation o on f.organisation_id = o.id),
+                                              y as (select max(status_id) as max_id from x group by application_id)
                                               select id,
                                                     application_id,
                                                     operator_name,
@@ -44,7 +46,7 @@ create or replace function ggircs_portal.search_application_list(search_field te
                                                     bcghgid,
                                                     submission_date,
                                                     application_revision_status
-                                                    from x where x.status_id = (select max(status_id) from x)
+                                                    from x inner join y on x.status_id = y.max_id
                                                     and '|| search_field || '::text ilike ''%' || search_value || '%'' order by '|| order_by_field || ' ' || direction;
                 end if;
             end
