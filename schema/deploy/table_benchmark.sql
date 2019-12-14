b05b39230f998fe0095ad04deebb0f4e99263e38
Matthieu Foucault <matthieu@button.is>
2019-12-13 17:29:43 -0800

    feature: Add incentive_multiplier to benchmark table.
    Added one benchmark per product in test data

--- schema/deploy/table_benchmark.sql
+++ schema/deploy/table_benchmark.sql
@@ -6,8 +6,9 @@ begin;
 create table ggircs_portal.benchmark (
   id integer primary key generated always as identity,
   product_id int not null references ggircs_portal.product(id),
-  benchmark int not null,
-  eligibility_threshold int not null,
+  benchmark numeric not null,
+  eligibility_threshold numeric not null,
+  incentive_multiplier numeric not null default 1,
   start_date timestamp with time zone,
   end_date timestamp with time zone,
   created_at timestamp with time zone not null default now(),
@@ -26,10 +27,11 @@ create trigger _100_timestamps
 comment on table ggircs_portal.benchmark is 'Table containing the benchmark and eligibility threshold for a product';
 comment on column ggircs_portal.benchmark.id is 'Unique ID for the benchmark';
 comment on column ggircs_portal.benchmark.product_id is 'Foreign key to the product';
-comment on column ggircs_portal.benchmark.benchmark is 'The value of the benchmark';
-comment on column ggircs_portal.benchmark.eligibility_threshold is 'The value of eligibility_threshold';
-comment on column ggircs_portal.benchmark.start_date is 'The date when this benchmark became/becomes active';
-comment on column ggircs_portal.benchmark.end_date is 'The date when this benchmark became/becomes inactive';
+comment on column ggircs_portal.benchmark.benchmark is 'The benchmark for a product, i.e. the emission intensity that would return a 100% incentive';
+comment on column ggircs_portal.benchmark.eligibility_threshold is 'The eligibility threshold for a product, i.e. the maximum emission intensity (exclusive) allowed to get an incentive';
+comment on column ggircs_portal.benchmark.incentive_multiplier is 'The multiplier applied to the incentive amounts under this product';
+comment on column ggircs_portal.benchmark.start_date is 'The date when this benchmark becomes active';
+comment on column ggircs_portal.benchmark.end_date is 'The date when this benchmark becomes inactive';
 comment on column ggircs_portal.benchmark.created_at is 'Creation date of row';
 comment on column ggircs_portal.benchmark.created_by is 'Creator of row';
 comment on column ggircs_portal.benchmark.updated_at is 'Last update date of row';
