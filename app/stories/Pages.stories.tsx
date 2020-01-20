bb8b8a3d652f442878cfba3ac1f71dd8cecd9928
Junmin Ahn <junmin@button.is>
2020-01-19 21:52:41 -0800

    fix lint error

--- app/stories/Pages.stories.tsx
+++ app/stories/Pages.stories.tsx
@@ -20,9 +20,8 @@ import UserList from 'pages/admin/user-list';
 // Import UserOrganisationFacilities from 'pages/user-organisation-facilities';
 import UserProfile from 'pages/user-profile';
 // Import ViewApplication from 'pages/reporter/view-application';
-
+import {getAllGroupNames} from '../lib/user-groups';
 import {Router} from './mockNextRouter';
-import {getAllGroupNames} from '../lib/user-groups'
 
 const render = Component => {
   const {query} = Component;
