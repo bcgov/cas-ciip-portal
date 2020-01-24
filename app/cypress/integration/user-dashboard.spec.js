dd0cb5df2a4098b608ab96ef94ef45a9995ac20a
dimak1 <dima1086@hotmail.com>
2020-01-23 16:43:44 -0800

    Add cypress test for User Dashboard

---
+++ app/cypress/integration/user-dashboard.spec.js
@@ -0,0 +1,20 @@
+describe('User dashboard', () => {
+  it('contains organisation dearch dropdown', () => {
+    cy.visit('/reporter/user-dashboard');
+    cy.get('.search-dropdown.dropdown').contains('Request access');
+    cy.get('.search-toggle.dropdown-toggle').click();
+
+    cy.get('.org-scroll').should('be.not.empty');
+
+    cy.get('.org-scroll')
+      .find('a.dropdown-item')
+      .its('length')
+      .should('be.gt', 1);
+  });
+
+  it('matches the snapshot', () => {
+    cy.visit('/reporter/user-dashboard');
+    cy.get('#page-content');
+    cy.percySnapshot();
+  });
+});
