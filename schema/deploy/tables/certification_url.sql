d48234a26fadc1664da463e2d02b29c2e6e65dc1
Dylan Leard <dylan@button.is>
2020-02-07 14:02:27 -0800

    add RLS for certification_url table + pgtap tests

--- schema/deploy/tables/certification_url.sql
+++ schema/deploy/tables/certification_url.sql
@@ -33,7 +33,63 @@ create trigger _set_expiry
   for each row
   execute procedure ggircs_portal.set_expiry('7 days');
 
-grant all on table ggircs_portal.certification_url to ciip_administrator, ciip_analyst, ciip_industry_user;
+do
+$grant$
+begin
+-- Grant ciip_administrator permissions
+perform ggircs_portal_private.grant_permissions('select', 'certification_url', 'ciip_administrator');
+perform ggircs_portal_private.grant_permissions('insert', 'certification_url', 'ciip_administrator');
+
+-- Grant ciip_analyst permissions
+perform ggircs_portal_private.grant_permissions('select', 'certification_url', 'ciip_analyst');
+
+-- Grant ciip_industry_user permissions
+perform ggircs_portal_private.grant_permissions('select', 'certification_url', 'ciip_industry_user');
+perform ggircs_portal_private.grant_permissions('insert', 'certification_url', 'ciip_industry_user');
+
+-- Grant ciip_guest permissions
+-- ?
+end
+$grant$;
+
+-- Enable row-level security
+alter table ggircs_portal.certification_url enable row level security;
+
+create or replace function ggircs_portal_private.get_valid_applications_via_certification_url()
+returns setof integer as
+$fn$
+  select a.id from ggircs_portal.application a
+    join ggircs_portal.facility f
+      on a.facility_id = f.id
+    join ggircs_portal.ciip_user_organisation cuo
+      on f.organisation_id = cuo.organisation_id
+    join ggircs_portal.ciip_user cu
+      on cuo.user_id = cu.id
+      and cu.uuid = (select sub from ggircs_portal.session());
+$fn$ language sql strict stable;
+
+grant execute on function ggircs_portal_private.get_valid_applications_via_certification_url to ciip_administrator, ciip_analyst, ciip_industry_user;
+
+do
+$policy$
+declare industry_user_statement text;
+begin
+-- ciip_administrator RLS
+perform ggircs_portal_private.upsert_policy('ciip_administrator_select_certification_url', 'certification_url', 'select', 'ciip_administrator', 'true');
+perform ggircs_portal_private.upsert_policy('ciip_administrator_insert_certification_url', 'certification_url', 'insert', 'ciip_administrator', 'true');
+
+-- ciip_analyst RLS
+perform ggircs_portal_private.upsert_policy('ciip_analyst_select_certification_url', 'certification_url', 'select', 'ciip_analyst', 'true');
+
+-- statement for select using & insert with check
+industry_user_statement := 'application_id in (select ggircs_portal_private.get_valid_applications_via_certification_url())' ;
+
+-- ciip_industry_user RLS
+perform ggircs_portal_private.upsert_policy('ciip_industry_user_select_certification_url', 'certification_url', 'select', 'ciip_industry_user', industry_user_statement);
+perform ggircs_portal_private.upsert_policy('ciip_industry_user_insert_certification_url', 'certification_url', 'insert', 'ciip_industry_user', industry_user_statement);
+
+end
+$policy$;
 
 comment on table ggircs_portal.certification_url is 'Table containing the certification_url for an application';
 comment on column ggircs_portal.certification_url.id is 'Unique ID for the certification_url';
