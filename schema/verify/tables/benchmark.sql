e2a6cf007cd0bcde006cfa4848f2ce5c4c24527f
Dylan Leard <dylan@button.is>
2020-02-07 13:16:35 -0800

    add RLS for benchmark table + pgtap tests

--- schema/verify/tables/benchmark.sql
+++ schema/verify/tables/benchmark.sql
@@ -4,4 +4,20 @@ begin;
 
 select pg_catalog.has_table_privilege('ggircs_portal.benchmark', 'select');
 
+-- ciip_administrator Grants
+select ggircs_portal_private.verify_grant('select', 'benchmark', 'ciip_administrator');
+select ggircs_portal_private.verify_grant('insert', 'benchmark', 'ciip_administrator');
+select ggircs_portal_private.verify_grant('update', 'benchmark', 'ciip_administrator');
+
+-- ciip_analyst Grants
+select ggircs_portal_private.verify_grant('select', 'benchmark', 'ciip_analyst');
+
+-- ciip_administrator Policies
+select ggircs_portal_private.verify_policy('select', 'ciip_administrator_select_benchmark', 'benchmark', 'ciip_administrator');
+select ggircs_portal_private.verify_policy('insert', 'ciip_administrator_insert_benchmark', 'benchmark', 'ciip_administrator');
+select ggircs_portal_private.verify_policy('update', 'ciip_administrator_update_benchmark', 'benchmark', 'ciip_administrator');
+
+-- ciip_analyst Policies
+select ggircs_portal_private.verify_policy('select', 'ciip_analyst_select_benchmark', 'benchmark', 'ciip_analyst');
+
 rollback;
