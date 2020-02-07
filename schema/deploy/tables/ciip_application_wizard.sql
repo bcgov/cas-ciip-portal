3ba13d674afca2eec22748f2c39a152bcc15e8c0
Dylan Leard <dylan@button.is>
2020-02-07 13:43:49 -0800

    add RLS for ciip_application_wizard table + pgtap tests

--- schema/deploy/tables/ciip_application_wizard.sql
+++ schema/deploy/tables/ciip_application_wizard.sql
@@ -12,7 +12,44 @@ create table ggircs_portal.ciip_application_wizard (
 create unique index ciip_application_wizard_form_id_uindex on ggircs_portal.ciip_application_wizard(form_id);
 create unique index ciip_application_wizard_form_position_uindex on ggircs_portal.ciip_application_wizard(form_position);
 
-grant all on table ggircs_portal.ciip_application_wizard to ciip_administrator, ciip_analyst, ciip_industry_user;
+do
+$grant$
+begin
+-- Grant ciip_administrator permissions
+perform ggircs_portal_private.grant_permissions('select', 'ciip_application_wizard', 'ciip_administrator');
+perform ggircs_portal_private.grant_permissions('insert', 'ciip_application_wizard', 'ciip_administrator');
+perform ggircs_portal_private.grant_permissions('update', 'ciip_application_wizard', 'ciip_administrator');
+
+-- Grant ciip_analyst permissions
+perform ggircs_portal_private.grant_permissions('select', 'ciip_application_wizard', 'ciip_analyst');
+
+-- Grant ciip_industry_user permissions
+perform ggircs_portal_private.grant_permissions('select', 'ciip_application_wizard', 'ciip_industry_user');
+
+-- Grant ciip_guest permissions
+-- ?
+end
+$grant$;
+
+-- Enable row-level security
+alter table ggircs_portal.ciip_application_wizard enable row level security;
+
+do
+$policy$
+begin
+-- ciip_administrator RLS
+perform ggircs_portal_private.upsert_policy('ciip_administrator_select_ciip_application_wizard', 'ciip_application_wizard', 'select', 'ciip_administrator', 'true');
+perform ggircs_portal_private.upsert_policy('ciip_administrator_insert_ciip_application_wizard', 'ciip_application_wizard', 'insert', 'ciip_administrator', 'true');
+perform ggircs_portal_private.upsert_policy('ciip_administrator_update_ciip_application_wizard', 'ciip_application_wizard', 'update', 'ciip_administrator', 'true');
+
+-- ciip_analyst RLS
+perform ggircs_portal_private.upsert_policy('ciip_analyst_select_ciip_application_wizard', 'ciip_application_wizard', 'select', 'ciip_analyst', 'true');
+
+-- ciip_industry_user RLS
+perform ggircs_portal_private.upsert_policy('ciip_industry_user_select_ciip_application_wizard', 'ciip_application_wizard', 'select', 'ciip_industry_user', 'true');
+
+end
+$policy$;
 
 comment on table ggircs_portal.ciip_application_wizard is 'A table defining which forms are in the application wizard and their order';
 comment on column ggircs_portal.ciip_application_wizard.form_id is 'The id of the form';
--- schema/verify/tables/ciip_application_wizard.sql
+++ schema/verify/tables/ciip_application_wizard.sql
@@ -4,4 +4,30 @@ begin;
 
 select pg_catalog.has_table_privilege('ggircs_portal.ciip_application_wizard', 'select');
 
+-- ciip_administrator Grants
+select ggircs_portal_private.verify_grant('select', 'ciip_application_wizard', 'ciip_administrator');
+select ggircs_portal_private.verify_grant('insert', 'ciip_application_wizard', 'ciip_administrator');
+select ggircs_portal_private.verify_grant('update', 'ciip_application_wizard', 'ciip_administrator');
+
+-- ciip_analyst Grants
+select ggircs_portal_private.verify_grant('select', 'ciip_application_wizard', 'ciip_analyst');
+
+-- ciip_industry_user Grants
+select ggircs_portal_private.verify_grant('select', 'ciip_application_wizard', 'ciip_industry_user');
+
+-- ciip_guest Grants
+
+-- ciip_administrator Policies
+select ggircs_portal_private.verify_policy('select', 'ciip_administrator_select_ciip_application_wizard', 'ciip_application_wizard', 'ciip_administrator');
+select ggircs_portal_private.verify_policy('insert', 'ciip_administrator_insert_ciip_application_wizard', 'ciip_application_wizard', 'ciip_administrator');
+select ggircs_portal_private.verify_policy('update', 'ciip_administrator_update_ciip_application_wizard', 'ciip_application_wizard', 'ciip_administrator');
+
+-- ciip_analyst Policies
+select ggircs_portal_private.verify_policy('select', 'ciip_analyst_select_ciip_application_wizard', 'ciip_application_wizard', 'ciip_analyst');
+
+-- ciip_industry_user Policies
+select ggircs_portal_private.verify_policy('select', 'ciip_industry_user_select_ciip_application_wizard', 'ciip_application_wizard', 'ciip_industry_user');
+
+-- ciip_guest Policies
+
 rollback;
