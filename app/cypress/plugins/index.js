7072df39d6840021f562959aa13bf3ab7bb1f9e1
Matthieu Foucault <matthieu@button.is>
2020-02-11 10:42:08 -0800

    test (e2e): add a timeout to axios request in percyHealthCheck

--- app/cypress/plugins/index.js
+++ app/cypress/plugins/index.js
@@ -1,5 +1,16 @@
-const percyHealthCheck = require('@percy/cypress/task');
+const Axios = require('axios');
 
 module.exports = (on, _config) => {
-  on('task', percyHealthCheck);
+  on('task', {
+    percyHealthCheck() {
+      return (
+        Axios.get('http://localhost:5338/percy/healthcheck', {
+          timeout: 10000
+        }) // This is not a real promise
+          // eslint-disable-next-line promise/prefer-await-to-then
+          .then(() => true)
+          .catch(() => false)
+      );
+    }
+  });
 };
