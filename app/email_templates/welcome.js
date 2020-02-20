332ac8309e0e36bd44b1623ff9b57e3301ba5f4e
Dylan Leard <dylan@button.is>
2020-02-20 12:39:00 -0800

    playing with tempalte

--- app/email_templates/welcome.js
+++ app/email_templates/welcome.js
@@ -2,14 +2,13 @@
 
 const createWelcomeMail = customVariables => {
   console.log(customVariables);
-  return `<div style="padding: 40px 0 30px 0; margin-left: 150px; margin-right: 150px;">
-	          <span>Province of BC Logo?</span>
-            <span style= "float: right">CleanBC Logo?</span>
-          </div>
-          <table align="center" border="1" cellpadding="0" cellspacing="0" width="600">
+  return `<table align="center" border="1" cellpadding="0" cellspacing="0" width="600">
             <tr>
-            <td/>
               <td bgcolor="#ffffff" style="padding: 40px 0 30px 0;" align="center">
+                <span style= "float: left">Province of BC Logo?</span>
+                <span style= "float: right">CleanBC Logo?</span>
+                <br/>
+                <br/>
                 <h3>Hello, ${customVariables.firstName} ${customVariables.lastName}.</h3>
                 <br/>
                 <h4>Thank you for registering for the CleanBC Industrial Incentive Program.</h4>
@@ -18,21 +17,18 @@ const createWelcomeMail = customVariables => {
                 <p>Email stuff</p>
                 <p>Email stuff</p>
                 <p>Email stuff</p>
-                <p>Email stuff</p>
-                <p>Email stuff</p>
-                <p>Email stuff</p>
+                <br/>
+                <p>Sent to : ${customVariables.email}.</p>
+                <p>Not you? Well that's just hard cheese my friend.</p>
               </td>
-              <td/>
             </tr>
             <tr>
-              <td/>
               <td bgcolor="#ffffff" style="padding: 40px 0 30px 0;" align="center">
                 <p>Bears are cool.</p>
                 <p>Do you like bears?</p>
                 <p>If you like bears click here:
                 <a href='https://tenor.com/search/dancing-polar-bear-meme-gifs'>BEARS!</a></p>
               </td>
-              <td/>
             </tr>
           </table>`;
 };
