56cba454b328e9d16f39c463618fb6d355cea72f
Dylan Leard <dylan@button.is>
2020-02-26 14:30:08 -0800

    update schema & fix typo

--- schema/deploy/computed_columns/certification_url_hash_matches.sql
+++ schema/deploy/computed_columns/certification_url_hash_matches.sql
@@ -15,7 +15,7 @@ begin;
     current_md5 bytea;
     begin
         current_md5 := (select ggircs_portal_private.current_form_results_md5(certification_url.application_id, certification_url.version_number));
-        select certification_url.form_result_md5 = current_md5 into result;
+        select certification_url.form_results_md5 = current_md5 into result;
       return result;
     end;
     $function$
