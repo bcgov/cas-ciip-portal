fa8721610907199cbf49214203168db710d47fc5
Dylan Leard <dylan@button.is>
2020-02-26 16:23:06 -0800

    update setup/teardown of cypress test after changes to schema

--- schema/data/fixtures/reporter-all-access-teardown.sql
+++ schema/data/fixtures/reporter-all-access-teardown.sql
@@ -1,7 +1,7 @@
 begin;
 
 update ggircs_portal.application_revision_status set application_revision_status = 'draft' where application_id=2 and version_number=1;
-update ggircs_portal.application_revision set legal_disclaimer_accepted = false, certification_signature = null where application_id=2 and version_number=1;
+update ggircs_portal.application_revision set legal_disclaimer_accepted = false where application_id=2 and version_number=1;
 delete from ggircs_portal.certification_url where application_id=2;
 enable trigger _random_id;
 delete from ggircs_portal.ciip_user_organisation where user_id=6 and organisation_id=7;
