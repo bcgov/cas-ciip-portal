60f76ba9b92f5b55e3987477b9d4108174a26a73
Dylan Leard <dylan@button.is>
2020-03-04 11:34:28 -0800

    update cypress setup/teardown fixtureas

--- schema/data/fixtures/reporter-all-access-teardown.sql
+++ schema/data/fixtures/reporter-all-access-teardown.sql
@@ -9,5 +9,9 @@ alter table ggircs_portal.ciip_user_organisation
   enable trigger _set_user_id;
 alter table ggircs_portal.application_revision_status
   enable trigger _status_change_email;
+alter table ggircs_portal.certification_url
+  enable trigger _certification_request_email;
+alter table ggircs_portal.certification_url
+  enable trigger _signed_by_certifier_email;
 
 commit;
