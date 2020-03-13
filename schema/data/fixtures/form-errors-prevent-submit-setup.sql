56636594b3a4af48f390bc5795634f86998a037c
Dylan Leard <dylan@button.is>
2020-03-13 14:24:47 -0700

    update here-string to sqlFixture cypress command so it ignores comments in the fixture files

--- schema/data/fixtures/form-errors-prevent-submit-setup.sql
+++ schema/data/fixtures/form-errors-prevent-submit-setup.sql
@@ -1,3 +1,7 @@
+-- disable _set_user_id trigger so a value can be inserted in the ciip_user_organisation table
+-- insert an 'approved' row in ciip_user_organisation so our cypress_test_reporter has access the the application we are testing (id=2)
+-- set legal_disclaimer_accepted=true so we can bypass the legal checkboxes & go right to the summary page
+
 begin;
 
 alter table ggircs_portal.ciip_user_organisation
