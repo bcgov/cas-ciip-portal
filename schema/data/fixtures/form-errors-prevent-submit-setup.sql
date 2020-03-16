b6fed15b604901e1a1a1341e6cf0241b6d697e94
Dylan Leard <dylan@button.is>
2020-03-16 14:04:41 -0700

    fix form-errors-preven-submit-setup fixture

--- schema/data/fixtures/form-errors-prevent-submit-setup.sql
+++ schema/data/fixtures/form-errors-prevent-submit-setup.sql
@@ -8,5 +8,7 @@ alter table ggircs_portal.ciip_user_organisation
   disable trigger _set_user_id;
 insert into ggircs_portal.ciip_user_organisation(user_id, organisation_id, status) values (6, 7, 'approved');
 update ggircs_portal.application_revision set legal_disclaimer_accepted = true where application_id=2 and version_number=1;
+update ggircs_portal.form_result set form_result = '{}'
+  where application_id=2 and version_number=1 and form_id=1;
 
 commit;
