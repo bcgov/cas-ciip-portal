23575165e295222bcae026c7adc92f4fa6354058
Matthieu Foucault <matthieu@button.is>
2020-03-26 09:48:02 -0700

    test: remove ciip_incentive tests checking incentive amounts

--- schema/test/unit/computed_columns/application_revision_ciip_incentive_test.sql
+++ schema/test/unit/computed_columns/application_revision_ciip_incentive_test.sql
@@ -3,32 +3,11 @@ create extension if not exists pgtap;
 reset client_min_messages;
 
 begin;
-select plan(7);
+select plan(5);
 
 select has_function(
   'ggircs_portal', 'application_revision_ciip_incentive', ARRAY['ggircs_portal.application_revision'],
   'Function application_revision_ciip_incentive should exist'
-);
-
-select results_eq(
-  $$
-    with record as (select row(application_revision.*)::ggircs_portal.application_revision from ggircs_portal.application_revision where application_id = 1 and version_number = 1)
-    select incentive_product from ggircs_portal.application_revision_ciip_incentive((select * from record)) where product_name = 'Other Pulp (Mechanical pulp, paper, newsprint)';
-  $$,
-  ARRAY[14537.6::numeric],
-  'ciip incentive function returns correct value when benchmark exists'
-);
-
--- remove benchmark for product_id 29
-delete from ggircs_portal.benchmark where product_id = 29;
-
-select results_eq(
-  $$
-    with record as (select row(application_revision.*)::ggircs_portal.application_revision from ggircs_portal.application_revision where application_id = 1 and version_number = 1)
-    select incentive_product from ggircs_portal.application_revision_ciip_incentive((select * from record)) where product_name = 'Other Pulp (Mechanical pulp, paper, newsprint)';
-  $$,
-  ARRAY[0::numeric],
-  'ciip incentive function returns incentive_product=0 when benchmark does not exist'
 );
 
 set role ciip_administrator;
