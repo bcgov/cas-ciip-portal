07dfc745bb7f66a496f6441cac8e6308450cfe5d
Dylan Leard <dylan@button.is>
2020-04-02 11:27:47 -0700

    remove old unused files

--- schema/revert/views/ciip_incentive_payment.sql
+++
@@ -1,7 +0,0 @@
--- Revert ggircs-portal:view_ciip_incentive_payment from pg
-
-begin;
-
-drop view ggircs_portal.ciip_incentive_payment;
-
-commit;
--- schema/verify/views/ciip_incentive_payment.sql
+++
@@ -1,7 +0,0 @@
--- Verify ggircs-portal:view_ciip_incentive_payment on pg
-
-begin;
-
-select pg_catalog.has_table_privilege('ggircs_portal.ciip_incentive_payment', 'select');
-
-rollback;
