b83db3156a21d71c2aaa70258fe7d7846c75c9ba
Dylan Leard <dylan@button.is>
2020-05-04 13:45:19 -0700

    refactor(product): remove save_product_mutation_chain

---
+++ app/mutations/product/updateProductMutation.ts
@@ -0,0 +1,33 @@
+import {graphql} from 'react-relay';
+import {RelayModernEnvironment} from 'relay-runtime/lib/store/RelayModernEnvironment';
+import {
+  updateProductMutation as updateProductMutationType,
+  updateProductMutationVariables
+} from 'updateProductMutation.graphql';
+import BaseMutation from 'mutations/BaseMutation';
+
+const mutation = graphql`
+  mutation updateProductMutation($input: UpdateProductInput!) {
+    updateProduct(input: $input) {
+      product {
+        id
+        product_name
+        product_description
+        product_state
+      }
+    }
+  }
+`;
+
+// TODO: May want to surface the onCompleted errors to the user (ie not reject, resolve & report)
+const updateProductMutation = async (
+  environment: RelayModernEnvironment,
+  variables: updateProductMutationVariables
+) => {
+  const m = new BaseMutation<updateProductMutationType>(
+    'update-product-mutation'
+  );
+  return m.performMutation(environment, mutation, variables);
+};
+
+export default updateProductMutation;
