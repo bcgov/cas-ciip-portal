37a8c227290c12b684a3774db8dfd102b89ddf68
Matthieu Foucault <matthieu@button.is>
2020-05-05 16:05:28 -0700

    test(e2e): application-form-validation should continue on valid form

--- schema/data/fixtures/form-validation-setup.sql
+++ schema/data/fixtures/form-validation-setup.sql
@@ -10,8 +10,7 @@ alter table ggircs_portal.ciip_user_organisation
 delete from ggircs_portal.ciip_user_organisation where user_id=6 and organisation_id=7;
 insert into ggircs_portal.ciip_user_organisation(user_id, organisation_id, status) values (6, 7, 'approved');
 truncate ggircs_portal.application restart identity cascade;
-select ggircs_portal.create_application_mutation_chain(1);
 select ggircs_portal.create_application_mutation_chain(2);
-update ggircs_portal.application_revision set legal_disclaimer_accepted=true where application_id=2 and version_number=1;
+update ggircs_portal.application_revision set legal_disclaimer_accepted=true where application_id=1 and version_number=1;
 
 commit;
