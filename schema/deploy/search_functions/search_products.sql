ecb9cd8a989d383e0c90eb62c03785f64c252bfa
Dylan Leard <dylan@button.is>
2020-05-06 14:51:47 -0700

    feat(product): update search_products.sql to align with new benchmark creation strategy

--- schema/deploy/search_functions/search_products.sql
+++ schema/deploy/search_functions/search_products.sql
@@ -11,60 +11,51 @@ create or replace function ggircs_portal.search_products(
 )
 
 returns setof ggircs_portal.product as
-        $function$
-            begin
-              if search_field is null or search_value is null
-                  then return query execute '
-                  with outerTable as
-                  (
-                    select
-                      p.*,
-                      benchmark,
-                      eligibility_threshold,
-                      incentive_multiplier,
-                      start_reporting_year,
-                      end_reporting_year
-                    from ggircs_portal.product as p
-                    left join ggircs_portal.benchmark as b on p.id = b.product_id
-                  )
-                  select
-                    id, product_name, units,
-                    product_state, requires_emission_allocation, is_ciip_product, requires_product_amount, subtract_exported_electricity_emissions, subtract_exported_electricity_emissions, subtract_exported_heat_emissions, subtract_exported_heat_emissions,
-                    subtract_generated_electricity_emissions, subtract_generated_heat_emissions, add_emissions_from_eios, created_at, created_by, updated_at, updated_by, deleted_at, deleted_by
-                    from outerTable
-                where
-                    (
-                      date_part(''year'', CURRENT_TIMESTAMP) between start_reporting_year and end_reporting_year
-                     or benchmark is null)
-                  order by ' || order_by_field || ' ' || direction;
-              else
-              return query execute
-              'with outerTable as
-              (
-                select
-                  p.*,
-                  benchmark,
-                  eligibility_threshold,
-                  incentive_multiplier,
-                  start_reporting_year,
-                  end_reporting_year
-                from ggircs_portal.product as p
-                left join ggircs_portal.benchmark as b on p.id = b.product_id
-              )
-              select
-                id, product_name, units, product_state,
-                requires_emission_allocation, is_ciip_product, requires_product_amount, subtract_exported_electricity_emissions, subtract_exported_electricity_emissions, subtract_exported_heat_emissions, subtract_exported_heat_emissions,
-                subtract_generated_electricity_emissions, subtract_generated_heat_emissions, add_emissions_from_eios, created_at, created_by, updated_at, updated_by, deleted_at, deleted_by from outerTable
-              where
-                '|| search_field || '::text ilike ''%' || search_value || '%''
-              and
-                (
-                  date_part(''year'', CURRENT_TIMESTAMP) between start_reporting_year and end_reporting_year
-                 or benchmark is null)
-              order by '|| order_by_field || ' ' || direction;
-              end if;
-            end
-        $function$ language plpgsql stable;
+  $function$
+    declare
+      search_query_input_query text;
+    begin
+
+      search_query_input_query :='with outerTable as
+          (
+            select
+              p.*,
+              benchmark,
+              eligibility_threshold,
+              incentive_multiplier,
+              start_reporting_year,
+              end_reporting_year,
+              b.created_at as benchmark_created_at
+            from ggircs_portal.product as p
+            left join ggircs_portal.benchmark as b
+              on p.id = b.product_id
+          ),
+          innerTable as
+          (
+            select distinct on (id) * from outerTable order by id, benchmark_created_at desc
+          )';
+
+      if search_field is null or search_value is null
+          then return query execute search_query_input_query ||
+            'select
+              id, product_name, units,
+              product_state, requires_emission_allocation, is_ciip_product, requires_product_amount, subtract_exported_electricity_emissions, subtract_exported_electricity_emissions, subtract_exported_heat_emissions, subtract_exported_heat_emissions,
+              subtract_generated_electricity_emissions, subtract_generated_heat_emissions, add_emissions_from_eios, created_at, created_by, updated_at, updated_by, deleted_at, deleted_by
+              from innerTable order by ' || order_by_field || ' ' || direction;
+      else
+        return query execute search_query_input_query ||
+          'select
+            id, product_name, units, product_state,
+            requires_emission_allocation, is_ciip_product, requires_product_amount, subtract_exported_electricity_emissions, subtract_exported_electricity_emissions, subtract_exported_heat_emissions, subtract_exported_heat_emissions,
+            subtract_generated_electricity_emissions, subtract_generated_heat_emissions, add_emissions_from_eios, created_at, created_by, updated_at, updated_by, deleted_at, deleted_by
+            from innerTable
+          where
+            '|| search_field || '::text ilike ''%' || search_value || '%''
+          order by '|| order_by_field || ' ' || direction;
+
+      end if;
+    end
+  $function$ language plpgsql stable;
 
   grant execute on function ggircs_portal.search_products to ciip_administrator, ciip_analyst, ciip_industry_user;
 
