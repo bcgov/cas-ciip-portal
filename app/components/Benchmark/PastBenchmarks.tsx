05863256d2d8fc79f768381e19fd0428dcb44661
Dylan Leard <dylan@button.is>
2020-05-07 10:33:50 -0700

    feat(benchmark): add past benchmarks to modal as a table

---
+++ app/components/Benchmark/PastBenchmarks.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {Table} from 'react-bootstrap';
+import moment from 'moment-timezone';
+
+interface Props {
+  pastBenchmarks: any;
+}
+
+const PastBenchmarksComponent: React.FunctionComponent<Props> = (props) => {
+  const {pastBenchmarks} = props;
+
+  const renderPastBenchmarks = pastBenchmarks.map(({node}) => (
+    <tr key={node.id}>
+      <td>{moment(node.createdAt).format('DD-MM-YYYY')}</td>
+      <td>{node.benchmark}</td>
+      <td>{node.startReportingYear}</td>
+      <td>{node.endReportingYear}</td>
+      <td>{node.eligibilityThreshold}</td>
+      <td>{node.incentiveMultiplier}</td>
+      <td>{node.minimumIncentiveRatio}</td>
+      <td>{node.maximumIncentiveRatio}</td>
+    </tr>
+  ));
+
+  return (
+    <>
+      <br />
+      <span>
+        <h5>Past Benchmarks</h5>
+        <hr />
+      </span>
+      <Table style={{textAlign: 'center'}}>
+        <thead>
+          <tr>
+            <th>Date Created (D-M-Y)</th>
+            <th>Benchmark</th>
+            <th>Start</th>
+            <th>End</th>
+            <th>ET</th>
+            <th>Multiplier</th>
+            <th>Min Ratio</th>
+            <th>Max Ratio</th>
+          </tr>
+        </thead>
+        <tbody>{renderPastBenchmarks}</tbody>
+      </Table>
+    </>
+  );
+};
+
+export default PastBenchmarksComponent;
