aec30bdac294a27a085e500281244b1dce57a363
Dylan Leard <dylan@button.is>
2020-05-11 10:02:07 -0700

    test(emission): add comment on ciip_fuel view for new column emission_category_id

--- schema/deploy/views/ciip_fuel.sql
+++ schema/deploy/views/ciip_fuel.sql
@@ -31,5 +31,6 @@ comment on column ggircs_portal.ciip_fuel.version_number is 'The application rev
 comment on column ggircs_portal.ciip_fuel.quantity is 'The fuel quantity';
 comment on column ggircs_portal.ciip_fuel.fuel_units is 'The fuel units';
 comment on column ggircs_portal.ciip_fuel.fuel_id is 'The id of the fuel';
+comment on column ggircs_portal.ciip_fuel.emission_category_id is 'The foreign key to the emission_category table, defines what emission category the fuel belongs to';
 
 commit;
