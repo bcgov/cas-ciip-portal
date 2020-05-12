e1d37fd35c558441216c436c4cbe9786eadd871d
Dylan Leard <dylan@button.is>
2020-05-12 16:11:31 -0700

    refactor(row level security): add policy file to create policies for facility table

--- schema/deploy/tables/facility.sql
+++ schema/deploy/tables/facility.sql
@@ -43,52 +43,16 @@ do
 -- Enable row-level security
 alter table ggircs_portal.facility enable row level security;
 
-create or replace function ggircs_portal_private.get_valid_facility_organisation()
-returns setof integer as
-$fn$
-  select o.id from ggircs_portal.organisation o
-    join ggircs_portal.ciip_user_organisation cuo
-      on o.id = cuo.organisation_id
-    join ggircs_portal.ciip_user cu
-      on cuo.user_id = cu.id
-      and cu.uuid = (select sub from ggircs_portal.session());
-$fn$ language sql strict stable;
 
-grant execute on function ggircs_portal_private.get_valid_facility_organisation to ciip_administrator, ciip_analyst, ciip_industry_user;
-
-do
-  $policy$
-  declare industry_user_statement text;
-    begin
-      -- ciip_administrator RLS
-      perform ggircs_portal_private.upsert_policy('ciip_administrator_select_facility', 'facility', 'select', 'ciip_administrator', 'true');
-      perform ggircs_portal_private.upsert_policy('ciip_administrator_insert_facility', 'facility', 'insert', 'ciip_administrator', 'true');
-      perform ggircs_portal_private.upsert_policy('ciip_administrator_update_facility', 'facility', 'update', 'ciip_administrator', 'true');
-
-      -- ciip_analyst RLS
-      perform ggircs_portal_private.upsert_policy('ciip_analyst_select_facility', 'facility', 'select', 'ciip_analyst', 'true');
-      perform ggircs_portal_private.upsert_policy('ciip_analyst_insert_facility', 'facility', 'insert', 'ciip_analyst', 'true');
-      perform ggircs_portal_private.upsert_policy('ciip_analyst_update_facility', 'facility', 'update', 'ciip_analyst', 'true');
-
-      -- statement for select using & insert with check
-      industry_user_statement := 'organisation_id in (select ggircs_portal_private.get_valid_facility_organisation())' ;
-
-      -- ciip_industry_user RLS
-      perform ggircs_portal_private.upsert_policy('ciip_industry_user_select_facility', 'facility', 'select', 'ciip_industry_user', industry_user_statement);
-      perform ggircs_portal_private.upsert_policy('ciip_industry_user_insert_facility', 'facility', 'insert', 'ciip_industry_user', industry_user_statement);
-
-    end
-  $policy$;
-
-    comment on table ggircs_portal.facility is 'Table containing information on an facility that has applied for CIIP';
-    comment on column ggircs_portal.facility.id is 'unique id for the facility';
-    comment on column ggircs_portal.facility.organisation_id is 'the id of the organization responsible for that facility';
-    comment on column ggircs_portal.facility.report_id is 'report id from swrs';
-    comment on column ggircs_portal.facility.swrs_report_id is 'swrs report id from swrs';
-    comment on column ggircs_portal.facility.swrs_facility_id is 'swrs facility id from swrs';
-    comment on column ggircs_portal.facility.swrs_organisation_id is 'swrs organisation id';
-    comment on column ggircs_portal.facility.facility_name is 'the facility name';
-    comment on column ggircs_portal.facility.facility_type is 'type of facility based on emission quantity';
-    comment on column ggircs_portal.facility.bcghgid is 'CAS internal identifier';
+  comment on table ggircs_portal.facility is 'Table containing information on an facility that has applied for CIIP';
+  comment on column ggircs_portal.facility.id is 'unique id for the facility';
+  comment on column ggircs_portal.facility.organisation_id is 'the id of the organization responsible for that facility';
+  comment on column ggircs_portal.facility.report_id is 'report id from swrs';
+  comment on column ggircs_portal.facility.swrs_report_id is 'swrs report id from swrs';
+  comment on column ggircs_portal.facility.swrs_facility_id is 'swrs facility id from swrs';
+  comment on column ggircs_portal.facility.swrs_organisation_id is 'swrs organisation id';
+  comment on column ggircs_portal.facility.facility_name is 'the facility name';
+  comment on column ggircs_portal.facility.facility_type is 'type of facility based on emission quantity';
+  comment on column ggircs_portal.facility.bcghgid is 'CAS internal identifier';
 
 commit;
