f0ce0171072e71e9f4478f833bf720c6b4f1ef42
Dylan Leard <dylan@button.is>
2020-05-25 11:27:22 -0700

    test: update certifier-requests page unit test

--- app/tests/unit/pages/certifier-requests.test.tsx
+++ app/tests/unit/pages/certifier-requests.test.tsx
@@ -7,12 +7,10 @@ const query: requestsQueryResponse['query'] = {
   session: {
     ' $fragmentRefs': {
       defaultLayout_session: true
-    },
-    ciipUserBySub: {
-      ' $fragmentRefs': {
-        CertificationRequestsTable_query: true
-      }
     }
+  },
+  ' $fragmentRefs': {
+    CertificationRequestsContainer_query: true
   }
 };
 
@@ -22,13 +20,8 @@ describe('certifier requests list page', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('It passes a query to the CertificationRequestsTable component', () => {
+  it('It passes a query to the CertificationRequestsContainer', () => {
     const wrapper = shallow(<CertifierRequests query={query} router={null} />);
-    expect(
-      wrapper
-        .find('Relay(CertificationRequestsTableComponent)')
-        .first()
-        .prop('query')
-    ).toBe(query.session.ciipUserBySub);
+    expect(wrapper.find('SearchTableComponent').prop('query')).toBe(query);
   });
 });
