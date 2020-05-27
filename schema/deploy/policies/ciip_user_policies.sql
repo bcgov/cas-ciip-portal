58500c846e41e6635e18dc0509b369af1ad60510
Dylan Leard <dylan@button.is>
2020-05-27 15:42:32 -0700

    fix: allow industry users to view details of other users

--- schema/deploy/policies/ciip_user_policies.sql
+++ schema/deploy/policies/ciip_user_policies.sql
@@ -17,7 +17,7 @@ perform ggircs_portal_private.upsert_policy('ciip_analyst_insert_ciip_user', 'ci
 perform ggircs_portal_private.upsert_policy('ciip_analyst_update_ciip_user', 'ciip_user', 'update', 'ciip_analyst', 'uuid=(select sub from ggircs_portal.session())');
 
 -- ciip_industry_user RLS
-perform ggircs_portal_private.upsert_policy('ciip_industry_user_select_ciip_user', 'ciip_user', 'select', 'ciip_industry_user', 'uuid=(select sub from ggircs_portal.session())');
+perform ggircs_portal_private.upsert_policy('ciip_industry_user_select_ciip_user', 'ciip_user', 'select', 'ciip_industry_user', 'true');
 perform ggircs_portal_private.upsert_policy('ciip_industry_user_insert_ciip_user', 'ciip_user', 'insert', 'ciip_industry_user', 'uuid=(select sub from ggircs_portal.session())');
 perform ggircs_portal_private.upsert_policy('ciip_industry_user_update_ciip_user', 'ciip_user', 'update', 'ciip_industry_user', 'uuid=(select sub from ggircs_portal.session())');
 
