2a2e876ee943a2a378778ee81dc820b31a06be3d
Kristen Cooke <kristen@button.is>
2020-05-27 20:04:06 -0700

    feat: Remove disclaimer checklist before signature

--- app/pages/certifier/certify.tsx
+++ app/pages/certifier/certify.tsx
@@ -9,7 +9,6 @@ import ApplicationDetailsContainer from 'containers/Applications/ApplicationDeta
 import ApplicationRecertificationContainer from 'containers/Applications/ApplicationRecertificationContainer';
 import CertificationSignature from 'containers/Forms/CertificationSignature';
 import DefaultLayout from 'layouts/default-layout';
-import LegalDisclaimerChecklist from 'components/LegalDisclaimerChecklist';
 import {USER} from 'data/group-constants';
 
 const ALLOWED_GROUPS = [USER];
@@ -48,55 +47,40 @@ class Certify extends Component<Props> {
     }
   `;
 
-  state = {
-    allChecked: false
-  };
-
   render() {
-    const {allChecked} = this.state;
     const {query} = this.props;
     const {
       hashMatches
     } = this.props.query.application.latestDraftRevision.certificationUrl;
 
     let LegalDisclaimer = null;
-    let Signature = null;
 
     if (
       !query.application.latestDraftRevision?.certificationUrl
         ?.certificationSignature
     ) {
       LegalDisclaimer = (
-        <>
-          <Card style={{margin: '1rem 0'}}>
-            <Card.Body>
-              <Card.Title className="blue">Legal Disclaimer</Card.Title>
-              <Card.Text style={{padding: '10px 0 10px 0'}}>
-                Please review the information below before approving an
-                application.{' '}
-                <a href="/resources/disclaimer" target="_blank">
-                  (<FontAwesomeIcon icon={faExternalLinkAlt} />
-                  expand)
-                </a>
-              </Card.Text>
-              <Card>
-                <div tabIndex={0} id="disclaimer-text" className="show-scrollbar">
+        <Card style={{margin: '1rem 0'}}>
+          <Card.Body>
+            <Card.Title className="blue">Legal Disclaimer</Card.Title>
+            <Card.Text style={{padding: '10px 0 10px 0'}}>
+              Please review the information below before approving an
+              application.{' '}
+              <a href="/resources/disclaimer" target="_blank">
+                (<FontAwesomeIcon icon={faExternalLinkAlt} />
+                expand)
+              </a>
+            </Card.Text>
+            <Card>
+              <div tabIndex={0} id="disclaimer-text" className="show-scrollbar">
                 <Card.Body>
                   <LegalDisclaimerText />
                 </Card.Body>
-                </div>
-              </Card>
-            </Card.Body>
-          </Card>
-          <LegalDisclaimerChecklist
-            onChange={(allChecked) => this.setState({allChecked})}
-          />
-        </>
+              </div>
+            </Card>
+          </Card.Body>
+        </Card>
       );
-
-      if (allChecked) {
-        Signature = <CertificationSignature application={query.application} />;
-      }
     }
 
     return (
@@ -115,7 +99,7 @@ class Certify extends Component<Props> {
                 liveValidate={false}
               />
               {LegalDisclaimer}
-              {Signature}
+              <CertificationSignature application={query.application} />
             </>
           ) : (
             <ApplicationRecertificationContainer
