35bc0b373c449f175d44f9d29b8dec4bf695dcb0
Kristen Cooke <kristen@button.is>
2020-06-10 22:30:58 -0700

    feat: Email notifying admin of submitted applications

--- app/server/emailTemplates/notifyAdminApplicationSubmitted.js
+++ app/server/emailTemplates/notifyAdminApplicationSubmitted.js
@@ -3,8 +3,24 @@ const createNotifyAdminApplicationSubmittedMail = ({
   operatorName
 }) => {
   return `
-    <p>${facilityName}<p>
-    <p>${operatorName}<p>
+  <table align="center" border="1" cellpadding="0" cellspacing="0" width="600">
+    <tr>
+      <td style="padding: 40px 0 30px 0; " align="center">
+        <h2 style="text-decoration: underline">Province of British Columbia</h2>
+        <h3>Clean BC Industrial Incentive Program</h3>
+      </td>
+    </tr>
+    <tr style="border-top: 0px">
+      <td style="padding: 20px 10px 30px 10px;" >
+        <p>The <strong>${facilityName}</strong> facility of <strong>${operatorName}</strong> has submitted or updated their application.</p>
+        <p>An analyst can now review their application.</p>
+      </td>
+    </tr>
+    <tr>
+      <td style="padding: 20px 10px 30px 10px;">
+      <p>Sent on behalf of the Climate Action Secretariat &amp; the CleanBC Industrial Incentive Program</p></td>
+    </tr>
+  </table>
   `;
 };
 
