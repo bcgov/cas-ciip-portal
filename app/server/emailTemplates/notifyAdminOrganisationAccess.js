ab2c649f12641b3280eb8b0f30923221cebcd77e
Kristen Cooke <kristen@button.is>
2020-06-10 22:22:47 -0700

    feat: Email to notify admin of org access requests

--- app/server/emailTemplates/notifyAdminOrganisationAccess.js
+++ app/server/emailTemplates/notifyAdminOrganisationAccess.js
@@ -4,9 +4,24 @@ const createNotifyAdminAccessRequestMail = ({
   operatorName
 }) => {
   return `
-    <p>${firstName}<p>
-    <p>${lastName}<p>
-    <p>${operatorName}<p>
+    <table align="center" border="1" cellpadding="0" cellspacing="0" width="600">
+    <tr>
+      <td style="padding: 40px 0 30px 0; " align="center">
+        <h2 style="text-decoration: underline">Province of British Columbia</h2>
+        <h3>Clean BC Industrial Incentive Program</h3>
+      </td>
+    </tr>
+    <tr style="border-top: 0px">
+      <td style="padding: 20px 10px 30px 10px;" >
+        <p><strong>${firstName} ${lastName}</strong> has requested access to the application for <strong>${operatorName}</strong>.</p>
+        <p>An administrator can now review and approve them as an authorized representative for that Operator.</p>
+      </td>
+    </tr>
+    <tr>
+      <td style="padding: 20px 10px 30px 10px;">
+      <p>Sent on behalf of the Climate Action Secretariat &amp; the CleanBC Industrial Incentive Program</p></td>
+    </tr>
+  </table>
   `;
 };
 
