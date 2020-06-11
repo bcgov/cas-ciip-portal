14f18c4f4c205c98046bd9eff9038e4f0c1b6742
Matthieu Foucault <matthieu@button.is>
2020-06-11 15:32:14 -0700

    feat: airflow-dag-trigger job waits for the dag to complete

---
+++ .bin/trigger-airflow-dag.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# TODO: move this script to pipeline project
+
+set -e
+
+dag_id=$1
+
+run_id=$(curl -sf -u "$AIRFLOW_USERNAME":"$AIRFLOW_PASSWORD" -X POST \
+  https://cas-airflow-"$NAMESPACE".pathfinder.gov.bc.ca/api/experimental/dags/"${dag_id}"/dag_runs \
+  -H 'Cache-Control: no-cache' \
+  -H 'Content-Type: application/json' \
+  -d '{}' | jq -r .run_id )
+
+echo "Started dag run $run_id"
+
+
+function get_run_state() {
+  run_timestamp=${run_id/manual__/}
+  curl -s -u "$AIRFLOW_USERNAME":"$AIRFLOW_PASSWORD" -X GET \
+    https://cas-airflow-"$NAMESPACE".pathfinder.gov.bc.ca/api/experimental/dags/"${dag_id}"/dag_runs/"${run_timestamp}" \
+    -H 'Cache-Control: no-cache' \
+    -H 'Content-Type: application/json' \
+    -d '{}'
+}
+
+while true; do
+  state=$(get_run_state)
+  # echo $state
+  case $state in
+    '{"state":"success"}' )
+      echo "dag succeeded"
+      exit 0
+      ;;
+    '{"state":"running"}' )
+      echo -n '.'
+      sleep 1
+      ;;
+    *error* )
+      echo "$state"
+      exit 1
+      ;;
+  esac
+done
