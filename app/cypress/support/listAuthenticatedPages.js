9ec8999d5e2921df8d1aa59fcf76173bf1872fc1
Kristen Cooke <kristen@button.is>
2020-07-02 10:54:59 -0700

    test: refactor test helper (lint), ensure it's run on CI before E2E tests

--- app/cypress/support/listAuthenticatedPages.js
+++ app/cypress/support/listAuthenticatedPages.js
@@ -1,31 +1,30 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
 
-async function listAuthenticatedPages(
-  writeFile = 'cypress/support/authenticatedPages.json'
-) {
-  async function getRoutes(currentPath, ignoreFolders) {
-    const json = {};
-    const entries = await fs.readdir(currentPath, {withFileTypes: true});
+function getRoutes(currentPath, ignoreFolders) {
+  const json = {};
+  const entries = fs.readdirSync(currentPath, {withFileTypes: true});
 
-    const files = entries
-      .filter((file) => !file.isDirectory())
-      .map((file) => path.basename(file.name, '.tsx'));
+  const files = entries
+    .filter((file) => !file.isDirectory())
+    .map((file) => path.basename(file.name, '.tsx'));
 
-    const folders = entries.filter(
-      (folder) => folder.isDirectory() && !ignoreFolders.includes(folder.name)
-    );
+  const folders = entries.filter(
+    (folder) => folder.isDirectory() && !ignoreFolders.includes(folder.name)
+  );
 
-    for (const folder of folders) {
-      json[folder.name] = await getRoutes(`${currentPath}/${folder.name}`);
-    }
-
-    return folders.length === 0 ? files : json;
+  for (const folder of folders) {
+    json[folder.name] = getRoutes(`${currentPath}/${folder.name}`);
   }
 
-  const routes = await getRoutes('pages', ['resources']);
-  if (writeFile) fs.writeFile(writeFile, JSON.stringify(routes));
-  return routes;
+  return folders.length === 0 ? files : json;
+}
+
+function listAuthenticatedPages(
+  writeFile = 'cypress/support/authenticatedPages.json'
+) {
+  const routes = getRoutes('pages', ['resources']);
+  if (writeFile) fs.writeFileSync(writeFile, JSON.stringify(routes));
 }
 
 listAuthenticatedPages();
