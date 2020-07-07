e170f6504fdb7ecd34730706706925b19f24dffa
Kristen Cooke <kristen@button.is>
2020-07-07 16:44:45 -0700

    feat: 404 page

---
+++ app/pages/404.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Row, Col} from 'react-bootstrap';
+import DefaultLayout from 'layouts/default-layout';
+import Link from 'next/link';
+
+export default () => {
+  return (
+    <>
+      <DefaultLayout needsUser={false} needsSession={false} session={null}>
+        <Row className="justify-content-center" style={{paddingTop: '3em'}}>
+          <Col md={{span: 6}} style={{textAlign: 'center'}}>
+            <h1>Page not found</h1>
+            <p>Sorry, we couldn&apos;t find the page you were looking for.</p>
+            <p>
+              <Link href="/">
+                <a className="full-width btn btn-primary">Return Home</a>
+              </Link>
+            </p>
+          </Col>
+        </Row>
+      </DefaultLayout>
+      <style jsx>{`
+        p {
+          margin: 2em 0;
+        }
+      `}</style>
+    </>
+  );
+};
