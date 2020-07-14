47daee74c2cf8761a49d076924cf2919db6aa0d0
Kristen Cooke <kristen@button.is>
2020-07-14 14:16:13 -0700

    Include whether each energy_product is_ciip_product

--- schema/data/prod/energy_product.sql
+++ schema/data/prod/energy_product.sql
@@ -14,22 +14,23 @@ begin;
 alter table ggircs_portal.product disable trigger _protect_read_only_products;
 
 with rows as (
-insert into ggircs_portal.product(id, product_name, units, product_state, requires_emission_allocation, requires_product_amount, is_read_only, is_energy_product)
+insert into ggircs_portal.product(id, product_name, units, product_state, requires_emission_allocation, is_ciip_product, requires_product_amount, is_read_only, is_energy_product)
 overriding system value
 values
-(1, 'Exported electricity', 'MWh', 'published' ,true, true, true, true),
-(2, 'Exported heat', 'GJ', 'published' ,true, true, true, true),
-(3, 'Purchased electricity', 'GWh', 'published' ,true, true, true, true),
-(4, 'Purchased heat', 'GJ', 'published' ,true, true, true, true),
-(5, 'Generated electricity', 'GWh', 'published' ,true, true, true, true),
-(6, 'Generated heat', 'GJ', 'published' ,true, true, true, true),
-(7, 'Emissions from EIOs', 'GJ', 'published' ,true, true, true, true)
+(1, 'Exported electricity', 'MWh', 'published' ,true, true, true, true, true),
+(2, 'Exported heat', 'GJ', 'published' ,true, false, true, true, true),
+(3, 'Purchased electricity', 'GWh', 'published' ,true, false, true, true, true),
+(4, 'Purchased heat', 'GJ', 'published' ,true, false, true, true, true),
+(5, 'Generated electricity', 'GWh', 'published' ,true, false, true, true, true),
+(6, 'Generated heat', 'GJ', 'published' ,true, false, true, true, true),
+(7, 'Emissions from EIOs', 'GJ', 'published' ,true, false, true, true, true)
 on conflict(id) do update
 set
 product_name=excluded.product_name,
 units=excluded.units,
 product_state=excluded.product_state,
 requires_emission_allocation=excluded.requires_emission_allocation,
+is_ciip_product=excluded.is_ciip_product,
 requires_product_amount=excluded.requires_product_amount,
 is_read_only=excluded.is_read_only,
 is_energy_product=excluded.is_energy_product
