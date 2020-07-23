931d08e7ba00209a221b6b67c57214f84e4c3f55
Dylan Leard <dylan@button.is>
2020-07-23 10:21:27 -0700

    feat: add override notification component

---
+++ app/components/Application/ApplicationOverrideNotificationCard.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {Card} from 'react-bootstrap';
+
+interface Props {
+  overrideJustification: string;
+}
+
+const ApplicationOverrideNotification: React.FunctionComponent<Props> = ({
+  overrideJustification
+}) => {
+  console.log(overrideJustification);
+  return (
+    <Card style={{margin: '1rem 0'}}>
+      <Card.Header className="bg-danger">
+        <h4>Override is active</h4>
+      </Card.Header>
+      <Card.Body>
+        <Card.Text>
+          <strong>
+            There were errors in this application that the reporter has chosen
+            to override. Justification for overriding the errors is provided in
+            the box below:
+          </strong>
+        </Card.Text>
+        <Card>
+          <div tabIndex={0} id="justification" className="scrollbar">
+            <Card.Body>{overrideJustification}</Card.Body>
+          </div>
+        </Card>
+      </Card.Body>
+      <style jsx>{`
+        #justification {
+          max-height: 19.2em;
+          background: #eee;
+          overflow-y: scroll;
+        }
+        .scrollbar::-webkit-scrollbar {
+          -webkit-appearance: none;
+          width: 8px;
+        }
+        .scrollbar::-webkit-scrollbar-thumb {
+          border-radius: 4px;
+          background-color: rgba(0, 0, 0, 0.5);
+          box-shadow: 0 0 1px rgba(255, 255, 255, 0.5);
+        }
+      `}</style>
+    </Card>
+  );
+};
+
+export default ApplicationOverrideNotification;
