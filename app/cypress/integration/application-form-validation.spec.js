d523b6dafd09b883edd81f49ca366ea1f0fe3063
Dylan Leard <dylan@button.is>
2020-07-27 11:10:56 -0700

    test: happo screenshots for all states of override justification on summary page

--- app/cypress/integration/application-form-validation.spec.js
+++ app/cypress/integration/application-form-validation.spec.js
@@ -95,10 +95,24 @@ describe('When reviewing a submitted application as an analyst', () => {
       component: 'Admin Summary Card',
       variant: 'with errors'
     });
+
+    // Override Justification screenshots
     cy.get('.override-accordion').happoScreenshot({
-      component: 'Summary Page',
-      variant: 'with errors'
+      component: 'Override Justification',
+      variant: 'closed'
     });
+    cy.get('.override-accordion > .btn').click();
+    cy.get('.override-accordion').happoScreenshot({
+      component: 'Override Justification',
+      variant: 'open'
+    });
+    cy.get('#overrideJustification').clear().type('justification goes here');
+    cy.get('.btn-success').click();
+    cy.get('.alert-secondary').happoScreenshot({
+      component: 'Override Justification',
+      variant: 'override active'
+    });
+    cy.get('.btn-danger').click();
     cy.visit(adminFormUrl);
 
     // Fix invalid data
