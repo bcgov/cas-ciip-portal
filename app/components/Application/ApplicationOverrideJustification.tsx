de428f42d28d9810ee96aa923fade42aeee41f04
Dylan Leard <dylan@button.is>
2020-08-04 11:53:42 -0700

    refactor: add useEffect for handleOverrideDelete call & use guard clause to decide what to render

--- app/components/Application/ApplicationOverrideJustification.tsx
+++ app/components/Application/ApplicationOverrideJustification.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {createFragmentContainer, RelayProp} from 'react-relay';
 import {Accordion, Alert, Button, Form} from 'react-bootstrap';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
@@ -79,118 +79,121 @@ export const ApplicationOverrideJustificationComponent: React.FunctionComponent<
     setAccordionOpen(true);
   };
 
-  if (overrideJustification && !hasErrors) handleOverrideDelete();
+  useEffect(() => {
+    if (overrideJustification && !hasErrors) handleOverrideDelete();
+  });
 
-  if (hasErrors && overrideActive) {
-    return (
-      <>
-        <Alert variant="danger">
-          <strong>Error Validation Override Active</strong>
-        </Alert>
-        <Alert variant="secondary">
-          <p>
-            <strong>
-              You have chosen to override the errors present in your
-              application. This may cause a delay in processing your
-              application.
-            </strong>
-          </p>
-          <p>
-            <strong>Your Override Justification:</strong>
-          </p>
-          <p>{overrideJustification}</p>
-          <Button
-            style={{marginRight: '5px'}}
-            variant="secondary"
-            onClick={handleOverrideEdit}
-          >
-            Edit Justification
-          </Button>
-          <Button variant="danger" onClick={handleOverrideDelete}>
-            Delete Override
-          </Button>
-        </Alert>
-      </>
-    );
-  }
+  const errorsWithOverrideActive = (
+    <>
+      <Alert variant="danger">
+        <strong>Error Validation Override Active</strong>
+      </Alert>
+      <Alert variant="secondary">
+        <p>
+          <strong>
+            You have chosen to override the errors present in your application.
+            This may cause a delay in processing your application.
+          </strong>
+        </p>
+        <p>
+          <strong>Your Override Justification:</strong>
+        </p>
+        <p>{overrideJustification}</p>
+        <Button
+          style={{marginRight: '5px'}}
+          variant="secondary"
+          onClick={handleOverrideEdit}
+        >
+          Edit Justification
+        </Button>
+        <Button variant="danger" onClick={handleOverrideDelete}>
+          Delete Override
+        </Button>
+      </Alert>
+    </>
+  );
 
-  if (hasErrors) {
-    return (
-      <>
-        <Alert variant="danger">
-          <Accordion>
-            <div className="override-accordion">
-              Your application contains errors shown below that must be fixed
-              before submission. You may either correct these or alternatively,
-              override and provide justification. Your justification will be
-              reviewed by CAS and may cause a delay in processing your
-              application.
-              <Accordion.Toggle
-                as={Button}
-                variant="secondary"
-                eventKey="0"
-                onClick={() => setAccordionOpen(!accordionOpen)}
-              >
-                Override and Justify
-                <FontAwesomeIcon
-                  icon={faCaretDown}
-                  style={{marginLeft: '0.75em'}}
-                />
-              </Accordion.Toggle>
-            </div>
-            <Accordion.Collapse in={accordionOpen} eventKey="0">
-              <>
-                <Form>
-                  <h4>Override Form Validation</h4>
-                  <Form.Group controlId="overrideJustification">
-                    <Form.Label>Justification for incomplete form:</Form.Label>
-                    <Form.Control
-                      as="textarea"
-                      rows={4}
-                      value={overrideJustification || ''}
-                      onChange={handleOverrideChange}
-                    />
-                  </Form.Group>
-                  <Button variant="success" onClick={handleOverrideSave}>
-                    Save
-                  </Button>
-                  <Accordion.Toggle
-                    as={Button}
-                    eventKey="0"
-                    variant="light"
-                    style={{
-                      marginLeft: '1em',
-                      border: '1px solid currentColor'
-                    }}
-                    onClick={handleOverrideCancel}
-                  >
-                    Cancel
-                  </Accordion.Toggle>
-                </Form>
-                {!overrideJustification && (
-                  <p className="justification-text">{emptyError}</p>
-                )}
-              </>
-            </Accordion.Collapse>
-          </Accordion>
-        </Alert>
-        <style jsx>
-          {`
-            .override-accordion {
-              display: flex;
-              flex-wrap: wrap;
-              justify-content: flex-end;
-            }
-            .justification-text {
-              color: red;
-            }
-          `}
-        </style>
-      </>
-    );
-  }
+  const errorsWithoutOverrideActive = (
+    <>
+      <Alert variant="danger">
+        <Accordion>
+          <div className="override-accordion">
+            Your application contains errors shown below that must be fixed
+            before submission. You may either correct these or alternatively,
+            override and provide justification. Your justification will be
+            reviewed by CAS and may cause a delay in processing your
+            application.
+            <Accordion.Toggle
+              as={Button}
+              variant="secondary"
+              eventKey="0"
+              onClick={() => setAccordionOpen(!accordionOpen)}
+            >
+              Override and Justify
+              <FontAwesomeIcon
+                icon={faCaretDown}
+                style={{marginLeft: '0.75em'}}
+              />
+            </Accordion.Toggle>
+          </div>
+          <Accordion.Collapse in={accordionOpen} eventKey="0">
+            <>
+              <Form>
+                <h4>Override Form Validation</h4>
+                <Form.Group controlId="overrideJustification">
+                  <Form.Label>Justification for incomplete form:</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={4}
+                    value={overrideJustification || ''}
+                    onChange={handleOverrideChange}
+                  />
+                </Form.Group>
+                <Button variant="success" onClick={handleOverrideSave}>
+                  Save
+                </Button>
+                <Accordion.Toggle
+                  as={Button}
+                  eventKey="0"
+                  variant="light"
+                  style={{
+                    marginLeft: '1em',
+                    border: '1px solid currentColor'
+                  }}
+                  onClick={handleOverrideCancel}
+                >
+                  Cancel
+                </Accordion.Toggle>
+              </Form>
+              {!overrideJustification && (
+                <p className="justification-text">{emptyError}</p>
+              )}
+            </>
+          </Accordion.Collapse>
+        </Accordion>
+      </Alert>
+      <style jsx>
+        {`
+          .override-accordion {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: flex-end;
+          }
+          .justification-text {
+            color: red;
+          }
+        `}
+      </style>
+    </>
+  );
 
-  return null;
+  const renderOverride = () => {
+    if (!hasErrors) return null;
+    if (hasErrors && overrideActive) return errorsWithOverrideActive;
+    return errorsWithoutOverrideActive;
+  };
+
+  return renderOverride();
 };
 
 export default createFragmentContainer(
