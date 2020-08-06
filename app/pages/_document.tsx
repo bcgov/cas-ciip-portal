f2341f9b9efb2a29ec032a8b1f6e883aa3466a77
Dylan Leard <dylan@button.is>
2020-08-06 14:43:55 -0700

    test: add lang='en' to html tag

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -8,7 +8,7 @@ const CONFIG = getConfig()?.publicRuntimeConfig;
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
