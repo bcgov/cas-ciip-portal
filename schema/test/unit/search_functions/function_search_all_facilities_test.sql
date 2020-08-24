a0092d78977ee6beb41e659a072d94fcbd4c871a
Dylan Leard <dylan@button.is>
2020-08-24 15:52:59 -0700

    test: add pgTap tests

--- schema/test/unit/search_functions/function_search_all_facilities_test.sql
+++ schema/test/unit/search_functions/function_search_all_facilities_test.sql
@@ -4,17 +4,17 @@ reset client_min_messages;
 
 begin;
 
-select plan(3);
+select plan(5);
 
 select has_function(
-  'ggircs_portal', 'search_all_facilities', array['text', 'text', 'text', 'text', 'text', 'int', 'int'],
+  'ggircs_portal', 'search_all_facilities', array['text', 'text', 'text', 'text', 'text', 'int', 'int', 'int'],
   'Function search_all_facilities should exist'
 );
 
 set jwt.claims.sub to '00000000-0000-0000-0000-000000000000';
 
 select is(
-  (select count(*) from ggircs_portal.search_all_facilities(null, null, 'id', 'asc', null, 0, 10000)),
+  (select count(*) from ggircs_portal.search_all_facilities(null, null, 'id', 'asc', null, 0, 10000, 2019)),
   (select count(*) from ggircs_portal.facility f
     join ggircs_portal.ciip_user_organisation cuo on f.organisation_id = cuo.organisation_id
     join ggircs_portal.ciip_user cu on cuo.user_id = cu.id
@@ -22,13 +22,34 @@ select is(
   'The search_all_facilities function should return all the facilities attached to organisations the user has access to if there is organisaion_row_id is null');
 
 select is(
-  (select count(*) from ggircs_portal.search_all_facilities(null, null, 'id', 'asc', '8', 0, 10000)),
+  (select count(*) from ggircs_portal.search_all_facilities(null, null, 'id', 'asc', '8', 0, 10000, 2019)),
   (select count(*) from ggircs_portal.facility f
     join ggircs_portal.ciip_user_organisation cuo on f.organisation_id = cuo.organisation_id
     and f.organisation_id = 8
     join ggircs_portal.ciip_user cu on cuo.user_id = cu.id
     and cu.uuid = '00000000-0000-0000-0000-000000000000'::uuid),
   'The search_all_facilities function should return only the facilities attached to organisation id=8 if there is organisaion_row_id is 8 (and the user has access to that organisation)');
+
+select id, organisation_id from ggircs_portal.facility;
+select * from ggircs_portal.application;
+
+select results_eq(
+  $$
+    select application_revision_status from ggircs_portal.search_all_facilities(null, null, 'id', 'asc', '8', 0, 10000, 2019)
+  $$,
+  ARRAY['submitted'::ggircs_portal.ciip_application_revision_status],
+  'The application revision status is submitted when there is a submitted application in 2019 and 2019 is passed as a search parameter'
+);
+
+update ggircs_portal.application set reporting_year=2018 where id=1;
+
+select results_eq(
+  $$
+    select application_revision_status from ggircs_portal.search_all_facilities(null, null, 'id', 'asc', '8', 0, 10000, 2019)
+  $$,
+  ARRAY[null::ggircs_portal.ciip_application_revision_status],
+  'The application revision status is null when there is a submitted application in 2018, but not 2019 and 2019 is passed as a search parameter'
+);
 
 select finish();
 
