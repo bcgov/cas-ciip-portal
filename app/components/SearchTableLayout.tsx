e26700e02e1c13621b84dc08b3b417ed4a7a37e7
Dylan Leard <dylan@button.is>
2020-08-24 11:57:10 -0700

    refactor: fix optional chaining & drop 'remove button' instructional text

--- app/components/SearchTableLayout.tsx
+++ app/components/SearchTableLayout.tsx
@@ -24,11 +24,10 @@ export const SearchTableLayoutComponent: React.FunctionComponent<Props> = (
 
   const noSearchResults =
     body.props.children.length === 0 ||
-    body?.props?.children[0].length === 0 ? (
+    body?.props?.children[0]?.length === 0 ? (
       <span id="no-search-results">
         <Alert variant="secondary">
           <p>&quot;No matching results to show.&quot;</p>
-          <p>Clicking &apos;Reset&apos; will clear your search parameters.</p>
         </Alert>
         <br />
       </span>
