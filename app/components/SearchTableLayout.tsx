b6f105e8fe6929ea1d82716f05b4b3ba8618a880
Dylan Leard <dylan@button.is>
2020-08-25 09:56:40 -0700

    refactor: css tweaks for better message display

--- app/components/SearchTableLayout.tsx
+++ app/components/SearchTableLayout.tsx
@@ -25,12 +25,9 @@ export const SearchTableLayoutComponent: React.FunctionComponent<Props> = (
   const noSearchResults =
     body.props.children.length === 0 ||
     body?.props?.children[0]?.length === 0 ? (
-      <span id="no-search-results">
-        <Alert variant="secondary">
-          <p>&quot;No matching results to show.&quot;</p>
-        </Alert>
-        <br />
-      </span>
+      <Alert variant="secondary" id="no-search-results">
+        No matching results to show.
+      </Alert>
     ) : null;
 
   return (
