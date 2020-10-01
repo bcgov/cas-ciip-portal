a760f264b2c982f7adcfcb57cbd503fa4f4aa30b
Dylan Leard <dylan@button.is>
2020-10-01 15:24:24 -0700

    refactor: clean up old code

--- schema/data/fixtures/analyst-all-access-setup.sql
+++ schema/data/fixtures/analyst-all-access-setup.sql
@@ -42,19 +42,7 @@ select ggircs_portal.create_application_mutation_chain(1);
 select ggircs_portal.create_application_mutation_chain(2);
 end;
 
--- *2* CERTIFICATION_URL
-
--- update ggircs_portal.certification_url set certification_signature = 'signed' where application_id=2;
 insert into ggircs_portal.application_revision_status(application_id, version_number, application_revision_status)
   values (2,1,'submitted');
-
--- delete from ggircs_portal.ciip_user_organisation where user_id=6 and organisation_id=7;
--- insert into ggircs_portal.ciip_user_organisation(user_id, organisation_id, status) values (6, 7, 'approved');
--- truncate ggircs_portal.application restart identity cascade;
--- select ggircs_portal.create_application_mutation_chain(1);
--- select ggircs_portal.create_application_mutation_chain(2);
--- update ggircs_portal.certification_url set certification_signature = 'signed' where application_id=2;
--- insert into ggircs_portal.application_revision_status(application_id, version_number, application_revision_status)
---   values (2,1,'submitted');
 
 commit;
