32ceb9e1b696996080e18b0c82721f8011e7b684
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2020-10-29 15:29:16 -0700

    test: adding test method and file path to the test make command

--- mocks_schema/deploy/mock_now_method.sql
+++ mocks_schema/deploy/mock_now_method.sql
@@ -3,18 +3,24 @@
 
 BEGIN;
 
+
   create or replace function mocks.now()
   returns timestamptz as
   $function$
+    declare
+      returnValue timestamptz;
     begin
       -- look at the database options if there is a value set
       -- if it's not set the current_setting method returns nil and we coalesce to 'false'
-      return coalesce(
+      returnValue := coalesce(
           current_setting('mocks.mocked_timestamp', true),
           pg_catalog.now()::text
-      );
+      )::timestamptz;
+      return returnValue;
     exception
+      -- In case of a parsing error or anything else, we return the regular now() behaviour
       when others then
+        raise notice 'mocks.now() encountered an error and defaulted back to the regular behaviour'
         return pg_catalog.now();
     end;
   $function$ language plpgsql volatile;
