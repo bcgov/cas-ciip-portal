8f89a078e4201cce9fe3826ada863bcfb785955e
Matthieu Foucault <matthieu@button.is>
2021-01-25 11:39:16 -0800

    chore: disable import check for authenticatedPages.json

--- app/cypress/support/index.js
+++ app/cypress/support/index.js
@@ -1,6 +1,7 @@
 import './commands';
 import 'cypress-axe';
 import 'cypress-plugin-retries';
+// eslint-disable-next-line import/no-unresolved
 import authenticatedPages from './authenticatedPages.json';
 
 Cypress.env.AUTHENTICATED_PAGES = authenticatedPages;
