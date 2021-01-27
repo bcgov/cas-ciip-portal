99ac1aefe889186719ee2fe018b1d52a3fa91923
Dylan Leard <dylan@button.is>
2021-01-27 14:38:05 -0800

    test: reporter views a 2018 application e2e test

--- app/cypress/integration/ciip-2018/reporter-view-2018-data.spec.js
+++ app/cypress/integration/ciip-2018/reporter-view-2018-data.spec.js
@@ -1,12 +1,19 @@
-describe('When logged in as a reporter', () => {
+describe('When viewing an application from the 2018 reporting year', () => {
   beforeEach(() => {
-    cy.getCypressPath();
     cy.cleanSchema();
     cy.sqlFixture('fixtures/ciip-2018/add-2018-data');
     cy.mockLogin('reporter');
   });
 
-  it('The reporter does stuff', () => {
-    cy.visit('/');
+  it('The view-application page renders the 2018 ciip data', () => {
+    cy.visit(
+      '/reporter/view-application?applicationId=WyJhcHBsaWNhdGlvbnMiLDFd&version=1'
+    );
+    cy.contains('Facility Description');
+    cy.contains('orgBookLegalName');
+    cy.contains('Application Type');
+    cy.contains('gasDescription');
+    cy.contains('Fuel Type alt');
+    cy.contains('Product Name');
   });
 });
