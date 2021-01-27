0d1a57a72d32cee911a0e1e309f3e5086de479ba
Dylan Leard <dylan@button.is>
2021-01-27 08:48:29 -0800

    feat: remove uniqueness constraint on form_position
    fix: remove index

--- schema/deploy/tables/ciip_application_wizard_001.sql
+++ schema/deploy/tables/ciip_application_wizard_001.sql
@@ -4,6 +4,7 @@
 begin;
 
 alter table ggircs_portal.ciip_application_wizard add column is_active boolean default true;
+drop index ggircs_portal.ciip_application_wizard_form_position_uindex;
 
 comment on column ggircs_portal.ciip_application_wizard.is_active is 'Boolean value indicates whether the form is currently in use or has been archived';
 
