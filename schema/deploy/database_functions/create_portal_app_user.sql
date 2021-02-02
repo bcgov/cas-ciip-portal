f2b2943be7f5d869a024f32a1b2ead4dc1b54099
Dylan Leard <dylan@button.is>
2021-02-02 14:40:30 -0800

    fix: drop portal_app user & create ciip_portal user if not exists

--- schema/deploy/database_functions/create_portal_app_user.sql
+++ schema/deploy/database_functions/create_portal_app_user.sql
@@ -5,6 +5,17 @@ begin;
 do $$
   begin
     execute format('revoke all privileges on database %I from portal_app', current_database());
+
+    if not exists (
+    select true
+    from   pg_catalog.pg_roles
+    where  rolname = 'ciip_portal') then
+
+    create user ciip_portal;
+    end if;
+
+    grant ciip_administrator, ciip_analyst, ciip_industry_user, ciip_guest to ciip_portal;
+    execute format('grant create, connect on database %I to ciip_portal', current_database());
   end;
 $$;
 
--- schema/revert/database_functions/create_portal_app_user.sql
+++ schema/revert/database_functions/create_portal_app_user.sql
@@ -2,21 +2,23 @@
 -- requires: database_functions/create_roles
 
 begin;
+do $$
+  begin
+    execute format('revoke all privileges on database %I from ciip_portal', current_database());
 
-do
-$$
-begin
-  if not exists (
+    if not exists (
     select true
     from   pg_catalog.pg_roles
     where  rolname = 'portal_app') then
 
     create user portal_app;
-  end if;
+    end if;
 
-  grant ciip_administrator, ciip_analyst, ciip_industry_user, ciip_guest to portal_app;
-  execute format('grant create, connect on database %I to portal_app', current_database());
-end;
+    grant ciip_administrator, ciip_analyst, ciip_industry_user, ciip_guest to portal_app;
+    execute format('grant create, connect on database %I to portal_app', current_database());
+  end;
 $$;
+
+drop user ciip_portal;
 
 commit;
