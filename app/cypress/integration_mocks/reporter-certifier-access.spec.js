a6a8259a647daffb774760fcf9fb6d620f11a88b
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-02-03 11:18:57 -0800

    test: cypress race condition when react loading

--- app/cypress/integration_mocks/reporter-certifier-access.spec.js
+++ app/cypress/integration_mocks/reporter-certifier-access.spec.js
@@ -33,13 +33,10 @@ describe('When logged in as a certifier(reporter) they should be able to load th
 });
 
 describe('When logged in as a certifier(reporter) they should be able to load all other certification related pages', () => {
-  before(() => {
+  beforeEach(() => {
     cy.cleanSchema();
     cy.deployProdData();
     cy.sqlFixture('fixtures/certifier-access-setup');
-  });
-
-  beforeEach(() => {
     cy.mockLogin('certifier');
     cy.useMockedTime(new Date('June 10, 2020 09:00:00'));
   });
@@ -52,13 +49,14 @@ describe('When logged in as a certifier(reporter) they should be able to load al
     );
     cy.get('body').contains('Submission Certification');
     cy.url().should('include', `/certifier/certify?applicationId=`);
-    cy.get('body').contains('Submission Certification');
   });
 
   it('can access the certification requests page via a link on the reporter dash', () => {
     cy.visit('/reporter');
     cy.get('.alert-link').contains('View all certification requests.').click();
-    cy.get('table.search-table > tbody > tr > td').contains('DRAFT');
+    cy.get('table.search-table:not(.loading) > tbody > tr > td').contains(
+      'DRAFT'
+    );
     cy.url().should('include', '/certifier/requests');
   });
 
@@ -66,34 +64,37 @@ describe('When logged in as a certifier(reporter) they should be able to load al
     cy.useMockedTime(new Date('June 10, 2020 09:00:00'));
     cy.visit('/certifier/requests');
     cy.useMockedTime(new Date('June 10, 2020 09:00:00'));
-    cy.contains('test_organisation');
+
+    cy.get('table.search-table:not(.loading) > tbody > tr > td').contains(
+      'test_organisation_facility_1'
+    );
+    cy.get('table.search-table:not(.loading) > tbody > tr > td').contains(
+      'DRAFT'
+    );
 
     cy.get('body').happoScreenshot({
       component: 'Batch certification page',
       variant: 'Nothing selected'
     });
 
-    cy.useMockedTime(new Date('June 10, 2020 09:00:00'));
-    cy.visit('/certifier/requests');
-    cy.contains('test_organisation');
     cy.get('tbody input[type="checkbox"]').first().click();
     cy.contains('Legal Disclaimer');
+    cy.get('h3').contains('Certifier Signature');
     cy.get('body').happoScreenshot({
       component: 'Batch certification page',
       variant: 'Multiple certification requests selected'
     });
-    cy.useMockedTime(new Date('June 10, 2020 09:00:00'));
-    cy.contains('Certifier Signature');
+
     cy.get('.btn-success').contains('Sign').click();
     cy.get('body').should('not.contain', 'Certifier Signature');
-    cy.get('table.search-table > tbody > tr > td').contains(
+    cy.get('table.search-table:not(.loading) > tbody > tr > td').contains(
       'Cypress Certifier'
     );
-    cy.get('body').happoScreenshot({
+    // This cannot be used until we understand why the mocked timestamp doesn't work in this particular case, on circleCI
+    cy.get('main').happoScreenshot({
       component: 'Batch certification page',
       variant: 'Multiple certification requests executed'
     });
-    cy.get('body').contains('Cypress Certifier');
     cy.contains('Jun 10th, 2020');
   });
 });
