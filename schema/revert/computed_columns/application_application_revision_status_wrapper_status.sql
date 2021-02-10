08a05d4dc0083ae3bd81acab748926be74c23844
Dylan Leard <dylan@button.is>
2021-02-10 15:55:10 -0800

    feat: simplify function names & add application_bcghgid comp column

--- schema/revert/computed_columns/application_application_revision_status_wrapper_status.sql
+++
@@ -1,7 +0,0 @@
--- Revert ggircs-portal:computed_columns/application_application_revision_status_wrapper_status from pg
-
-begin;
-
-drop function ggircs_portal.application_application_revision_status_wrapper_status;
-
-commit;
