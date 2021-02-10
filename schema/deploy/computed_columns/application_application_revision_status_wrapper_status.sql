21652ac22afd63376d5fa40a10c0c2a06b064cc5
Dylan Leard <dylan@button.is>
2021-02-10 14:26:24 -0800

    feat: application-facility_name wrapper

--- schema/deploy/computed_columns/application_application_revision_status_wrapper_status.sql
+++ schema/deploy/computed_columns/application_application_revision_status_wrapper_status.sql
@@ -3,14 +3,12 @@
 
 begin;
 
--- wrapper. Note the () for notation. Failing to use them will throw an error
 create or replace function ggircs_portal.application_application_revision_status_wrapper_status(app ggircs_portal.application)
   returns varchar
   as $$
     select (ggircs_portal.application_application_revision_status(app::ggircs_portal.application, null)).application_revision_status::varchar;
   $$ language sql stable;
 
--- don't forget the comments...
 comment on function ggircs_portal.application_application_revision_status_wrapper_status(ggircs_portal.application) is E'@sortable\nThis function is a wrapper to return application_revision_status as a scalar';
 
-COMMIT;
+commit;
