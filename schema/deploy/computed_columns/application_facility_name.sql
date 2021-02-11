15405b22fa8c2fad128c227bffa7d434abe53168
Dylan Leard <dylan@button.is>
2021-02-10 17:23:00 -0800

    refactor: remove unnecessary computed columns

--- schema/deploy/computed_columns/application_facility_name.sql
+++ schema/deploy/computed_columns/application_facility_name.sql
@@ -1,14 +1,14 @@
 -- Deploy ggircs-portal:computed_columns/application_facility_name to pg
--- requires: computed_columns/application_facility
+-- requires: tables/application
 
 begin;
 
 create or replace function ggircs_portal.application_facility_name(app ggircs_portal.application)
   returns varchar
   as $$
-    select (ggircs_portal.application_facility(app::ggircs_portal.application)).facility_name::varchar;
+    select facility_name::varchar from ggircs_portal.facility where facility.id = app.facility_id;
   $$ language sql stable;
 
-comment on function ggircs_portal.application_facility_name(ggircs_portal.application) is E'@sortable\nThis function is a wrapper to return facility_name as a scalar from the composite return of the application_facility computed column';
+comment on function ggircs_portal.application_facility_name(ggircs_portal.application) is E'@sortable\nThis function is a wrapper to return facility_name from the facility object';
 
 commit;
--- schema/revert/computed_columns/application_operator.sql
+++
@@ -1,7 +0,0 @@
--- Revert ggircs-portal:computed_columns/application_operator from pg
-
-begin;
-
-drop function ggircs_portal.application_operator;
-
-commit;
