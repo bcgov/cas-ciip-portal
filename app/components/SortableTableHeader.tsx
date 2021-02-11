1c2e2215fe17fd1a4976261c4fbea70f99fb50b9
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-02-11 09:18:45 -0800

    feat: sort headers through router query string

--- app/components/SortableTableHeader.tsx
+++ app/components/SortableTableHeader.tsx
@@ -1,25 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faSort} from '@fortawesome/free-solid-svg-icons';
+import {
+  faSort,
+  faCaretUp,
+  faCaretDown
+} from '@fortawesome/free-solid-svg-icons';
+import {useRouter} from 'next/router';
 
 interface Props {
-  sort: (action: string, columnName: string) => void;
   headerVariables: {
     columnName: string;
     displayName: string;
   };
 }
 
+const SORT_DIRECTION = ['ASC', 'DESC'];
+const SORT_ICONS = [faCaretDown, faCaretUp];
+
 const SortableTableHeader: React.FunctionComponent<Props> = ({
-  sort,
   headerVariables
 }) => {
+  const router = useRouter();
+  const sortDirectionIndex = SORT_DIRECTION.indexOf(
+    router.query.direction.toString()
+  );
+  const [currentSortDirection, setCurrentSortDirection] = useState(
+    Math.max(sortDirectionIndex, 0)
+  );
+
+  const triggerSort = (columName) => {
+    //Cycle
+    const sortDirection = (currentSortDirection + 1) % 2;
+    setCurrentSortDirection(sortDirection);
+
+    const url = {
+      pathname: router.pathname,
+      query: {
+        ...router.query,
+        orderByField: columName,
+        direction: SORT_DIRECTION[sortDirection]
+      }
+    };
+
+    router.replace(url, url, {shallow: true});
+  };
+
   const {columnName, displayName} = headerVariables;
   return (
-    <th onClick={() => sort('sortColumn', columnName)}>
+    <th onClick={() => triggerSort(columnName)}>
       <span>{displayName}</span>
       <span style={{height: '100%', position: 'absolute', right: '0.75em'}}>
-        {displayName && <FontAwesomeIcon color="white" icon={faSort} />}
+        {displayName && (
+          <FontAwesomeIcon
+            color="white"
+            icon={
+              router.query.orderByField === columnName
+                ? SORT_ICONS[currentSortDirection]
+                : faSort
+            }
+          />
+        )}
       </span>
       <style>{`
         .table thead th {
