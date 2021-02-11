6cc6077ba38f6544d8d2057c1a4c6e8284148aa5
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-02-10 17:22:04 -0800

    refactor: product list table no longer uses refetch container

--- app/containers/Products/ProductListContainer.tsx
+++ app/containers/Products/ProductListContainer.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect} from 'react';
-import {graphql, createRefetchContainer} from 'react-relay';
+import React from 'react';
+import {graphql, createFragmentContainer} from 'react-relay';
 import {ProductListContainer_query} from 'ProductListContainer_query.graphql';
 import SearchTableLayout from 'components/SearchTableLayout';
 import LoadingSpinner from 'components/LoadingSpinner';
@@ -15,31 +15,14 @@ interface Props {
   productCount: number;
   updateProductCount: (...args: any[]) => void;
   handleEvent: (...args: any[]) => void;
-  relay: any;
 }
 
 export const ProductList: React.FunctionComponent<Props> = ({
   query,
-  relay,
-  orderByField,
-  searchField,
-  searchValue,
-  direction,
   handleEvent,
   productCount,
   updateProductCount
 }) => {
-  useEffect(() => {
-    const refetchVariables = {
-      searchField,
-      searchValue,
-      orderByField,
-      direction,
-      productCount
-    };
-    relay.refetch(refetchVariables);
-  });
-
   if (query?.searchProducts?.edges) {
     const allProducts = query.searchProducts.edges;
 
@@ -82,59 +65,36 @@ export const ProductList: React.FunctionComponent<Props> = ({
 // we need the first two billion edges to force graphql to return the right type
 // @see https://relay.dev/docs/en/pagination-container#connection
 // https://www.prisma.io/blog/relay-moderns-connection-directive-1ecd8322f5c8
-export default createRefetchContainer(
-  ProductList,
-  {
-    query: graphql`
-      fragment ProductListContainer_query on Query
-      @argumentDefinitions(
-        searchField: {type: "String"}
-        searchValue: {type: "String"}
-        orderByField: {type: "String"}
-        direction: {type: "String"}
-        productCount: {type: "Int"}
-      ) {
-        ...ProductRowItemContainer_query
-        searchProducts(
-          first: 2147483647
-          searchField: $searchField
-          searchValue: $searchValue
-          orderByField: $orderByField
-          direction: $direction
-        ) @connection(key: "ProductListContainer_searchProducts") {
-          edges {
-            node {
-              id
-              ...ProductRowItemContainer_product
-            }
+export default createFragmentContainer(ProductList, {
+  query: graphql`
+    fragment ProductListContainer_query on Query
+    @argumentDefinitions(
+      searchField: {type: "String"}
+      searchValue: {type: "String"}
+      orderByField: {type: "String"}
+      direction: {type: "String"}
+      productCount: {type: "Int"}
+    ) {
+      ...ProductRowItemContainer_query
+      searchProducts(
+        first: 2147483647
+        searchField: $searchField
+        searchValue: $searchValue
+        orderByField: $orderByField
+        direction: $direction
+      ) @connection(key: "ProductListContainer_searchProducts") {
+        edges {
+          node {
+            id
+            ...ProductRowItemContainer_product
           }
         }
-        # TODO: This is here to trigger a refactor as updating the edge / running the query in the mutation is not triggering a refresh
-        # Find a way to not pull the totalcount?
-        allProducts(first: $productCount) {
-          totalCount
-        }
       }
-    `
-  },
-  graphql`
-    query ProductListContainerRefetchQuery(
-      $searchField: String
-      $searchValue: String
-      $orderByField: String
-      $direction: String
-      $productCount: Int
-    ) {
-      query {
-        ...ProductListContainer_query
-          @arguments(
-            searchField: $searchField
-            searchValue: $searchValue
-            orderByField: $orderByField
-            direction: $direction
-            productCount: $productCount
-          )
+      # TODO: This is here to trigger a refactor as updating the edge / running the query in the mutation is not triggering a refresh
+      # Find a way to not pull the totalcount?
+      allProducts(first: $productCount) {
+        totalCount
       }
     }
   `
-);
+});
