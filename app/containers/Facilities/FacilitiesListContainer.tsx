1cb2f8eb7ff41fc384be3aa7e223314677a71753
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-02-10 16:13:46 -0800

    feat: facilities list doesn't use a refetch container

--- app/containers/Facilities/FacilitiesListContainer.tsx
+++ app/containers/Facilities/FacilitiesListContainer.tsx
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import PaginationBar from 'components/PaginationBar';
-import {graphql, createRefetchContainer, RelayRefetchProp} from 'react-relay';
+import {graphql, createFragmentContainer} from 'react-relay';
 import SearchTableLayout from 'components/SearchTableLayout';
 import {FacilitiesListContainer_query} from 'FacilitiesListContainer_query.graphql';
 import FacilitiesRowItemContainer from './FacilitiesRowItemContainer';
@@ -9,23 +9,15 @@ import SelectReportingYearDropDown from 'components/SelectReportingYearDropdown'
 interface Props {
   direction: string;
   orderByField: string;
-  searchField: string;
-  searchValue: string;
   offsetValue: number;
   handleEvent: (...args: any[]) => void;
   selectedReportingYear: number;
   query: FacilitiesListContainer_query;
-  relay: RelayRefetchProp;
 }
 
 export const FacilitiesList: React.FunctionComponent<Props> = ({
-  direction,
-  orderByField,
-  searchField,
-  searchValue,
   handleEvent,
   selectedReportingYear,
-  relay,
   query
 }) => {
   const reportingYears = query.allReportingYears.edges;
@@ -37,37 +29,13 @@ export const FacilitiesList: React.FunctionComponent<Props> = ({
   const {edges} = query.searchAllFacilities;
   const [offsetValue, setOffset] = useState(0);
   const [activePage, setActivePage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
   const maxResultsPerPage = 10;
-  useEffect(() => {
-    const refetchVariables = {
-      searchField,
-      searchValue,
-      orderByField,
-      direction,
-      maxResultsPerPage,
-      offsetValue,
-      reportingYear: selectedReportingYear
-    };
-    setIsLoading(true);
-    relay.refetch(refetchVariables, undefined, () => {
-      setIsLoading(false);
-    });
-  }, [
-    searchField,
-    searchValue,
-    orderByField,
-    direction,
-    offsetValue,
-    relay,
-    selectedReportingYear
-  ]);
 
   const displayNameToColumnNameMap = {
     'Organisation Name': 'organisation_name',
     'Facility Name': 'facility_name',
     'Facility Type': 'facility_type',
-    'BC GHG id': 'facility_bcghgid',
+    'BC GHG id': 'bcghgid',
     'Reporting period of last SWRS report': 'last_swrs_reporting_year',
     Status: 'application_revision_status',
     '': null
@@ -103,7 +71,6 @@ export const FacilitiesList: React.FunctionComponent<Props> = ({
         body={body}
         displayNameToColumnNameMap={displayNameToColumnNameMap}
         handleEvent={handleEvent}
-        isLoading={isLoading}
         extraControls={extraControls}
       />
       <PaginationBar
@@ -120,74 +87,45 @@ export const FacilitiesList: React.FunctionComponent<Props> = ({
   );
 };
 
-export default createRefetchContainer(
-  FacilitiesList,
-  {
-    query: graphql`
-      fragment FacilitiesListContainer_query on Query
-      @argumentDefinitions(
-        searchField: {type: "String"}
-        searchValue: {type: "String"}
-        orderByField: {type: "String"}
-        direction: {type: "String"}
-        organisationRowId: {type: "String"}
-        offsetValue: {type: "Int"}
-        maxResultsPerPage: {type: "Int"}
-        reportingYear: {type: "Int"}
+export default createFragmentContainer(FacilitiesList, {
+  query: graphql`
+    fragment FacilitiesListContainer_query on Query
+    @argumentDefinitions(
+      searchField: {type: "String"}
+      searchValue: {type: "String"}
+      orderByField: {type: "String"}
+      direction: {type: "String"}
+      organisationRowId: {type: "String"}
+      offsetValue: {type: "Int"}
+      maxResultsPerPage: {type: "Int"}
+      reportingYear: {type: "Int"}
+    ) {
+      ...FacilitiesRowItemContainer_query
+      searchAllFacilities(
+        searchField: $searchField
+        searchValue: $searchValue
+        orderByField: $orderByField
+        organisationRowId: $organisationRowId
+        direction: $direction
+        offsetValue: $offsetValue
+        maxResultsPerPage: $maxResultsPerPage
+        reportingYear: $reportingYear
       ) {
-        ...FacilitiesRowItemContainer_query
-        searchAllFacilities(
-          searchField: $searchField
-          searchValue: $searchValue
-          orderByField: $orderByField
-          organisationRowId: $organisationRowId
-          direction: $direction
-          offsetValue: $offsetValue
-          maxResultsPerPage: $maxResultsPerPage
-          reportingYear: $reportingYear
-        ) {
-          edges {
-            node {
-              rowId
-              totalFacilityCount
-              ...FacilitiesRowItemContainer_facilitySearchResult
-            }
-          }
-        }
-        allReportingYears {
-          edges {
-            node {
-              reportingYear
-            }
+        edges {
+          node {
+            rowId
+            totalFacilityCount
+            ...FacilitiesRowItemContainer_facilitySearchResult
           }
         }
       }
-    `
-  },
-  graphql`
-    query FacilitiesListContainerRefetchQuery(
-      $searchField: String
-      $searchValue: String
-      $orderByField: String
-      $direction: String
-      $organisationRowId: String
-      $offsetValue: Int
-      $maxResultsPerPage: Int
-      $reportingYear: Int
-    ) {
-      query {
-        ...FacilitiesListContainer_query
-          @arguments(
-            orderByField: $orderByField
-            direction: $direction
-            searchField: $searchField
-            searchValue: $searchValue
-            organisationRowId: $organisationRowId
-            offsetValue: $offsetValue
-            maxResultsPerPage: $maxResultsPerPage
-            reportingYear: $reportingYear
-          )
+      allReportingYears {
+        edges {
+          node {
+            reportingYear
+          }
+        }
       }
     }
   `
-);
+});
