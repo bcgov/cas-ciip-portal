f04c9bdd3c888ae806b3a14dc7e455b701ed9842
Dylan Leard <dylan@button.is>
2021-02-10 17:23:00 -0800

    refactor: remove unnecessary computed columns

--- schema/deploy/computed_columns/application_bcghgid.sql
+++ schema/deploy/computed_columns/application_bcghgid.sql
@@ -6,7 +6,7 @@ begin;
 create or replace function ggircs_portal.application_bcghgid(app ggircs_portal.application)
   returns varchar
   as $$
-    select (ggircs_portal.application_facility(app::ggircs_portal.application)).bcghgid::varchar;
+    select bcghgid::varchar from ggircs_portal.facility where facility.id = app.facility_id;
   $$ language sql stable;
 
 comment on function ggircs_portal.application_bcghgid(ggircs_portal.application) is E'@sortable\nThis function is a wrapper to return bcghgid as a scalar from the composite return of the application_facility computed column';
