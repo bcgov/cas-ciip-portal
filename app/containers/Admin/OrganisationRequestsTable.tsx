80c0292e261bf875d5978fe0d6e77ddee0b7cbb4
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-02-10 17:19:17 -0800

    refactor: organisation request table no longer uses refetch container

--- app/containers/Admin/OrganisationRequestsTable.tsx
+++ app/containers/Admin/OrganisationRequestsTable.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect} from 'react';
-import {graphql, createRefetchContainer, RelayRefetchProp} from 'react-relay';
+import React from 'react';
+import {graphql, createFragmentContainer} from 'react-relay';
 import {OrganisationRequestsTable_query} from 'OrganisationRequestsTable_query.graphql';
 import SearchTableLayout from 'components/SearchTableLayout';
 import OrganisationRequestsTableRow from './OrganisationRequestsTableRow';
@@ -12,18 +12,11 @@ interface Props {
   direction?: string;
   searchDisplay?: string;
   handleEvent: (...args: any[]) => void;
-  relay: RelayRefetchProp;
 }
 export const OrganisationRequestsTableComponent: React.FunctionComponent<Props> = (
   props
 ) => {
-  const {
-    orderByField,
-    searchField,
-    searchValue,
-    direction,
-    handleEvent
-  } = props;
+  const {handleEvent} = props;
 
   const displayNameToColumnNameMap = {
     'User ID': 'user_id',
@@ -36,15 +29,7 @@ export const OrganisationRequestsTableComponent: React.FunctionComponent<Props> 
   };
 
   const {edges} = props.query.searchCiipUserOrganisation;
-  useEffect(() => {
-    const refetchVariables = {
-      searchField,
-      searchValue,
-      orderByField,
-      direction
-    };
-    props.relay.refetch(refetchVariables);
-  });
+
   const body = (
     <tbody>
       {edges.map((edge) => (
@@ -64,49 +49,28 @@ export const OrganisationRequestsTableComponent: React.FunctionComponent<Props> 
   );
 };
 
-export default createRefetchContainer(
-  OrganisationRequestsTableComponent,
-  {
-    query: graphql`
-      fragment OrganisationRequestsTable_query on Query
-      @argumentDefinitions(
-        searchField: {type: "String"}
-        searchValue: {type: "String"}
-        orderByField: {type: "String"}
-        direction: {type: "String"}
+export default createFragmentContainer(OrganisationRequestsTableComponent, {
+  query: graphql`
+    fragment OrganisationRequestsTable_query on Query
+    @argumentDefinitions(
+      searchField: {type: "String"}
+      searchValue: {type: "String"}
+      orderByField: {type: "String"}
+      direction: {type: "String"}
+    ) {
+      searchCiipUserOrganisation(
+        searchField: $searchField
+        searchValue: $searchValue
+        orderByField: $orderByField
+        direction: $direction
       ) {
-        searchCiipUserOrganisation(
-          searchField: $searchField
-          searchValue: $searchValue
-          orderByField: $orderByField
-          direction: $direction
-        ) {
-          edges {
-            node {
-              id
-              ...OrganisationRequestsTableRow_userOrganisation
-            }
+        edges {
+          node {
+            id
+            ...OrganisationRequestsTableRow_userOrganisation
           }
         }
       }
-    `
-  },
-  graphql`
-    query OrganisationRequestsTableRefetchQuery(
-      $searchField: String
-      $searchValue: String
-      $orderByField: String
-      $direction: String
-    ) {
-      query {
-        ...OrganisationRequestsTable_query
-          @arguments(
-            searchField: $searchField
-            searchValue: $searchValue
-            orderByField: $orderByField
-            direction: $direction
-          )
-      }
     }
   `
-);
+});
