b26f114dcb4cc72c56695b22037f351206943575
Dylan Leard <dylan@button.is>
2021-02-10 17:23:00 -0800

    refactor: remove unnecessary computed columns

--- schema/verify/computed_columns/application_operator.sql
+++
@@ -1,7 +0,0 @@
--- Verify ggircs-portal:computed_columns/application_operator on pg
-
-begin;
-
-select pg_get_functiondef('ggircs_portal.application_operator(ggircs_portal.application)'::regprocedure);
-
-rollback;
