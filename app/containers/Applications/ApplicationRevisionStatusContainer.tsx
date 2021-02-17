48c2d5bf432074beb29743bb9e658859232239aa
Dylan Leard <dylan@button.is>
2021-02-17 15:18:18 -0800

    fix: remove import from search function

--- app/containers/Applications/ApplicationRevisionStatusContainer.tsx
+++ app/containers/Applications/ApplicationRevisionStatusContainer.tsx
@@ -3,7 +3,7 @@ import {Row, Col, Dropdown, Button, Badge, Modal} from 'react-bootstrap';
 import {graphql, createFragmentContainer, RelayProp} from 'react-relay';
 import DropdownMenuItemComponent from 'components/DropdownMenuItemComponent';
 import createApplicationRevisionStatusMutation from 'mutations/application/createApplicationRevisionStatusMutation';
-import {CiipApplicationRevisionStatus} from 'ApplicationRowItemContainer_applicationSearchResult.graphql';
+import {CiipApplicationRevisionStatus} from 'createApplicationRevisionStatusMutation.graphql';
 
 interface Props {
   applicationRevisionStatus;
