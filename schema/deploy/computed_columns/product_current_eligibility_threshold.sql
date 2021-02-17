2ed195d1d2530f5b25b40ca76a34455c92d247fa
Dylan Leard <dylan@button.is>
2021-02-17 13:56:03 -0800

    feat: add computed columns for benchnmark/e. threshold

---
+++ schema/deploy/computed_columns/product_current_eligibility_threshold.sql
@@ -0,0 +1,16 @@
+-- Deploy ggircs-portal:computed_columns/product_current_eligibility_threshold to pg
+-- requires: tables/benchmark
+
+begin;
+
+create or replace function ggircs_portal.product_current_eligibility_threshold(prod ggircs_portal.product)
+  returns numeric
+  as $$
+    select eligibility_threshold from ggircs_portal.benchmark b
+    where b.product_id = prod.id
+    order by b.created_at desc limit 1;
+  $$ language sql stable;
+
+comment on function ggircs_portal.product_current_eligibility_threshold(ggircs_portal.product) is E'@sortable\nComputed column returns the current eligibility_threshold value for the product passed in via function parameter';
+
+commit;
