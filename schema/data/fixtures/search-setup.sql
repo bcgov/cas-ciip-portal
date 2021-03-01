7c1b71d88baaa1b38ef52da000357513057c56f5
Dylan Leard <dylan@button.is>
2021-03-01 14:56:30 -0800

    test: update fixture to create application statuses on an interval

--- schema/data/fixtures/search-setup.sql
+++ schema/data/fixtures/search-setup.sql
@@ -11,11 +11,13 @@ select test_helper.create_test_users();
 select test_helper.create_applications(4, False, True);
 
 -- Modify application #1-3 to be 'submitted', 4 stays in draft
-insert into ggircs_portal.application_revision_status(application_id, version_number, application_revision_status)
-  values (1,1,'submitted');
-insert into ggircs_portal.application_revision_status(application_id, version_number, application_revision_status)
-  values (2,1,'submitted');
-insert into ggircs_portal.application_revision_status(application_id, version_number, application_revision_status)
-  values (3,1,'approved');
+insert into ggircs_portal.application_revision_status(application_id, version_number, application_revision_status, created_at)
+  values (1,1,'submitted', now() + interval '1 minute');
+insert into ggircs_portal.application_revision_status(application_id, version_number, application_revision_status, created_at)
+  values (2,1,'submitted', now() + interval '1 minute');
+insert into ggircs_portal.application_revision_status(application_id, version_number, application_revision_status, created_at)
+  values (3,1,'approved', now() + interval '1 minute');
+
+select test_helper.modify_triggers('enable');
 
 commit;
