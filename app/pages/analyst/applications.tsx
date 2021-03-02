20cb75f47b895678e815e7ea061461638902c971
Dylan Leard <dylan@button.is>
2021-03-02 15:29:33 -0800

    refactor: remove SearchTable from applications page

--- app/pages/analyst/applications.tsx
+++ app/pages/analyst/applications.tsx
@@ -3,7 +3,6 @@ import {graphql} from 'react-relay';
 import {applicationsQueryResponse} from 'applicationsQuery.graphql';
 import ApplicationListContainer from 'containers/Applications/ApplicationListContainer';
 import DefaultLayout from 'layouts/default-layout';
-import SearchTable from 'components/SearchTable';
 import {INCENTIVE_ANALYST, ADMIN_GROUP} from 'data/group-constants';
 
 const ALLOWED_GROUPS = [INCENTIVE_ANALYST, ...ADMIN_GROUP];
@@ -15,9 +14,6 @@ interface Props {
 class Applications extends Component<Props> {
   static allowedGroups = ALLOWED_GROUPS;
   static isAccessProtected = true;
-  // In downstream component
-  // onClick={(event) => handleApplicationsAction({action: Applications.SORT_ACTION})})
-  // I didn't fully understand how to make this work, so I moved on with the way I did.
   static query = graphql`
     query applicationsQuery(
       $id: Int
@@ -27,6 +23,10 @@ class Applications extends Component<Props> {
       $submission_date: Datetime
       $status: CiipApplicationRevisionStatus
       $order_by: [ApplicationsOrderBy!]
+      $after_cursor: Cursor
+      $before_cursor: Cursor
+      $num_forward: Int
+      $num_back: Int
     ) {
       query {
         session {
@@ -41,6 +41,10 @@ class Applications extends Component<Props> {
             submission_date: $submission_date
             status: $status
             order_by: $order_by
+            after_cursor: $after_cursor
+            before_cursor: $before_cursor
+            num_forward: $num_forward
+            num_back: $num_back
           )
       }
     }
@@ -50,7 +54,8 @@ class Applications extends Component<Props> {
     return {
       variables: {
         orderByField: 'operator_name',
-        direction: 'ASC'
+        direction: 'ASC',
+        num_forward: 10
       }
     };
   }
@@ -59,13 +64,7 @@ class Applications extends Component<Props> {
     const {query} = this.props;
     return (
       <DefaultLayout title="Applications" session={query.session}>
-        <SearchTable
-          query={query}
-          defaultOrderByField="operator_name"
-          defaultOrderByDisplay="Operator Name"
-        >
-          {(props) => <ApplicationListContainer {...props} />}
-        </SearchTable>
+        <ApplicationListContainer query={query} />
       </DefaultLayout>
     );
   }
