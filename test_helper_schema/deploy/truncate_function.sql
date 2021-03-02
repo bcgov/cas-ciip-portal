ad9ce7c34d449416e0c095c8cbf806e2326e3fe7
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-03-01 16:15:35 -0800

    refactor: test truncate function is in one transaction

--- test_helper_schema/deploy/truncate_function.sql
+++ test_helper_schema/deploy/truncate_function.sql
@@ -6,18 +6,14 @@ BEGIN;
 create or replace function test_helper.clean_ggircs_portal_schema()
 returns void as
 $function$
-  declare
-    table_name text;
-	truncate_statement text;
+
   begin
-      for table_name in
-          select pg_tables.tablename as table_name from pg_tables where schemaname = 'ggircs_portal'
-      loop
-          raise notice 'Truncating %', table_name;
-          truncate_statement:= format('truncate table ggircs_portal.%I restart identity cascade', table_name);
-          execute truncate_statement;
-      end loop;
-  end;
+    execute (select 'TRUNCATE TABLE '
+    || string_agg(format('ggircs_portal.%I', tablename), ', ')
+    || ' RESTART IDENTITY CASCADE'
+    from pg_tables
+    where schemaname = 'ggircs_portal');
+  end
 
 $function$ language plpgsql volatile;
 
