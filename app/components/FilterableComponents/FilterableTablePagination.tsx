57d400594edbb1f331f46ddf8b98549fed4c7624
Dylan Leard <dylan@button.is>
2021-03-05 10:38:15 -0800

    feat: add max_results selection dropdown

--- app/components/FilterableComponents/FilterableTablePagination.tsx
+++ app/components/FilterableComponents/FilterableTablePagination.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useRouter} from 'next/router';
-import {Pagination} from 'react-bootstrap';
+import {Pagination, Dropdown} from 'react-bootstrap';
 
 interface Props {
   totalCount: number;
@@ -24,24 +24,40 @@ const FilterableTablePaginationComponent: React.FunctionComponent<Props> = (
 
   const pageData = router.query.pageVars ? getPageData() : {};
 
-  const maxResultsPerPage = pageData?.maxResultsPerPage || DEFAULT_MAX_RESULTS;
+  const maxResultsPerPage = pageData?.max_results || DEFAULT_MAX_RESULTS;
   const activePage = pageData?.offset / maxResultsPerPage + 1 || 1;
   const maxPages = Math.ceil(totalCount / maxResultsPerPage);
 
   const items = [];
 
-  const handlePageChange = (pageNumber: number) => {
-    const paginationObject = {
-      offset: (pageNumber - 1) * maxResultsPerPage
-    };
+  const setQueryString = (toUrlObject: {
+    offset: number;
+    max_results: number;
+  }) => {
     const url = {
       pathname: router.pathname,
       query: {
         ...router.query,
-        pageVars: JSON.stringify(paginationObject)
+        pageVars: JSON.stringify(toUrlObject)
       }
     };
     router.push(url, url, {shallow: true});
+  };
+
+  const handlePageChange = (pageNumber: number) => {
+    const paginationObject = {
+      offset: (pageNumber - 1) * maxResultsPerPage,
+      max_results: pageData.max_results || DEFAULT_MAX_RESULTS
+    };
+    setQueryString(paginationObject);
+  };
+
+  const handleMaxResultsChange = (numResults: number) => {
+    const resultsObject = {
+      offset: 0,
+      max_results: numResults
+    };
+    setQueryString(resultsObject);
   };
 
   // Determines what page numbers to render based on the location of the activePage
@@ -73,27 +89,55 @@ const FilterableTablePaginationComponent: React.FunctionComponent<Props> = (
       </Pagination.Item>
     );
   }
-  if (maxPages > 1)
-    return (
-      <Pagination>
-        <Pagination.First onClick={() => handlePageChange(1)} />
-        <Pagination.Prev
-          onClick={() =>
-            activePage > 1 ? handlePageChange(activePage - 1) : null
-          }
-        />
-        {startPage !== 1 && <Pagination.Ellipsis />}
-        {items}
-        {endPage !== maxPages && <Pagination.Ellipsis />}
-        <Pagination.Next
-          onClick={() =>
-            activePage !== maxPages ? handlePageChange(activePage + 1) : null
-          }
-        />
-        <Pagination.Last onClick={() => handlePageChange(maxPages)} />
-      </Pagination>
-    );
-  return null;
+  return (
+    <>
+      <div className="pagination">
+        <Dropdown>
+          <Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
+            Results Per Page: <strong>{maxResultsPerPage}</strong>
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item onClick={() => handleMaxResultsChange(2)}>
+              20
+            </Dropdown.Item>
+            <Dropdown.Item onClick={() => handleMaxResultsChange(5)}>
+              50
+            </Dropdown.Item>
+            <Dropdown.Item onClick={() => handleMaxResultsChange(10)}>
+              100
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        {maxPages > 1 && (
+          <Pagination style={{marginLeft: '15px'}}>
+            <Pagination.First onClick={() => handlePageChange(1)} />
+            <Pagination.Prev
+              onClick={() =>
+                activePage > 1 ? handlePageChange(activePage - 1) : null
+              }
+            />
+            {startPage !== 1 && <Pagination.Ellipsis />}
+            {items}
+            {endPage !== maxPages && <Pagination.Ellipsis />}
+            <Pagination.Next
+              onClick={() =>
+                activePage !== maxPages
+                  ? handlePageChange(activePage + 1)
+                  : null
+              }
+            />
+            <Pagination.Last onClick={() => handlePageChange(maxPages)} />
+          </Pagination>
+        )}
+      </div>
+      <style jsx>{`
+        .pagination {
+          display: flex;
+        }
+      `}</style>
+    </>
+  );
 };
 
 export default FilterableTablePaginationComponent;
