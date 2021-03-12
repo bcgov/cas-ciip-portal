471ead14a2b9ecda6f5758adc287d4440bcbd17d
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-03-11 16:34:56 -0800

    feat: helper to verify that a function is not present

---
+++ schema/deploy/database_functions/verify_function_not_present.sql
@@ -0,0 +1,23 @@
+-- Deploy ggircs-portal:database_functions/verify_function_not_present to pg
+
+BEGIN;
+
+create or replace function ggircs_portal_private.verify_function_not_present(function_name text)
+returns boolean
+as
+$function$
+
+  begin
+
+    if (select exists(select * from pg_proc where proname=function_name)) then
+      raise exception '% exists when it should not', function_name;
+    else
+      perform true;
+    end if;
+
+  end;
+
+$function$
+language 'plpgsql' stable;
+
+COMMIT;
