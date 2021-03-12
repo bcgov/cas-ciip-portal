b946f30a89ef0fa4aaacc8494a68381d5c31dd00
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-03-12 14:23:51 -0800

    feat: removing type search_certification_url_result

--- schema/verify/types/search_certification_url_result.sql
+++ schema/verify/types/search_certification_url_result.sql
@@ -2,12 +2,6 @@
 
 begin;
 
-do $$
-  begin
-    assert (
-      select true from pg_catalog.pg_type where typname = 'search_certification_url_result'
-    ), 'type "search_certification_url_result" is not defined';
-  end;
-$$;
+select ggircs_portal_private.verify_type_not_present('search_certification_url_result');
 
 rollback;
