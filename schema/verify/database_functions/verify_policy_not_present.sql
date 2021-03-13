246089d766e17292806e8ea28ca3e64bf19bcb72
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-03-12 16:24:23 -0800

    feat: function to verify that a policy is absent

---
+++ schema/verify/database_functions/verify_policy_not_present.sql
@@ -0,0 +1,7 @@
+-- Verify ggircs-portal:database_functions/verify_policy_not_present on pg
+
+BEGIN;
+
+select pg_get_functiondef('ggircs_portal_private.ggircs_portal_private.verify_policy_not_present(text,text)'::regprocedure);
+
+ROLLBACK;
