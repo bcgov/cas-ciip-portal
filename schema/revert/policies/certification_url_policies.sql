0bd67c45aebc6aec67b104805f02efb147f8f468
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-03-17 15:24:56 -0700

    fix: revert on certification url policies

--- schema/revert/policies/certification_url_policies.sql
+++ schema/revert/policies/certification_url_policies.sql
@@ -9,6 +9,8 @@ $policy$
     certifier_user_statement text;
   begin
 
+    certifier_user_statement := 'certifier_email = ((select email_address from ggircs_portal.ciip_user where ciip_user.uuid = (select sub from ggircs_portal.session())))';
+
     -- ciip_industry_user (certifier) RLS
     perform ggircs_portal_private.upsert_policy('certifier_select_certification_url', 'certification_url', 'select', 'ciip_industry_user', certifier_user_statement);
     perform ggircs_portal_private.upsert_policy('certifier_update_certification_url', 'certification_url', 'update', 'ciip_industry_user', certifier_user_statement);
