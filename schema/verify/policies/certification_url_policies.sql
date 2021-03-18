cfc17e3da9629314d04f02a7785bb36c2e85fce9
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-03-18 15:13:28 -0700

    fix: using database function to verify that the policy is not present

--- schema/verify/policies/certification_url_policies.sql
+++ schema/verify/policies/certification_url_policies.sql
@@ -2,30 +2,8 @@
 
 begin;
 
--- Verifies if a policy doesn't exist
-create or replace function throw_if_policy_exists(operation text, policy_name text, table_name_input text, role_name text)
-returns boolean
-as
-$function$
-  declare
-    return_value boolean := false;
-  begin
-    if (ggircs_portal_private.verify_policy(operation, policy_name, table_name_input, role_name) = true) then
-      return_value := true;
-      raise exception 'the policy % was found', policy_name;
-    end if;
-  exception when others then
-    if(return_value) then
-      raise exception 'Policy % was found when it shouldnt have', operation;
-    end if;
-
-    return true;
-  end;
-$function$
-language 'plpgsql' stable;
-
 -- ciip_industry_user (certifier) Policies
-select throw_if_policy_exists('select', 'certifier_select_certification_url', 'certification_url', 'ciip_industry_user');
-select throw_if_policy_exists('update', 'certifier_update_certification_url', 'certification_url', 'ciip_industry_user');
+select ggircs_portal_private.verify_policy_not_present('certifier_select_certification_url', 'ggircs_portal.certification_url');
+select ggircs_portal_private.verify_policy_not_present('certifier_update_certification_url', 'ggircs_portal.certification_url');
 
 rollback;
