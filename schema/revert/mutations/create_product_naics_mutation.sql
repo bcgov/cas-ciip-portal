ad267ada6bb965eaf49af4d36157d25d52e90226
Dylan Leard <dylan@button.is>
2021-03-19 13:17:43 -0700

    feat: add custom upsert mutation

---
+++ schema/revert/mutations/create_product_naics_mutation.sql
@@ -0,0 +1,7 @@
+-- Revert ggircs-portal:mutations/create_product_naics_mutation from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ schema/verify/mutations/create_product_naics_mutation.sql
@@ -0,0 +1,7 @@
+-- Verify ggircs-portal:mutations/create_product_naics_mutation on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
