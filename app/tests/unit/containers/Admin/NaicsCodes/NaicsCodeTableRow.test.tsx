4e7b9238e8f0761c9101f205fb208d2886965cbe
Dylan Leard <dylan@button.is>
2021-03-24 14:08:04 -0700

    test: add confirmation modal tests

--- app/tests/unit/containers/Admin/NaicsCodes/NaicsCodeTableRow.test.tsx
+++ app/tests/unit/containers/Admin/NaicsCodes/NaicsCodeTableRow.test.tsx
@@ -20,4 +20,64 @@ describe('NaicsCodeTable', () => {
     );
     expect(renderer).toMatchSnapshot();
   });
+  it('should open the confimation modal delete button click', async () => {
+    const renderer = shallow(
+      <NaicsCodeTableRowContainer
+        relay={null}
+        connectionId="connection"
+        naicsCode={{
+          ' $refType': 'NaicsCodeTableRow_naicsCode',
+          id: 'abc',
+          naicsCode: 'code',
+          ciipSector: 'sector',
+          naicsDescription: 'description',
+          deletedAt: null
+        }}
+      />
+    );
+
+    renderer.find('td').at(3).find('Button').simulate('click');
+
+    expect(renderer.find('Modal').prop('show')).toBe(true);
+  });
+
+  it('should call the update mutation on confirmation', async () => {
+    const spy = jest
+      .spyOn(require('mutations/naics_code/updateNaicsCodeMutation'), 'default')
+      .mockImplementation(() => null);
+    const relay = {environment: null};
+    const renderer = shallow(
+      <NaicsCodeTableRowContainer
+        relay={relay as any}
+        connectionId="connection"
+        naicsCode={{
+          ' $refType': 'NaicsCodeTableRow_naicsCode',
+          id: 'abc',
+          naicsCode: 'code',
+          ciipSector: 'sector',
+          naicsDescription: 'description',
+          deletedAt: null
+        }}
+      />
+    );
+
+    renderer.find('Modal').find('Button').prop('onClick')({} as any);
+
+    expect(spy).toBeCalledTimes(1);
+    expect(spy).toBeCalledWith(
+      null,
+      {
+        input: {
+          id: 'abc',
+          naicsCodePatch: {
+            ciipSector: 'sector',
+            deletedAt: expect.any(String),
+            naicsCode: 'code',
+            naicsDescription: 'description'
+          }
+        }
+      },
+      'connection'
+    );
+  });
 });
