521eb662b0c390cd714755271a11bd38107376f1
Dylan Leard <dylan@button.is>
2021-04-01 08:10:22 -0700

    test: result type pgTap test

---
+++ schema/test/unit/types/application_validation_result_test.sql
@@ -0,0 +1,14 @@
+set client_min_messages to warning;
+create extension if not exists pgtap;
+reset client_min_messages;
+
+begin;
+select plan(1);
+
+select has_type(
+  'ggircs_portal', 'application_validation_result', 'Type application_validation_result should exist'
+);
+
+select finish();
+
+rollback;
