7190466c0b1ec4c7051e060bf1c2039211ed1db1
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-04-06 14:46:28 -0700

    feat: scrollable naics list

--- app/components/Admin/NaicsCodeSelectionComponent.tsx
+++ app/components/Admin/NaicsCodeSelectionComponent.tsx
@@ -13,34 +13,47 @@ export const NaicsCodeSelectionComponent: React.FunctionComponent<Props> = ({
   const router = useRouter();
 
   return (
-    <ListGroup variant="flush">
-      {naicsCodes.map(({code, description, id}) => (
-        <Link
-          key={id}
-          prefetch={false}
-          href={{
-            pathname: router.pathname,
-            query: {
-              ...router.query,
-              naicsCodeId: id
-            }
-          }}
-        >
-          <ListGroup.Item action active={router.query?.naicsCodeId === id}>
-            {/* <table>
-              <tbody>
-                <tr>
-                  <td>{code}</td>
-                  <td style={{paddingLeft: '1em'}}>{description}</td>
-                </tr>
-              </tbody>
-            </table> */}
-            {description}
-            <br />
-            <b>{code}</b>
-          </ListGroup.Item>
-        </Link>
-      ))}
-    </ListGroup>
+    <>
+      <div className="scrollable" tabIndex={0}>
+        <ListGroup variant="flush">
+          {naicsCodes.map(({code, description, id}) => (
+            <Link
+              key={id}
+              prefetch={false}
+              href={{
+                pathname: router.pathname,
+                query: {
+                  ...router.query,
+                  naicsCodeId: id
+                }
+              }}
+            >
+              <ListGroup.Item action active={router.query?.naicsCodeId === id}>
+                {description}
+                <br />
+                <b>{code}</b>
+              </ListGroup.Item>
+            </Link>
+          ))}
+        </ListGroup>
+      </div>
+      <style jsx>
+        {`
+          .scrollable {
+            overflow-y: scroll;
+            max-height: calc(100vh - 108px - 60px - 2.4rem - 146px);
+          }
+          .scrollable::-webkit-scrollbar {
+            -webkit-appearance: none;
+            width: 8px;
+          }
+          .scrollable::-webkit-scrollbar-thumb {
+            border-radius: 4px;
+            background-color: rgba(0, 0, 0, 0.5);
+            box-shadow: 0 0 1px rgba(255, 255, 255, 0.5);
+          }
+        `}
+      </style>
+    </>
   );
 };
