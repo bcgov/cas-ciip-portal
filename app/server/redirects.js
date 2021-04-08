695a3185b17cfd7ada972910cf4a3924cf51c9d8
Matthieu Foucault <matthieu@button.is>
2021-04-08 10:41:06 -0700

    fix(security): do not trust user input when redirecting

--- app/server/redirects.js
+++ app/server/redirects.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const redirectRouter = express.Router();
+
+const base64Decode = (str) =>
+  Buffer.from(String(str), 'base64').toString('utf8');
+
+const isValidApplicationId = (applicationId) => {
+  try {
+    const idParts = JSON.parse(base64Decode(applicationId));
+    return (
+      Array.isArray(idParts) &&
+      idParts.length === 2 &&
+      idParts[0] === 'applications' &&
+      typeof idParts[1] === 'number'
+    );
+  } catch (e) {
+    return false;
+  }
+};
+
 redirectRouter.get('/reporter/view-application', (req, res) => {
   const {applicationId, version} = req.query;
-  if (!applicationId || !version) {
+  if (
+    !applicationId ||
+    !version ||
+    !isValidApplicationId(applicationId) ||
+    Number.isNaN(Number(version))
+  ) {
     res.redirect('/404');
     return;
   }
