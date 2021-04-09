bc41e434b59db7de86b379ab3ca10b95b368192a
Dylan Leard <dylan@button.is>
2021-04-09 14:36:07 -0700

    refactor: moved application_review_step data out of comment migration

--- schema/deploy/tables/review_comment_001.sql
+++ schema/deploy/tables/review_comment_001.sql
@@ -8,27 +8,24 @@ alter table ggircs_portal.review_comment add column application_review_step_id i
 create index ggircs_portal_review_comment_app_review_step_foreign_key on ggircs_portal.review_comment(application_review_step_id);
 comment on column ggircs_portal.review_comment.application_review_step_id is 'Foreign key to application_review_step. Defines the review step that this comment belongs to.';
 
-create or replace function ggircs_portal_private.create_review_steps()
+create or replace function ggircs_portal_private.connect_comments_to_review_step()
   returns void as $$
 declare
   temp_row record;
   temp_id int;
 begin
     for temp_row in
-      select id from ggircs_portal.application
+      select * from ggircs_portal.application_review_step
     loop
-      insert into ggircs_portal.application_review_step(application_id, review_step_id)
-      values (temp_row.id, 1)
-      on conflict("application_id", "review_step_id") do nothing returning id into temp_id;
-      update ggircs_portal.review_comment rc set application_review_step_id = temp_id where rc.application_id = temp_row.id and application_review_step_id is null;
+      update ggircs_portal.review_comment rc set application_review_step_id = temp_row.id where rc.application_id = temp_row.application_id and rc.application_review_step_id is null;
     end loop;
 end;
 $$ language plpgsql volatile;
 
 -- Data migration: add comments with null application_review_step_id to an application_review_step
-select ggircs_portal_private.create_review_steps();
+select ggircs_portal_private.connect_comments_to_review_step();
 
-drop function ggircs_portal_private.create_review_steps;
+drop function ggircs_portal_private.connect_comments_to_review_step;
 
 alter table ggircs_portal.review_comment alter column application_review_step_id set not null;
 
