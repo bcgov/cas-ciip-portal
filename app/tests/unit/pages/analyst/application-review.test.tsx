7eac526b6c81338e1057920a5261e7a803e79f03
Kristen Cooke <kristen@button.is>
2021-04-08 17:37:49 -0700

    refactor: write test query once, and selectively customize when needed

--- app/tests/unit/pages/analyst/application-review.test.tsx
+++ app/tests/unit/pages/analyst/application-review.test.tsx
@@ -3,8 +3,8 @@ import {shallow} from 'enzyme';
 import ApplicationReview from 'pages/analyst/application-review';
 import {applicationReviewQueryResponse} from 'applicationReviewQuery.graphql';
 
-describe('The application-review page', () => {
-  const query: applicationReviewQueryResponse['query'] = {
+const getTestQuery = () => {
+  return {
     session: {
       ' $fragmentRefs': {
         defaultLayout_session: true
@@ -42,14 +42,28 @@ describe('The application-review page', () => {
       ApplicationDetailsContainer_query: true
     }
   };
+};
 
+describe('The application-review page', () => {
   it('matches the last accepted Snapshot', () => {
-    const wrapper = shallow(<ApplicationReview router={null} query={query} />);
+    const query = getTestQuery();
+    const wrapper = shallow(
+      <ApplicationReview
+        router={null}
+        query={query as applicationReviewQueryResponse['query']}
+      />
+    );
     expect(wrapper).toMatchSnapshot();
   });
 
   it('passes the applicationRevision prop to the IncentiveCalculator', () => {
-    const wrapper = shallow(<ApplicationReview router={null} query={query} />);
+    const query = getTestQuery();
+    const wrapper = shallow(
+      <ApplicationReview
+        router={null}
+        query={query as applicationReviewQueryResponse['query']}
+      />
+    );
     expect(
       wrapper
         .find('Relay(IncentiveCalculator)')
@@ -59,46 +73,20 @@ describe('The application-review page', () => {
   });
 
   it('renders the ApplicationOverrideNotification component if an override has been set', () => {
-    const overrideQuery: applicationReviewQueryResponse['query'] = {
-      session: {
-        ' $fragmentRefs': {
-          defaultLayout_session: true
-        }
-      },
-      application: {
-        ' $fragmentRefs': {
-          ApplicationDetailsContainer_application: true
-        },
-        rowId: 1,
-        reviewRevisionStatus: {
-          ' $fragmentRefs': {
-            ApplicationRevisionStatusContainer_applicationRevisionStatus: true
-          }
-        },
-        applicationReviewStepsByApplicationId: {
-          edges: [
-            {
-              node: {
-                ' $fragmentRefs': {
-                  ReviewSidebar_applicationReviewStep: true
-                }
-              }
-            }
-          ]
-        }
-      },
+    const overrideQuery = {
+      ...getTestQuery(),
       applicationRevision: {
         overrideJustification: 'oops',
         ' $fragmentRefs': {
           IncentiveCalculatorContainer_applicationRevision: true
         }
-      },
-      ' $fragmentRefs': {
-        ApplicationDetailsContainer_query: true
       }
     };
     const wrapper = shallow(
-      <ApplicationReview router={null} query={overrideQuery} />
+      <ApplicationReview
+        router={null}
+        query={overrideQuery as applicationReviewQueryResponse['query']}
+      />
     );
     expect(
       wrapper.find('ApplicationOverrideNotification').props()
