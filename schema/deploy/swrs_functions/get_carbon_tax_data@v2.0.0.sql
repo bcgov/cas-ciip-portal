1fe295100b77bb95b8ac72ff1da67207bcc3d185
Dylan Leard <dylan@button.is>
2021-04-15 10:56:25 -0700

    refactor: update function to use new fkey

--- schema/deploy/swrs_functions/get_carbon_tax_data@v2.0.0.sql
+++ schema/deploy/swrs_functions/get_carbon_tax_data@v2.0.0.sql
@@ -7,6 +7,17 @@
 
 begin;
 
+  create type ggircs_portal.carbon_tax_data as (
+      fuel_mapping_id int,
+      carbon_taxed boolean,
+      cta_rate_units varchar(1000),
+      unit_conversion_factor int,
+      fuel_charge numeric,
+      rate_start_date date,
+      rate_end_date date,
+      carbon_tax_rate numeric
+  );
+
   create or replace function ggircs_portal.get_carbon_tax_data()
   returns setof ggircs_portal.carbon_tax_data
   as
