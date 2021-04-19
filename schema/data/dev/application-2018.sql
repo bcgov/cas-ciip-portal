91eac52f58695e7a57964c94701d42034717dde0
Dylan Leard <dylan@button.is>
2021-04-19 14:26:13 -0700

    refactor: do not hardcode id in application-2018.sql fixture

--- schema/data/dev/application-2018.sql
+++ schema/data/dev/application-2018.sql
@@ -22,17 +22,25 @@ begin;
   select ggircs_portal.create_application_mutation_chain(2);
   select ggircs_portal.create_application_mutation_chain(3);
 
-  -- Set application submitted
-  update ggircs_portal.application_revision_status set application_revision_status='submitted' where application_id=3 and version_number=1;
-  -- Create a legacy application_review_step for the 2018 application with ID=3
-  insert into ggircs_portal.application_review_step(application_id, review_step_id) values (3,1);
-  -- Create a comment
-  insert into ggircs_portal.review_comment(description, comment_type, application_review_step_id)
-    values ('This is a comment','general', (select id from ggircs_portal.application_review_step where application_id=3));
-  update ggircs_portal.application_revision_status set application_revision_status='requested changes' where application_id=3 and version_number=1;
+  do $$
+    declare app_id integer;
+    begin
+      app_id = (select id from ggircs_portal.application where reporting_year=2018 and facility_id=2);
 
-  -- Set legal_disclaimer_accepted to true for application id=3
-  update ggircs_portal.application_revision set legal_disclaimer_accepted = true where application_id=3;
+      -- Set application submitted
+      update ggircs_portal.application_revision_status set application_revision_status='submitted' where application_id=app_id and version_number=1;
+      -- Create a legacy application_review_step for the 2018 application with ID=3
+      insert into ggircs_portal.application_review_step(application_id, review_step_id) values (app_id,1);
+      -- Create a comment
+      insert into ggircs_portal.review_comment(description, comment_type, application_review_step_id)
+        values ('This is a comment','general', (select id from ggircs_portal.application_review_step where application_id=app_id));
+      update ggircs_portal.application_revision_status set application_revision_status='requested changes' where application_id=app_id and version_number=1;
+
+      -- Set legal_disclaimer_accepted to true for application id=3
+      update ggircs_portal.application_revision set legal_disclaimer_accepted = true where application_id=app_id;
+
+    end;
+  $$;
 
 
   alter table ggircs_portal.application_revision_status enable trigger _status_change_email;
