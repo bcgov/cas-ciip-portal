9dfd397a258542d55b2f1936e8a4819afc724d8b
Naomi Aro <naomi.aro@gov.bc.ca>
2021-04-19 16:35:53 -0700

    fix: update contact us page email locations

--- app/pages/resources/contact.tsx
+++ app/pages/resources/contact.tsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import {Container} from 'react-bootstrap';
+import {Container, Alert} from 'react-bootstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faInfoCircle} from '@fortawesome/free-solid-svg-icons';
 import {graphql} from 'react-relay';
 import {CiipPageComponentProps} from 'next-env';
 import {contactQueryResponse} from 'contactQuery.graphql';
@@ -21,19 +23,28 @@ class Contact extends Component<Props> {
     }
   `;
 
-  // TODO: Add content to this empty page
   render() {
     const {query} = this.props;
     const {session} = query || {};
     return (
       <DefaultLayout session={session} title="Contact Us">
         <Container>
-          <p>
-            Please email{' '}
-            <a href="mailto:GHGRegulator@gov.bc.ca?subject=CIIP Portal Inquiry">
-              GHGRegulator@gov.bc.ca
-            </a>{' '}
-            with any questions or concerns.
+          <Alert variant="info">
+            <FontAwesomeIcon icon={faInfoCircle} />
+            <span className="pl-2">
+              For help with your CIIP application, or questions about the
+              CleanBC Industrial Incentive Program, please email{' '}
+              <a href="mailto:GHGRegulator@gov.bc.ca?subject=Support Request (CIIP)">
+                GHGRegulator@gov.bc.ca
+              </a>
+            </span>
+          </Alert>
+          <p className="px-4 py-3">
+            To report a website error, contact the development team at{' '}
+            <a href="mailto:ggircs@gov.bc.ca?subject=Support Request (CIIP)">
+              ggircs@gov.bc.ca
+            </a>
+            .
           </p>
         </Container>
       </DefaultLayout>
