ca1024e6c0f575112149bb73e94831cbcbdf883a
Dylan Leard <dylan@button.is>
2021-04-20 11:22:57 -0700

    refactor: add uindex on gas_type

--- schema/deploy/tables/gas_001.sql
+++ schema/deploy/tables/gas_001.sql
@@ -4,6 +4,7 @@
 begin;
 
 alter table ggircs_portal.gas add column is_ciip_emission boolean default true;
+create unique index gas_gas_type_uindex on ggircs_portal.gas(gas_type);
 
 comment on column ggircs_portal.fuel.comments is 'boolean column indicates whether this row should be included when totalling emissions in the context of CIIP (CleanBC Industrial Incentive Program)';
 
--- schema/revert/tables/gas_001.sql
+++ schema/revert/tables/gas_001.sql
@@ -3,5 +3,6 @@
 begin;
 
 alter table ggircs_portal.gas drop column is_ciip_emission;
+drop index ggircs_portal.gas_gas_type_uindex;
 
 commit;
