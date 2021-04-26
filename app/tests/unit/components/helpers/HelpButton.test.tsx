0b9f754437b2c1d092e7679da5b1a73aeb89f916
Naomi Aro <naomi.aro@gov.bc.ca>
2021-04-26 11:28:16 -0700

    fix: update snapshots

--- app/tests/unit/components/helpers/HelpButton.test.tsx
+++ app/tests/unit/components/helpers/HelpButton.test.tsx
@@ -3,13 +3,26 @@ import {mount} from 'enzyme';
 import HelpButton from 'components/helpers/HelpButton';
 
 describe('HelpButton', () => {
-  it('(closed) matches the last accepted snapshot', () => {
-    const r = mount(<HelpButton />);
-    expect(r).toMatchSnapshot();
+  describe('as an internal user', () => {
+    it('(closed) matches the last accepted snapshot', () => {
+      const r = mount(<HelpButton />);
+      expect(r).toMatchSnapshot();
+    });
+    it('(opened) matches the last accepted snapshot', () => {
+      const r = mount(<HelpButton />);
+      r.find('button#help-button').simulate('click');
+      expect(r).toMatchSnapshot();
+    });
   });
-  it('(opened) matches the last accepted snapshot', () => {
-    const r = mount(<HelpButton />);
-    r.find('button#help-button').simulate('click');
-    expect(r).toMatchSnapshot();
+  describe('as an external user', () => {
+    it('(closed) matches the last accepted snapshot', () => {
+      const r = mount(<HelpButton isInternalUser={false} />);
+      expect(r).toMatchSnapshot();
+    });
+    it('(opened) matches the last accepted snapshot', () => {
+      const r = mount(<HelpButton isInternalUser={false} />);
+      r.find('button#help-button').simulate('click');
+      expect(r).toMatchSnapshot();
+    });
   });
 });
