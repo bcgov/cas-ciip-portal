5e980d009ad15b6acab4403e89e7a6c319bb7a94
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-04-28 16:01:20 -0700

    test: passing set of tests for emisson_total_matches_rev_zero_test

--- schema/test/unit/application_validation_functions/emisson_total_matches_rev_zero_test.sql
+++ schema/test/unit/application_validation_functions/emisson_total_matches_rev_zero_test.sql
@@ -3,7 +3,7 @@ create extension if not exists pgtap;
 reset client_min_messages;
 
 begin;
-select plan(2);
+select plan(6);
 
 select has_function(
   'ggircs_portal', 'emission_total_matches_rev_zero', array['ggircs_portal.application_revision'],
@@ -12,25 +12,35 @@ select has_function(
 
 select test_helper.clean_ggircs_portal_schema();
 select test_helper.modify_triggers('disable');
--- select test_helper.mock_open_window();
 
-with org_id as (insert into ggircs_portal.organisation(operator_name) values ('Operator1') returning id),
-fac_id as (insert into ggircs_portal.facility(facility_name, organisation_id
-) values ('Facility1', (select * from org_id)) returning id),
-app_id as (insert into ggircs_portal.application(facility_id) values ((select * from fac_id)) returning id)
-insert into ggircs_portal.application_revision(application_id, version_number) values ((select * from app_id), 0), ((select * from app_id), 1);
+with
+  org_id as (
+    insert into ggircs_portal.organisation(operator_name)
+    values ('Operator1')
+    returning id
+  ),
+  fac_id as (
+    insert into ggircs_portal.facility(facility_name, organisation_id)
+    values ('Facility1', (select * from org_id))
+    returning id
+  ),
+  app_id as (
+    insert into ggircs_portal.application(facility_id)
+    values ((select * from fac_id))
+    returning id
+  )
+insert into ggircs_portal.application_revision(application_id, version_number)
+values
+  ((select * from app_id), 0),
+  ((select * from app_id), 1);
 
--- select test_helper.create_applications(1, true, true);
 
-
-
--- Computed column returns the sum of (ciip) emissions for an application revision.
+-- Should return true when both emissions are equal
 create or replace function ggircs_portal.application_revision_total_ciip_emissions(application_revision ggircs_portal.application_revision)
 returns numeric
   as $$
     select 99::numeric;
   $$ language sql stable;
-
 
 
 select results_eq (
@@ -40,7 +50,93 @@ select results_eq (
   $$
     VALUES ('true'::boolean)
   $$,
-  'should be true'
+  'emission_total_matches_rev_zero should return true when emissions are strictly equal'
+ );
+
+-- Should return true when both emissions are 9.99 tCO2e apart
+create or replace function ggircs_portal.application_revision_total_ciip_emissions(application_revision ggircs_portal.application_revision)
+returns numeric
+  as $$
+    select case
+      when application_revision.version_number = 0 then 1000.0
+      else 1009.9
+    end;
+  $$ language sql stable;
+
+
+select results_eq (
+  $$
+    select ggircs_portal.emission_total_matches_rev_zero((select row(application_revision.*)::ggircs_portal.application_revision from ggircs_portal.application_revision where application_id=1 and version_number=1))
+  $$,
+  $$
+    VALUES ('true'::boolean)
+  $$,
+  'emission_total_matches_rev_zero should return true when emissions are less than 10 apart'
+ );
+
+-- Should return true when both emissions are -9.99 tCO2e apart
+create or replace function ggircs_portal.application_revision_total_ciip_emissions(application_revision ggircs_portal.application_revision)
+returns numeric
+  as $$
+    select case
+      when application_revision.version_number = 0 then 1000.0
+      else 990.1
+    end;
+  $$ language sql stable;
+
+
+select results_eq (
+  $$
+    select ggircs_portal.emission_total_matches_rev_zero((select row(application_revision.*)::ggircs_portal.application_revision from ggircs_portal.application_revision where application_id=1 and version_number=1))
+  $$,
+  $$
+    VALUES ('true'::boolean)
+  $$,
+  'emission_total_matches_rev_zero should return true when emissions are less than -10 apart'
+ );
+
+
+-- Should return false when emissions are  > 9.99 tCO2e apart
+create or replace function ggircs_portal.application_revision_total_ciip_emissions(application_revision ggircs_portal.application_revision)
+returns numeric
+  as $$
+    select case
+      when application_revision.version_number = 0 then 1000.0
+      else 28973582475.1
+    end;
+  $$ language sql stable;
+
+
+select results_eq (
+  $$
+    select ggircs_portal.emission_total_matches_rev_zero((select row(application_revision.*)::ggircs_portal.application_revision from ggircs_portal.application_revision where application_id=1 and version_number=1))
+  $$,
+  $$
+    VALUES ('false'::boolean)
+  $$,
+  'emission_total_matches_rev_zero should return false when emissions are more than 10 apart'
+ );
+
+
+-- Should return false when emissions are  < -9.99 tCO2e apart
+create or replace function ggircs_portal.application_revision_total_ciip_emissions(application_revision ggircs_portal.application_revision)
+returns numeric
+  as $$
+    select case
+      when application_revision.version_number = 0 then 1000.0
+      else 0
+    end;
+  $$ language sql stable;
+
+
+select results_eq (
+  $$
+    select ggircs_portal.emission_total_matches_rev_zero((select row(application_revision.*)::ggircs_portal.application_revision from ggircs_portal.application_revision where application_id=1 and version_number=1))
+  $$,
+  $$
+    VALUES ('false'::boolean)
+  $$,
+  'mission_total_matches_rev_zero should return false when emissions are more than -10 apart'
  );
 
 select finish();
