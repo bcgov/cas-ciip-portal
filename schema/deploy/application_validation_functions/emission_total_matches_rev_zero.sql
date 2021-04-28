d37f19f8ae48f7260b6a486bd49cfa13bcb82985
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-04-28 08:47:24 -0700

    chore: creating migration for the validation function

---
+++ schema/deploy/application_validation_functions/emission_total_matches_rev_zero.sql
@@ -0,0 +1,7 @@
+-- Deploy ggircs-portal:application_validation_functions/emission_total_matches_rev_zero to pg
+
+begin;
+
+-- XXX Add DDLs here.
+
+commit;
---
+++ schema/verify/application_validation_functions/emission_total_matches_rev_zero.sql
@@ -0,0 +1,7 @@
+-- Verify ggircs-portal:application_validation_functions/emission_total_matches_rev_zero on pg
+
+begin;
+
+-- XXX Add verifications here.
+
+rollback;
