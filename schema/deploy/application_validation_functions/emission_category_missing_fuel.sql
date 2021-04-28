51e22690953fc586d7e5609eca7b32577895749b
Dylan Leard <dylan@button.is>
2021-04-28 13:55:57 -0700

    refactor: use computed columns in validation function

--- schema/deploy/application_validation_functions/emission_category_missing_fuel.sql
+++ schema/deploy/application_validation_functions/emission_category_missing_fuel.sql
@@ -8,20 +8,19 @@ create or replace function ggircs_portal.emission_category_missing_fuel(app_revi
   returns boolean
   as $$
 
-  with x as (
+  with emissions as (
     select distinct(source_type_name) as reported_via_emission
-    from ggircs_portal.ciip_emission ce
-    where ce.application_id = app_revision.application_id
-    and ce.version_number = app_revision.version_number
-    and ce.annual_co2e > 0
-  ), y as (
+    from ggircs_portal.application_revision_emission_form_data(app_revision) ed
+    where ed.annual_co2e > 0
+  ), fuels as (
+    with fuel_data as (
+      select * from ggircs_portal.application_revision_fuel_form_data(app_revision)
+    )
     select distinct(display_name) as reported_via_fuel from ggircs_portal.emission_category ec
-    join ggircs_portal.ciip_fuel cf
-    on cf.emission_category_id = ec.id
-    and cf.application_id = app_revision.application_id
-    and cf.version_number = app_revision.version_number
-    and cf.quantity > 0
-  ) select (select count(*) from x where reported_via_emission not in (select reported_via_fuel from y)) = 0;
+    join fuel_data
+      on fuel_data.emission_category_id = ec.id
+      and fuel_data.quantity > 0
+  ) select (select count(*) from emissions where reported_via_emission not in (select reported_via_fuel from fuels)) = 0;
 
 $$ language sql stable;
 
