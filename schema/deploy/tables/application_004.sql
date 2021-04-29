7cd9df77f5df1750f3a83d70388f0251594ebee8
Dylan Leard <dylan@button.is>
2021-04-29 10:12:30 -0700

    refactor: comment on swrs_report_version reset & cleanup sqitch plan

--- schema/deploy/tables/application_004.sql
+++ schema/deploy/tables/application_004.sql
@@ -3,6 +3,12 @@
 
 begin;
 
+/**
+  The previous migration (application_003) set the swrs_report_version for all applications to the latest version.
+  This was wrong, since many of the application's swrs versions (version 0 of the application) are not up to date.
+  Setting the swrs_report_version to 1 for all applications ensures that applications that require
+  a swrs update to their version 0 data (via the refresh_swrs_version_data() function) will receive it.
+**/
 update ggircs_portal.application set swrs_report_version=1;
 
 commit;
