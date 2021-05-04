562f27b18d8ef4e69e3a919f1ce02b3a0316b4d2
Dylan Leard <dylan@button.is>
2021-05-04 14:20:41 -0700

    feat: create an index for all fkeys currently missing an index

---
+++ schema/revert/database_functions/add_missing_indices.sql
@@ -0,0 +1,7 @@
+-- Revert ggircs-portal:database_functions/add_missing_indices from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ schema/verify/database_functions/add_missing_indices.sql
@@ -0,0 +1,7 @@
+-- Verify ggircs-portal:database_functions/add_missing_indices on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
