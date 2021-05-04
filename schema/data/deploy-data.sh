1ce24d5cfdbaf9a539de53ed67adc33f9dae93ba
Dylan Leard <dylan@button.is>
2021-05-04 09:59:52 -0700

    refactor: add validation seed data to deployTestData() target

--- schema/data/deploy-data.sh
+++ schema/data/deploy-data.sh
@@ -206,6 +206,7 @@ deployTestData() {
   _psql -f "./prod/gas.sql"
   _psql -f "./prod/emission_gas.sql"
   _psql -f "./prod/review_step.sql"
+  _psql -f "./prod/application_validation_function.sql"
   return 0;
 }
 
