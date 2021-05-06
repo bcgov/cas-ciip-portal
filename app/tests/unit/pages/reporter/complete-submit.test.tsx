9b526da564c0e2fb2812b2b45584f1a888a06205
Naomi Aro <naomi.aro@gov.bc.ca>
2021-05-06 14:29:10 -0700

    test: look for application specific page items

--- app/tests/unit/pages/reporter/complete-submit.test.tsx
+++ app/tests/unit/pages/reporter/complete-submit.test.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import Link from 'next/link';
 import CompleteSubmit from 'pages/reporter/complete-submit';
 import {completeSubmitQueryResponse} from 'completeSubmitQuery.graphql';
+import {getViewApplicationPageRoute} from 'routes';
 
 const query: completeSubmitQueryResponse['query'] = {
   session: {
@@ -33,5 +35,22 @@ describe('Reporter application submission page', () => {
   it('should render properly', () => {
     const wrapper = shallow(<CompleteSubmit router={router} query={query} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should display the facility and operator name', () => {
+    const wrapper = shallow(<CompleteSubmit router={router} query={query} />);
+    const lead = wrapper.find('.lead').text();
+
+    expect(lead).toContain('Super Clean');
+    expect(lead).toContain('CleanBC Org');
+  });
+
+  it('should link to the latest submitted application revision', () => {
+    const wrapper = shallow(<CompleteSubmit router={router} query={query} />);
+    const link = wrapper.find(Link);
+
+    expect(link.prop('href')).toEqual(
+      getViewApplicationPageRoute('testing', 2)
+    );
   });
 });
