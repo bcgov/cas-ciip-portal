41e2602a613105e898a427ef3197ffadec050f87
Dylan Leard <dylan@button.is>
2021-05-12 08:54:34 -0700

    refactor: move call to mock function into deployDev

--- schema/data/deploy-data.sh
+++ schema/data/deploy-data.sh
@@ -233,18 +233,18 @@ deployDevData() {
   _psql -f "./dev/user.sql"
   _psql -f "./dev/linked_product.sql"
   _psql -f "./dev/product_naics_code.sql"
+  _psql -f "./swrs_dev/override_last_swrs_reporting_year.sql"
   return 0;
 }
 
 deploySwrsDevData() {
   ./swrs_dev/deploy-swrs-data.sh --dev
-  _psql -f "./swrs_dev/override_last_swrs_reporting_year.sql"
+
   return 0;
 }
 
 deploySwrsLoadTestingData() {
   ./swrs_dev/deploy-swrs-data.sh --load-test
-  _psql -f "./swrs_dev/override_last_swrs_reporting_year.sql"
   return 0;
 }
 
