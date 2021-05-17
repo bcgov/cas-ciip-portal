3bc9ff1222b4efded97c4ad38ad276963abc0551
Kristen Cooke <kristen@button.is>
2021-05-17 16:56:50 -0700

    test: update tests and extract test queries into factory function

--- app/tests/unit/containers/Forms/ProductField.test.tsx
+++ app/tests/unit/containers/Forms/ProductField.test.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import {ProductFieldComponent} from 'containers/Forms/ProductField';
-import {ProductField_query} from '__generated__/ProductField_query.graphql';
+import {
+  ProductField_query,
+  CiipProductState
+} from '__generated__/ProductField_query.graphql';
 import {ProductField_naicsCode} from '__generated__/ProductField_naicsCode.graphql';
 import {createDefaultJsonSchemaFormProps} from 'tests/json-schema-utils';
 
-describe('The ProductionFields Component with published product matched to a naics code', () => {
+const getTestQuery = ({
+  noNaicsCode,
+  productState,
+  reportedProductIsAllowable
+}) => {
   const query: ProductField_query = {
     ' $refType': 'ProductField_query',
     allProducts: {
@@ -14,7 +21,7 @@ describe('The ProductionFields Component with published product matched to a nai
           node: {
             rowId: 1,
             units: 'bar',
-            productState: 'PUBLISHED',
+            productState: productState as CiipProductState,
             requiresEmissionAllocation: true,
             requiresProductAmount: true,
             isEnergyProduct: false,
@@ -32,233 +39,117 @@ describe('The ProductionFields Component with published product matched to a nai
   };
   const naicsCode: ProductField_naicsCode = {
     ' $refType': 'ProductField_naicsCode',
-    allProductsByNaicsCode: {
+    allowableProducts: {
       edges: [
         {
           node: {
-            rowId: 1,
-            productState: 'PUBLISHED'
+            productId: reportedProductIsAllowable ? 1 : 2,
+            isMandatory: false
           }
         }
       ]
     }
   };
-
-  const props = {
+  const withoutNaicsCode = {
     ...createDefaultJsonSchemaFormProps(),
-    formData: {
-      productRowId: 1,
-      productUnits: 'bar',
-      productEmissions: 123,
-      productAmount: 456,
-      requiresEmissionAllocation: true
-    },
-    query,
-    naicsCode
+    query
   };
+  return noNaicsCode
+    ? withoutNaicsCode
+    : {
+        ...withoutNaicsCode,
+        naicsCode
+      };
+};
 
+const getTestElement = ({
+  noNaicsCode = false,
+  includeFormData = true,
+  productState = 'PUBLISHED',
+  reportedProductIsAllowable = true
+}) => {
+  const data = getTestQuery({
+    noNaicsCode,
+    productState,
+    reportedProductIsAllowable
+  });
+  const formData = {
+    productRowId: 1,
+    productUnits: 'bar',
+    productEmissions: 123,
+    productAmount: 456,
+    requiresEmissionAllocation: true
+  };
+  return (
+    <ProductFieldComponent
+      {...data}
+      formData={includeFormData ? formData : {}}
+    />
+  );
+};
+
+describe('The ProductField with published product matched to a naics code', () => {
   it('should match the snapshot when rendering a product', () => {
-    const wrapper = shallow(<ProductFieldComponent {...props} />);
+    const wrapper = shallow(getTestElement({}));
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should not render any warnings', () => {
-    const wrapper = shallow(<ProductFieldComponent {...props} />);
+    const wrapper = shallow(getTestElement({}));
     expect(wrapper.exists('Alert')).toBe(false);
   });
 });
 
-describe('The ProductionFields Component with published product not matched to a naics code', () => {
-  const query: ProductField_query = {
-    ' $refType': 'ProductField_query',
-    allProducts: {
-      edges: [
-        {
-          node: {
-            rowId: 1,
-            units: 'bar',
-            productState: 'PUBLISHED',
-            requiresEmissionAllocation: true,
-            requiresProductAmount: true,
-            isEnergyProduct: false,
-            addPurchasedElectricityEmissions: false,
-            addPurchasedHeatEmissions: false,
-            subtractExportedElectricityEmissions: false,
-            subtractExportedHeatEmissions: false,
-            subtractGeneratedElectricityEmissions: false,
-            subtractGeneratedHeatEmissions: false,
-            addEmissionsFromEios: false
-          }
-        }
-      ]
-    }
-  };
-  const naicsCode: ProductField_naicsCode = {
-    ' $refType': 'ProductField_naicsCode',
-    allProductsByNaicsCode: {
-      edges: [
-        {
-          node: {
-            rowId: 2,
-            productState: 'PUBLISHED'
-          }
-        }
-      ]
-    }
-  };
-
-  const props = {
-    ...createDefaultJsonSchemaFormProps(),
-    formData: {
-      productRowId: 1,
-      productUnits: 'bar',
-      productEmissions: 123,
-      productAmount: 456,
-      requiresEmissionAllocation: true
-    },
-    query,
-    naicsCode
-  };
-
+describe('The ProductField with published product not matched to a naics code', () => {
   it('should match the snapshot when rendering a product not matched to a naics code', () => {
-    const wrapper = shallow(<ProductFieldComponent {...props} />);
+    const wrapper = shallow(
+      getTestElement({reportedProductIsAllowable: false})
+    );
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should render the not in naics code warning', () => {
-    const wrapper = shallow(<ProductFieldComponent {...props} />);
+    const wrapper = shallow(
+      getTestElement({reportedProductIsAllowable: false})
+    );
     expect(wrapper.find('Alert').text()).toContain(
       'Product or Service is not associated with the NAICS code'
     );
   });
 });
 
-describe('The ProductionFields Component with archived product matched to a naics code', () => {
-  const query: ProductField_query = {
-    ' $refType': 'ProductField_query',
-    allProducts: {
-      edges: [
-        {
-          node: {
-            rowId: 1,
-            units: 'bar',
-            productState: 'ARCHIVED',
-            requiresEmissionAllocation: true,
-            requiresProductAmount: true,
-            isEnergyProduct: false,
-            addPurchasedElectricityEmissions: false,
-            addPurchasedHeatEmissions: false,
-            subtractExportedElectricityEmissions: false,
-            subtractExportedHeatEmissions: false,
-            subtractGeneratedElectricityEmissions: false,
-            subtractGeneratedHeatEmissions: false,
-            addEmissionsFromEios: false
-          }
-        }
-      ]
-    }
-  };
-
-  const naicsCode: ProductField_naicsCode = {
-    ' $refType': 'ProductField_naicsCode',
-    allProductsByNaicsCode: {
-      edges: [
-        {
-          node: {
-            rowId: 1,
-            productState: 'ARCHIVED'
-          }
-        }
-      ]
-    }
-  };
-
-  const props = {
-    ...createDefaultJsonSchemaFormProps(),
-    formData: {
-      productRowId: 1,
-      productUnits: 'bar',
-      productEmissions: 123,
-      productAmount: 456,
-      requiresEmissionAllocation: true
-    },
-    query,
-    naicsCode
-  };
-
+describe('The ProductField with archived product matched to a naics code', () => {
   it('should match the snapshot when rendering an archived product', () => {
-    const wrapper = shallow(<ProductFieldComponent {...props} />);
+    const wrapper = shallow(getTestElement({productState: 'ARCHIVED'}));
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should not render the archived warning', () => {
-    const wrapper = shallow(<ProductFieldComponent {...props} />);
+  it('should render the archived warning', () => {
+    const wrapper = shallow(getTestElement({productState: 'ARCHIVED'}));
     expect(wrapper.find('Alert').text()).toContain(
       'Product or Service has been archived'
     );
   });
 });
 
-describe('The ProductionFields Component with archived product not matched to a naics code', () => {
-  const query: ProductField_query = {
-    ' $refType': 'ProductField_query',
-    allProducts: {
-      edges: [
-        {
-          node: {
-            rowId: 1,
-            units: 'bar',
-            productState: 'ARCHIVED',
-            requiresEmissionAllocation: true,
-            requiresProductAmount: true,
-            isEnergyProduct: false,
-            addPurchasedElectricityEmissions: false,
-            addPurchasedHeatEmissions: false,
-            subtractExportedElectricityEmissions: false,
-            subtractExportedHeatEmissions: false,
-            subtractGeneratedElectricityEmissions: false,
-            subtractGeneratedHeatEmissions: false,
-            addEmissionsFromEios: false
-          }
-        }
-      ]
-    }
-  };
-
-  const naicsCode: ProductField_naicsCode = {
-    ' $refType': 'ProductField_naicsCode',
-    allProductsByNaicsCode: {
-      edges: [
-        {
-          node: {
-            rowId: 2,
-            productState: 'ARCHIVED'
-          }
-        }
-      ]
-    }
-  };
-
-  const props = {
-    ...createDefaultJsonSchemaFormProps(),
-    formData: {
-      productRowId: 1,
-      productUnits: 'bar',
-      productEmissions: 123,
-      productAmount: 456,
-      requiresEmissionAllocation: true
-    },
-    query,
-    naicsCode
-  };
-
+describe('The ProductField with archived product not matched to a naics code', () => {
   it('should match the snapshot when rendering an archived product not matched to a naics code', () => {
-    const wrapper = shallow(<ProductFieldComponent {...props} />);
+    const wrapper = shallow(
+      getTestElement({
+        productState: 'ARCHIVED',
+        reportedProductIsAllowable: false
+      })
+    );
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should render the archived warning and the not in naics warning', () => {
-    const wrapper = shallow(<ProductFieldComponent {...props} />);
+    const wrapper = shallow(
+      getTestElement({
+        productState: 'ARCHIVED',
+        reportedProductIsAllowable: false
+      })
+    );
     expect(wrapper.find('Alert').at(0).text()).toContain(
       'Product or Service has been archived'
     );
@@ -268,27 +159,18 @@ describe('The ProductionFields Component with archived product not matched to a 
   });
 });
 
-describe('The ProductionFields Component with no naics code', () => {
-  const query: ProductField_query = {
-    ' $refType': 'ProductField_query',
-    allProducts: {
-      edges: []
-    }
-  };
-
-  const props = {
-    ...createDefaultJsonSchemaFormProps(),
-    formData: {},
-    query
-  };
-
+describe('The ProductField with no naics code', () => {
   it('should match the snapshot', () => {
-    const wrapper = shallow(<ProductFieldComponent {...props} />);
+    const wrapper = shallow(
+      getTestElement({noNaicsCode: true, includeFormData: false})
+    );
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should render the no products found warning', () => {
-    const wrapper = shallow(<ProductFieldComponent {...props} />);
+    const wrapper = shallow(
+      getTestElement({noNaicsCode: true, includeFormData: false})
+    );
     expect(wrapper.find('Alert').at(0).text()).toContain(
       'No products were found'
     );
