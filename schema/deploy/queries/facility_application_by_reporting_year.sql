8d067e70922f727ea50107e86443551f9436228f
Matthieu Foucault <matthieu@button.is>
2021-05-27 15:28:46 -0700

    fix: facilities applications are ordered by operator and facility name

--- schema/deploy/queries/facility_application_by_reporting_year.sql
+++ schema/deploy/queries/facility_application_by_reporting_year.sql
@@ -6,6 +6,7 @@ begin;
 create or replace function ggircs_portal.facility_application_by_reporting_year(_reporting_year int)
   returns setof ggircs_portal.facility_application
   as $$
+  select * from (
     (select facility.id, organisation.id, application.id,
       organisation.operator_name, facility.facility_name, facility.facility_type, facility.bcghgid,
       facility.swrs_facility_id, application.reporting_year
@@ -14,8 +15,7 @@ create or replace function ggircs_portal.facility_application_by_reporting_year(
       on facility.id = application.facility_id
       and application.reporting_year = _reporting_year
     join ggircs_portal.organisation on facility.organisation_id = organisation.id
-    where _reporting_year is not null
-    order by organisation.operator_name, facility.facility_name)
+    where _reporting_year is not null)
     union
     (select facility.id, organisation.id, application.id,
       organisation.operator_name, facility.facility_name, facility.facility_type, facility.bcghgid,
@@ -25,8 +25,8 @@ create or replace function ggircs_portal.facility_application_by_reporting_year(
       on facility.id = application.facility_id
       and application.reporting_year = (select reporting_year from ggircs_portal.default_displayed_reporting_year())
     join ggircs_portal.organisation on facility.organisation_id = organisation.id
-    where _reporting_year is null
-    order by organisation.operator_name, facility.facility_name);
+    where _reporting_year is null)
+  ) sub order by operator_name, facility_name;
   $$ language 'sql' stable;
 
 grant execute on function ggircs_portal.facility_application_by_reporting_year to ciip_administrator, ciip_analyst, ciip_industry_user;
