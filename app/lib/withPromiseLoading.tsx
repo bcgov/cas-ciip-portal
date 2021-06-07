ebd47c26595f007e5c16757b089ece00bf658699
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-06-07 16:25:17 -0700

    refactor: hoc cleans up when unmounted to avoid dom changes

--- app/lib/withPromiseLoading.tsx
+++ app/lib/withPromiseLoading.tsx
@@ -1,16 +1,30 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
-const withPromiseLoading = function a(
-  Component: React.ComponentClass | React.FunctionComponent,
+const withPromiseLoading = function higherOrderComponent<TProps>(
+  Component: React.ComponentClass<TProps> | React.FunctionComponent<TProps>,
   asyncEventHandler: string,
   isInFlightProp: string
 ) {
-  return (props) => {
+  return (props: TProps) => {
     const [isLoading, setIsLoading] = useState(false);
 
+    let isSubscribed = true;
+
+    // This is to avoid making changes to the state after the component has been unmounted
+    // In case the asyncEventHandler has side-effects on the DOM
+    useEffect(() => {
+      return function cleanUp() {
+        isSubscribed = false;
+      };
+    }, []);
+
     const handleAsyncEvent = async (...args) => {
+      if (!isSubscribed) return;
       setIsLoading(true);
+
       await props[asyncEventHandler](...args);
+
+      if (!isSubscribed) return;
       setIsLoading(false);
     };
 
