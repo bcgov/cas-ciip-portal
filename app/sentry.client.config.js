0f24dad7ae6647dc6636bd857affbf5e8e80638d
Matthieu Foucault <matthieu@button.is>
2021-06-09 16:31:45 -0700

    chore: sentry release should be available on client

--- app/sentry.client.config.js
+++ app/sentry.client.config.js
@@ -5,7 +5,8 @@
 import * as Sentry from '@sentry/nextjs';
 import getConfig from 'next/config';
 
-const {SENTRY_ENVIRONMENT} = getConfig().publicRuntimeConfig || {};
+const {SENTRY_ENVIRONMENT, SENTRY_RELEASE} =
+  getConfig().publicRuntimeConfig || {};
 
 const SENTRY_DSN = SENTRY_ENVIRONMENT
   ? 'https://0c7b819c89924f4abe36297bf7cbc7cb@o646776.ingest.sentry.io/5797775'
@@ -13,5 +14,6 @@ const SENTRY_DSN = SENTRY_ENVIRONMENT
 
 Sentry.init({
   dsn: SENTRY_DSN,
-  environment: SENTRY_ENVIRONMENT
+  environment: SENTRY_ENVIRONMENT,
+  release: SENTRY_RELEASE
 });
