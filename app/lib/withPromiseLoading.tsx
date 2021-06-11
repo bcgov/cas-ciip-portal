1adfa30eed880e9edd1b088821b01f571e590c1b
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-06-11 16:56:20 -0700

    refactor: naming the hoc through reflection

--- app/lib/withPromiseLoading.tsx
+++ app/lib/withPromiseLoading.tsx
@@ -10,43 +10,45 @@ const withPromiseLoading = function higherOrderComponent<
   asyncEventHandler: HandlerKey,
   isInFlightProp: InFlightKey
 ): (props: TProps) => JSX.Element {
-  return function WithPromiseLoadingWrapper(props: TProps) {
-    const [isLoading, setIsLoading] = useState(false);
+  return {
+    [`WithPromiseLoading(${Wrapped.name})`](props: TProps) {
+      const [isLoading, setIsLoading] = useState(false);
 
-    let isSubscribed = true;
+      let isSubscribed = true;
 
-    // This is to avoid making changes to the state after the component has been unmounted
-    // In case the asyncEventHandler has side-effects on the DOM
-    useEffect(() => {
-      return function cleanUp() {
-        isSubscribed = false;
+      // This is to avoid making changes to the state after the component has been unmounted
+      // In case the asyncEventHandler has side-effects on the DOM
+      useEffect(() => {
+        return function cleanUp() {
+          isSubscribed = false;
+        };
+      }, []);
+
+      const handleAsyncEvent = async (...args) => {
+        if (!isSubscribed) return;
+        setIsLoading(true);
+
+        try {
+          // We can only verify that the key exists on TProps,
+          // not the type of the property for that key
+          await (props[asyncEventHandler] as any)(...args);
+        } catch (err) {
+          Sentry.captureException(err);
+        }
+
+        if (!isSubscribed) return;
+        setIsLoading(false);
       };
-    }, []);
 
-    const handleAsyncEvent = async (...args) => {
-      if (!isSubscribed) return;
-      setIsLoading(true);
+      const childProps = {
+        ...props,
+        [isInFlightProp]: isLoading,
+        [asyncEventHandler]: handleAsyncEvent
+      };
 
-      try {
-        // We can only verify that the key exists on TProps,
-        // not the type of the property for that key
-        await (props[asyncEventHandler] as any)(...args);
-      } catch (err) {
-        Sentry.captureException(err);
-      }
-
-      if (!isSubscribed) return;
-      setIsLoading(false);
-    };
-
-    const childProps = {
-      ...props,
-      [isInFlightProp]: isLoading,
-      [asyncEventHandler]: handleAsyncEvent
-    };
-
-    return <Wrapped {...childProps} />;
-  };
+      return <Wrapped {...childProps} />;
+    }
+  }[`WithPromiseLoading(${Wrapped.name})`];
 };
 
 export default withPromiseLoading;
