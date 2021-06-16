ea15265c4dd846ff9226d4c485d52aa426781f1e
Naomi Aro <naomi.aro@gov.bc.ca>
2021-06-15 17:27:54 -0700

    refactor: remove async on tests

--- app/tests/unit/lib/user-groups.test.ts
+++ app/tests/unit/lib/user-groups.test.ts
@@ -6,7 +6,7 @@ import {
 } from 'lib/user-groups';
 
 describe('User Groups', () => {
-  it('should return all groups', async () => {
+  it('should return all groups', () => {
     const GROUPS = [
       'Guest',
       'User',
@@ -19,49 +19,49 @@ describe('User Groups', () => {
     expect(getAllGroups()).toStrictEqual(GROUPS);
   });
 
-  it('should return empty when all groups invalid', async () => {
+  it('should return empty when all groups invalid', () => {
     const userGroups = ['fake 1', 'fake 2'];
     const groups = compactGroups(userGroups);
 
     expect(groups).toStrictEqual([]);
   });
 
-  it('should return only valid groups', async () => {
+  it('should return only valid groups', () => {
     const userGroups = ['fake 1', 'Pending Analyst', 'fake 3'];
     const groups = compactGroups(userGroups);
 
     expect(groups).toStrictEqual(['Pending Analyst']);
   });
 
-  it('should default to group Guest', async () => {
+  it('should default to group Guest', () => {
     const userGroups = [];
     const groupName = getPriorityGroup(userGroups);
 
     expect(groupName).toBe('Guest');
   });
 
-  it('should return the valid group name', async () => {
+  it('should return the valid group name', () => {
     const userGroups = ['Incentive Administrator', 'fake 1'];
     const groupName = getPriorityGroup(userGroups);
 
     expect(groupName).toBe('Incentive Administrator');
   });
 
-  it('should get the group name with min priority number', async () => {
+  it('should get the group name with min priority number', () => {
     const userGroups = ['Incentive Administrator', 'Incentive Analyst'];
     const groupName = getPriorityGroup(userGroups);
 
     expect(groupName).toBe('Incentive Administrator');
   });
 
-  it('should get the default landing route', async () => {
+  it('should get the default landing route', () => {
     const userGroups = [];
     const groupPath = getUserGroupLandingRoute(userGroups);
 
     expect(groupPath).toBe('/');
   });
 
-  it('should get the correct landing route', async () => {
+  it('should get the correct landing route', () => {
     const userGroups = ['Incentive Administrator', 'Incentive Analyst'];
     const groupPath = getUserGroupLandingRoute(userGroups);
 
