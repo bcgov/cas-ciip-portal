913c9cf0713c206312e62bcf9b15d381a2b62158
Naomi Aro <naomi.aro@gov.bc.ca>
2021-06-16 10:27:07 -0700

    refactor: add describe blocks to tests

--- app/tests/unit/lib/user-groups.test.ts
+++ app/tests/unit/lib/user-groups.test.ts
@@ -6,65 +6,73 @@ import {
 } from 'lib/user-groups';
 
 describe('User Groups', () => {
-  it('should return all groups', () => {
-    const GROUPS = [
-      'Guest',
-      'User',
-      'Realm Administrator',
-      'Incentive Administrator',
-      'Incentive Analyst',
-      'Pending Analyst'
-    ];
+  describe('getAllGroups', () => {
+    it('should return all groups', () => {
+      const GROUPS = [
+        'Guest',
+        'User',
+        'Realm Administrator',
+        'Incentive Administrator',
+        'Incentive Analyst',
+        'Pending Analyst'
+      ];
 
-    expect(getAllGroups()).toStrictEqual(GROUPS);
+      expect(getAllGroups()).toStrictEqual(GROUPS);
+    });
   });
 
-  it('should return empty when all groups invalid', () => {
-    const userGroups = ['fake 1', 'fake 2'];
-    const groups = compactGroups(userGroups);
+  describe('compactGroups', () => {
+    it('should return empty when all groups invalid', () => {
+      const userGroups = ['fake 1', 'fake 2'];
+      const groups = compactGroups(userGroups);
 
-    expect(groups).toStrictEqual([]);
+      expect(groups).toStrictEqual([]);
+    });
+
+    it('should return only valid groups', () => {
+      const userGroups = ['fake 1', 'Pending Analyst', 'fake 3'];
+      const groups = compactGroups(userGroups);
+
+      expect(groups).toStrictEqual(['Pending Analyst']);
+    });
   });
 
-  it('should return only valid groups', () => {
-    const userGroups = ['fake 1', 'Pending Analyst', 'fake 3'];
-    const groups = compactGroups(userGroups);
+  describe('getPriorityGroup', () => {
+    it('should default to group Guest', () => {
+      const userGroups = [];
+      const groupName = getPriorityGroup(userGroups);
 
-    expect(groups).toStrictEqual(['Pending Analyst']);
+      expect(groupName).toBe('Guest');
+    });
+
+    it('should return the valid group name', () => {
+      const userGroups = ['Incentive Administrator', 'fake 1'];
+      const groupName = getPriorityGroup(userGroups);
+
+      expect(groupName).toBe('Incentive Administrator');
+    });
+
+    it('should get the group name with min priority number', () => {
+      const userGroups = ['Incentive Administrator', 'Incentive Analyst'];
+      const groupName = getPriorityGroup(userGroups);
+
+      expect(groupName).toBe('Incentive Administrator');
+    });
   });
 
-  it('should default to group Guest', () => {
-    const userGroups = [];
-    const groupName = getPriorityGroup(userGroups);
+  describe('getUserGroupLandingRoute', () => {
+    it('should get the default landing route', () => {
+      const userGroups = [];
+      const groupPath = getUserGroupLandingRoute(userGroups);
 
-    expect(groupName).toBe('Guest');
-  });
+      expect(groupPath).toBe('/');
+    });
 
-  it('should return the valid group name', () => {
-    const userGroups = ['Incentive Administrator', 'fake 1'];
-    const groupName = getPriorityGroup(userGroups);
+    it('should get the correct landing route', () => {
+      const userGroups = ['Incentive Administrator', 'Incentive Analyst'];
+      const groupPath = getUserGroupLandingRoute(userGroups);
 
-    expect(groupName).toBe('Incentive Administrator');
-  });
-
-  it('should get the group name with min priority number', () => {
-    const userGroups = ['Incentive Administrator', 'Incentive Analyst'];
-    const groupName = getPriorityGroup(userGroups);
-
-    expect(groupName).toBe('Incentive Administrator');
-  });
-
-  it('should get the default landing route', () => {
-    const userGroups = [];
-    const groupPath = getUserGroupLandingRoute(userGroups);
-
-    expect(groupPath).toBe('/');
-  });
-
-  it('should get the correct landing route', () => {
-    const userGroups = ['Incentive Administrator', 'Incentive Analyst'];
-    const groupPath = getUserGroupLandingRoute(userGroups);
-
-    expect(groupPath).toBe('/admin');
+      expect(groupPath).toBe('/admin');
+    });
   });
 });
