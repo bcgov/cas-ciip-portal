aca40e7b512719c360f5bd31a2b91012e7e63130
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-06-25 09:35:57 -0700

    refactor: generic modal uses global styles

--- app/components/GenericConfirmationModal.tsx
+++ app/components/GenericConfirmationModal.tsx
@@ -55,24 +55,6 @@ export const GenericConfirmationModal: React.FunctionComponent<Props> = ({
           background: #036;
           color: #fff;
         }
-        .btn-primary#confirm {
-          background: #036;
-        }
-        .btn-primary#confirm:hover,
-        .btn-primary#cancel:hover {
-          background: #002040;
-          border-color: #002040;
-        }
-        .btn-outline-primary#confirm,
-        .btn-outline-primary#cancel {
-          color: #036;
-          border-color: #036;
-        }
-        .btn-outline-primary#confirm:hover,
-        .btn-outline-primary#cancel:hover {
-          color: #fff;
-          background: #002040;
-        }
       `}</style>
     </Modal>
   );
