d40084f787571c7709b592c0bf3e657516b5af70
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-06-24 17:14:33 -0700

    refactor: some hover styles for buttons

--- app/layouts/GlobalBootstrapOverrideStyles.tsx
+++ app/layouts/GlobalBootstrapOverrideStyles.tsx
@@ -1,7 +1,12 @@
 import css from 'styled-jsx/css';
 
 export default css.global`
+  /* blues */
+
   a {
+    color: #0053b3;
+  }
+  .btn-link {
     color: #0053b3;
   }
   .btn-link {
@@ -12,15 +17,44 @@ export default css.global`
     border-color: #036;
   }
   .btn-outline-primary {
-    color: #0053b3;
-    border-color: #0053b3;
+    color: #036;
+    border-color: #036;
   }
+  .btn-outline-primary:hover {
+    background-color: #036;
+    border-color: #036;
+  }
+
+  /* greens */
+
   .badge-success,
   .btn-success {
     background-color: #24883e;
   }
+  .btn-outline-success {
+    color: #24883e;
+    border-color: #24883e;
+  }
+
+  /* teals */
+
   .badge-info,
   .btn-info {
     background-color: #197ea3;
+  }
+  .btn-info.focus,
+  .btn-info:focus,
+  .btn-info:hover {
+    background-color: #177192;
+  }
+  .btn-outline-info {
+    color: #197ea3;
+    border-color: #197ea3;
+  }
+  .btn-outline-info.focus,
+  .btn-outline-info:focus,
+  .btn-outline-info:hover {
+    background-color: #177192;
+    border-color: #177192;
   }
 `;
