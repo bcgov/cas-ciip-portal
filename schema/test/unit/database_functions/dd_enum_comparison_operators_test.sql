388f8405ae87d35627b3c8d1eedd04971451728e
Naomi Aro <naomi.aro@gov.bc.ca>
2021-07-12 15:50:14 -0700

    test: negative operator doesn't throw

--- schema/test/unit/database_functions/dd_enum_comparison_operators_test.sql
+++ schema/test/unit/database_functions/dd_enum_comparison_operators_test.sql
@@ -8,7 +8,7 @@ create extension if not exists pgtap;
 reset client_min_messages;
 
 begin;
-select plan(2);
+select plan(3);
 
 select operators_are(
   'public',
@@ -25,9 +25,11 @@ select operators_are(
   'Added enum operators exist for Metabase.'
 );
 
-prepare prepared_enum_test (text) AS select id from ggircs_portal.product where product_state = $1;
+prepare prepared_enum_test_eq (text) AS select id from ggircs_portal.product where product_state = $1;
+select lives_ok( 'execute prepared_enum_test_eq(''published'')', 'should not complain that the = operator does not exist');
 
-select lives_ok( 'execute prepared_enum_test(''published'')', 'should not complain that the operator does not exist');
+prepare prepared_enum_test_neq (text) AS select id from ggircs_portal.product where product_state != $1;
+select lives_ok( 'execute prepared_enum_test_neq(''published'')', 'should not complain that the <> operator does not exist');
 
 select finish();
 rollback;
