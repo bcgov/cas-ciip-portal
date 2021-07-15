0e303e3ad1e885e156a55e2dc5bf426c01ab297c
Matthieu Foucault <matthieu@button.is>
2021-07-15 14:59:46 -0700

    docs: add comments to ciip_incentive_per_product view

--- schema/deploy/views/ciip_incentive_per_product.sql
+++ schema/deploy/views/ciip_incentive_per_product.sql
@@ -19,4 +19,21 @@ create view ggircs_portal.ciip_incentive_per_product as (
   order by ar.application_id, product_id
 );
 
+comment on view ggircs_portal.ciip_incentive_per_product is E'@omit/nReturns a record for each benchmarked product contained in the latest version of submitted applications';
+comment on column ggircs_portal.ciip_incentive_per_product.application_id is 'The id of the application this product is reported in';
+comment on column ggircs_portal.ciip_incentive_per_product.version_number is 'The most recent submitted version number for the application';
+comment on column ggircs_portal.ciip_incentive_per_product.product_id is 'The ID of the benchmarked product';
+comment on column ggircs_portal.ciip_incentive_per_product.product_name is 'The name of the benchmarked product';
+comment on column ggircs_portal.ciip_incentive_per_product.product_amount is 'The production amount reported in the application';
+comment on column ggircs_portal.ciip_incentive_per_product.product_emissions is 'The emissions allocated to the product (manually or automatically)';
+comment on column ggircs_portal.ciip_incentive_per_product.incentive_ratio is 'The incentive ratio of the product, calculated using the CIIP incentive formula';
+comment on column ggircs_portal.ciip_incentive_per_product.incentive_multiplier is 'The incentive multiplier for the product, configured by the CIIP administrator';
+comment on column ggircs_portal.ciip_incentive_per_product.payment_allocation_factor is 'The part of the incentive that will be assigned to this product, defined based on the emissions allocated to the product';
+comment on column ggircs_portal.ciip_incentive_per_product.incremental_carbon_tax is 'The incremental carbon tax allocated to this product';
+comment on column ggircs_portal.ciip_incentive_per_product.incentive_product is 'The incentive product of the benchmarked product';
+comment on column ggircs_portal.ciip_incentive_per_product.incentive_product_max is 'The maximum incentive for the product (i.e., incremental_carbon_tax * incentive_multiplier)';
+comment on column ggircs_portal.ciip_incentive_per_product.emission_intensity is 'The emission intensity for the product (i.e. product_emissions / product_amount)';
+comment on column ggircs_portal.ciip_incentive_per_product.benchmark is 'The benchmark for this product, for the application reporting year, configured by the CIIP administrator';
+comment on column ggircs_portal.ciip_incentive_per_product.eligibility_threshold is 'The threshold for this product, for the application reporting year, configured by the CIIP administrator';
+
 commit;
