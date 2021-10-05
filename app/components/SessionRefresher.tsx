8ff0cf37a1b6a51aa41df0230798a00792d5034c
Naomi Aro <naomi.aro@gov.bc.ca>
2021-10-05 16:32:50 -0700

    feat: start session refresh

---
+++ app/components/SessionRefresher.tsx
@@ -0,0 +1,34 @@
+import React, {useEffect} from 'react';
+import throttle from 'lodash.throttle';
+
+interface Props {
+  refreshUrl: string;
+}
+
+const SessionRefresher: React.FunctionComponent<Props> = ({refreshUrl}) => {
+  const extendSession = async () => {
+    try {
+      await fetch(refreshUrl);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    const throttledSession = throttle(extendSession, 1000 * 60);
+    window.addEventListener('keydown', throttledSession);
+    window.addEventListener('mousedown', throttledSession);
+    window.addEventListener('scroll', throttledSession);
+
+    return () => {
+      window.removeEventListener('keydown', throttledSession);
+      window.removeEventListener('mousedown', throttledSession);
+      window.removeEventListener('scroll', throttledSession);
+      throttledSession.cancel();
+    };
+  }, []);
+
+  return null;
+};
+
+export default SessionRefresher;
