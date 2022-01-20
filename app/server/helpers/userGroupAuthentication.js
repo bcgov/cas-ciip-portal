7401964b87ab2ccfc49ede2a96babb0b3018b0b8
Dylan Leard <dylan@button.is>
2022-01-19 16:32:04 -0800

    fix: redirect unauthorized idir users to /analyst/pending

--- app/server/helpers/userGroupAuthentication.js
+++ app/server/helpers/userGroupAuthentication.js
@@ -13,16 +13,14 @@ const getUserGroups = (req) => {
   )
     return [groupConstants.GUEST];
 
-  const brokerSessionId = req.kauth.grant.id_token.content.broker_session_id;
+  const identityProvider = req.kauth.grant.id_token.content.identity_provider;
   const { groups } = req.kauth.grant.id_token.content;
 
   const processedGroups = groups.map((value) => removeFirstLetter(value));
   const validGroups = compactGroups(processedGroups);
 
   if (validGroups.length === 0) {
-    return brokerSessionId &&
-      brokerSessionId.length === 41 &&
-      brokerSessionId.startsWith("idir.")
+    return identityProvider === "idir"
       ? [groupConstants.PENDING_ANALYST]
       : [groupConstants.USER];
   }
