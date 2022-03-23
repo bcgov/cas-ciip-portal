9ddb07b816880fa8f5bfe31162fbf8ae8d1c87e5
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2022-03-22 17:57:17 -0700

    test: updating tests with proper mocks

--- app/tests/unit/components/Dashboard/ggircsAppLink.test.tsx
+++ app/tests/unit/components/Dashboard/ggircsAppLink.test.tsx
@@ -2,20 +2,14 @@ import React from "react";
 import { mount } from "enzyme";
 import GgircsAppLink from "components/Dashboard/ggircsAppLink";
 
+jest.mock("next/config", () => () => ({
+  publicRuntimeConfig: {
+    GGIRCS_HOST: "a-test-ggircs-host",
+  },
+}));
+
 describe("the ggircs app link component", () => {
-  const oldEnvironment = process.env;
-
-  beforeEach(() => {
-    jest.resetModules();
-    process.env = { ...oldEnvironment };
-  });
-
-  afterAll(() => {
-    process.env = oldEnvironment;
-  });
-
   it("matches the snapshot with the ggircs link", () => {
-    process.env.GGIRCS_HOST = "a-test-ggircs-host";
     const componentUnderTest = mount(<GgircsAppLink />);
     expect(componentUnderTest).toMatchSnapshot();
   });
--- app/tests/unit/components/Dashboard/programAdministration.test.tsx
+++ app/tests/unit/components/Dashboard/programAdministration.test.tsx
@@ -2,20 +2,14 @@ import React from "react";
 import { mount } from "enzyme";
 import ProgramDataManagement from "components/Dashboard/programAdministration";
 
+jest.mock("next/config", () => () => ({
+  publicRuntimeConfig: {
+    METABASE_HOST: "a-test-metabase-host",
+  },
+}));
+
 describe("the ggircs app link component", () => {
-  const oldEnvironment = process.env;
-
-  beforeEach(() => {
-    jest.resetModules();
-    process.env = { ...oldEnvironment };
-  });
-
-  afterAll(() => {
-    process.env = oldEnvironment;
-  });
-
   it("matches the snapshot with the metabase link", () => {
-    process.env.METABASE_HOST = "a-test-metabase-host";
     const componentUnderTest = mount(<ProgramDataManagement />);
     expect(componentUnderTest).toMatchSnapshot();
   });
