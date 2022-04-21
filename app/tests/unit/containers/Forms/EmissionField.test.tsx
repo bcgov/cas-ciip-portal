6e99fe358f34a3475ccaf44a1b20e8e2866a385b
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2022-04-21 16:46:00 -0700

    test: testing the emission field component

---
+++ app/tests/unit/containers/Forms/EmissionField.test.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import EmissionFieldComponent from "containers/Forms/EmissionField";
+import { mount } from "enzyme";
+import { createDefaultJsonSchemaFormProps } from "tests/json-schema-utils";
+
+describe("The EmissionField", () => {
+  it("Displays the emission total passed in as a prop", () => {
+    const componentUnderTest = mount(
+      <EmissionFieldComponent
+        totalOnsiteEmissions={1234567.89}
+        {...(createDefaultJsonSchemaFormProps() as any)}
+      />
+    );
+
+    expect(componentUnderTest.text()).toContain(
+      "Total On-site Emissions (excl. CO2BioC): 1,234,567.89 tCO2e"
+    );
+  });
+});
