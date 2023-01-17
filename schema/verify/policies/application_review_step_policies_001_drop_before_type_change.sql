68b086ed9c43034c31d8f033f7e0750aaa7e3c99
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2023-01-16 17:54:37 -0800

    refactor: sqitch verify

--- schema/verify/policies/application_review_step_policies_001_drop_before_type_change.sql
+++ schema/verify/policies/application_review_step_policies_001_drop_before_type_change.sql
@@ -2,6 +2,11 @@
 
 begin;
 
-select ggircs_portal_private.verify_policy_not_present('ciip_industry_user_select_application_review_step', 'ggircs_portal.application_review_step');
+-- The following lines can be uncommented if we only use the verify script as we deploy
+-- If using `sqitch verify` to assess the constistency of the database, the following changes will
+-- be invalidated by the associated ..._002_recreate_after_type_change migration
+
+
+-- select ggircs_portal_private.verify_policy_not_present('ciip_industry_user_select_application_review_step', 'ggircs_portal.application_review_step');
 
 rollback;
--- schema/verify/policies/ciip_user_policies_001_drop_before_type_change.sql
+++ schema/verify/policies/ciip_user_policies_001_drop_before_type_change.sql
@@ -2,10 +2,15 @@
 
 begin;
 
-select ggircs_portal_private.verify_policy_not_present('ciip_analyst_insert_ciip_user', 'ggircs_portal.ciip_user');
-select ggircs_portal_private.verify_policy_not_present('ciip_analyst_update_ciip_user', 'ggircs_portal.ciip_user');
-select ggircs_portal_private.verify_policy_not_present('ciip_industry_user_insert_ciip_user', 'ggircs_portal.ciip_user');
-select ggircs_portal_private.verify_policy_not_present('ciip_industry_user_update_ciip_user', 'ggircs_portal.ciip_user');
-select ggircs_portal_private.verify_policy_not_present('ciip_guest_select_ciip_user', 'ggircs_portal.ciip_user');
+-- The following lines can be uncommented if we only use the verify script as we deploy
+-- If using `sqitch verify` to assess the constistency of the database, the following changes will
+-- be invalidated by the associated ..._002_recreate_after_type_change migration
+
+
+-- select ggircs_portal_private.verify_policy_not_present('ciip_analyst_insert_ciip_user', 'ggircs_portal.ciip_user');
+-- select ggircs_portal_private.verify_policy_not_present('ciip_analyst_update_ciip_user', 'ggircs_portal.ciip_user');
+-- select ggircs_portal_private.verify_policy_not_present('ciip_industry_user_insert_ciip_user', 'ggircs_portal.ciip_user');
+-- select ggircs_portal_private.verify_policy_not_present('ciip_industry_user_update_ciip_user', 'ggircs_portal.ciip_user');
+-- select ggircs_portal_private.verify_policy_not_present('ciip_guest_select_ciip_user', 'ggircs_portal.ciip_user');
 
 rollback;
