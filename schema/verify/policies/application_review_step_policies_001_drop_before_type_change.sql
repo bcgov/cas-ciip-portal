506f4293f4609e21e041a8ecd3fa5de31fa30b97
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2023-01-18 15:01:01 -0800

    chore: removing sqitch verify after db deploy in ci

--- schema/verify/policies/application_review_step_policies_001_drop_before_type_change.sql
+++ schema/verify/policies/application_review_step_policies_001_drop_before_type_change.sql
@@ -2,11 +2,6 @@
 
 begin;
 
--- The following lines can be uncommented if we only use the verify script as we deploy
--- If using `sqitch verify` to assess the constistency of the database, the following changes will
--- be invalidated by the associated ..._002_recreate_after_type_change migration
-
-
--- select ggircs_portal_private.verify_policy_not_present('ciip_industry_user_select_application_review_step', 'ggircs_portal.application_review_step');
+select ggircs_portal_private.verify_policy_not_present('ciip_industry_user_select_application_review_step', 'ggircs_portal.application_review_step');
 
 rollback;
--- schema/verify/policies/ciip_user_organisation_policies_001_drop_before_type_change.sql
+++ schema/verify/policies/ciip_user_organisation_policies_001_drop_before_type_change.sql
@@ -2,12 +2,7 @@
 
 begin;
 
--- The following lines can be uncommented if we only use the verify script as we deploy
--- If using `sqitch verify` to assess the constistency of the database, the following changes will
--- be invalidated by the associated ..._002_recreate_after_type_change migration
-
-
--- select ggircs_portal_private.verify_policy_not_present('ciip_industry_user_select_ciip_user_organisation', 'ggircs_portal.ciip_user_organisation');
--- select ggircs_portal_private.verify_policy_not_present('ciip_industry_user_insert_ciip_user_organisation', 'ggircs_portal.ciip_user_organisation');
+select ggircs_portal_private.verify_policy_not_present('ciip_industry_user_select_ciip_user_organisation', 'ggircs_portal.ciip_user_organisation');
+select ggircs_portal_private.verify_policy_not_present('ciip_industry_user_insert_ciip_user_organisation', 'ggircs_portal.ciip_user_organisation');
 
 rollback;
