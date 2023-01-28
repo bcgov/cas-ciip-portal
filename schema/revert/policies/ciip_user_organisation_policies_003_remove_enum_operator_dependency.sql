8b334d6a9c9a5806607d1c34214f2c6315b7d597
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2023-01-27 17:32:32 -0800

    refactor: unnecessary upsert

--- schema/revert/policies/ciip_user_organisation_policies_003_remove_enum_operator_dependency.sql
+++ schema/revert/policies/ciip_user_organisation_policies_003_remove_enum_operator_dependency.sql
@@ -8,13 +8,6 @@ do
 
     -- ciip_industry_user RLS
     perform ggircs_portal_private.upsert_policy(
-      'ciip_industry_user_select_ciip_user_organisation',
-      'ciip_user_organisation',
-      'select',
-      'ciip_industry_user',
-      $$user_id=(select id from ggircs_portal.ciip_user where uuid = (select sub from ggircs_portal.session()))$$
-    );
-    perform ggircs_portal_private.upsert_policy(
         'ciip_industry_user_insert_ciip_user_organisation',
         'ciip_user_organisation',
         'insert',
--- schema/verify/policies/ciip_user_organisation_policies_003_remove_enum_operator_dependency.sql
+++ schema/verify/policies/ciip_user_organisation_policies_003_remove_enum_operator_dependency.sql
@@ -3,7 +3,6 @@
 begin;
 
 -- ciip_industry_user Policies
-select ggircs_portal_private.verify_policy('select', 'ciip_industry_user_select_ciip_user_organisation', 'ciip_user_organisation', 'ciip_industry_user');
 select ggircs_portal_private.verify_policy('insert', 'ciip_industry_user_insert_ciip_user_organisation', 'ciip_user_organisation', 'ciip_industry_user');
 
 rollback;
