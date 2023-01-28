8d8a4174be339897ffa40ba3f6200b421c9870c4
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2023-01-27 17:28:09 -0800

    fix: ciip user organisation policy doesnt depend on equal operator

---
+++ schema/deploy/policies/ciip_user_organisation_policies_003_remove_enum_operator_dependency.sql
@@ -0,0 +1,28 @@
+-- Deploy ggircs-portal:policies/ciip_user_organisation_policies_003_remove_enum_operator_dependency to pg
+
+begin;
+
+do
+  $policy$
+  begin
+
+    -- ciip_industry_user RLS
+    perform ggircs_portal_private.upsert_policy(
+      'ciip_industry_user_select_ciip_user_organisation',
+      'ciip_user_organisation',
+      'select',
+      'ciip_industry_user',
+      $$user_id=(select id from ggircs_portal.ciip_user where uuid = (select sub from ggircs_portal.session()))$$
+    );
+    perform ggircs_portal_private.upsert_policy(
+        'ciip_industry_user_insert_ciip_user_organisation',
+        'ciip_user_organisation',
+        'insert',
+        'ciip_industry_user',
+        $$user_id=(select id from ggircs_portal.ciip_user where uuid = (select sub from ggircs_portal.session())) and status in ('pending')$$
+    );
+
+  end
+  $policy$;
+
+commit;
