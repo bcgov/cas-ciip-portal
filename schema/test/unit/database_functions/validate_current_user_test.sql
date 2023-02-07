80a8bd4efba04a548dbf298c37e8951e5d6b32c3
Dylan Leard <dylan@button.is>
2023-02-07 15:07:41 -0800

    test: update pgTap tests

--- schema/test/unit/database_functions/validate_current_user_test.sql
+++ schema/test/unit/database_functions/validate_current_user_test.sql
@@ -7,14 +7,16 @@ alter table ggircs_portal.ciip_user disable trigger _welcome_email;
 
 select no_plan();
 
+truncate ggircs_portal.ciip_user restart identity cascade;
+
 set jwt.claims.sub to 'whoathisisastrangeuuid@testing_idp';
 set jwt.claims.email to 'test@example.com';
 
 
 -- returns true if the user is not in the database (new user)
 select is(
-    (select ggircs_portal.validate_current_user()),
-    false,
+    (select ggircs_portal_private.validate_current_user()),
+    true,
     'returns true if the user is not in the database (new user)'
 );
 
@@ -22,19 +24,28 @@ select is(
 insert into ggircs_portal.ciip_user (uuid, email_address) values ('whoathisisastrangeuuid@testing_idp','test@example.com');
 
 select is(
-    (select ggircs_portal.validate_current_user()),
+    (select ggircs_portal_private.validate_current_user()),
     true,
     'returns true if the user is not in the database (new user)'
 );
 
-
 -- returns true if the user is in the database with a different uuid but the allow_uuid_update flag set to true (returning user after KC gold migration)
+update ggircs_portal.ciip_user set allow_uuid_update = true;
+set jwt.claims.sub to 'whoathisisastrangeuuid@testing_idp_new';
 
+select is(
+    (select ggircs_portal_private.validate_current_user()),
+    true,
+    'returns true if the user is in the database with a different uuid but the allow_uuid_update flag set to true'
+);
 
 -- returns false if the user is in the database, different uuid and allow_uuid_update set to false (new account with same email)
-
-
-
+update ggircs_portal.ciip_user set allow_uuid_update = false;
+select is(
+    (select ggircs_portal_private.validate_current_user()),
+    false,
+    'returns false if the user is in the database, different uuid and allow_uuid_update set to false (new account with same email) '
+);
 
 select finish();
 
