8addccc3aa382ce1b4ffd4fca8da84dc58aa0220
Dylan Leard <dylan@button.is>
2023-02-09 13:28:04 -0800

    chore: update graphql schema & pgTap tests

--- schema/test/unit/queries/validate_current_user_test.sql
+++ schema/test/unit/queries/validate_current_user_test.sql
@@ -15,7 +15,7 @@ set jwt.claims.email to 'test@example.com';
 
 -- returns true if the user is not in the database (new user)
 select is(
-    (select ggircs_portal_private.validate_current_user()),
+    (select ggircs_portal.validate_current_user()),
     true,
     'returns true if the user is not in the database (new user)'
 );
@@ -24,7 +24,7 @@ select is(
 insert into ggircs_portal.ciip_user (uuid, email_address) values ('whoathisisastrangeuuid@testing_idp','test@example.com');
 
 select is(
-    (select ggircs_portal_private.validate_current_user()),
+    (select ggircs_portal.validate_current_user()),
     true,
     'returns true if the user is not in the database (new user)'
 );
@@ -34,7 +34,7 @@ update ggircs_portal.ciip_user set allow_uuid_update = true;
 set jwt.claims.sub to 'whoathisisastrangeuuid@testing_idp_new';
 
 select is(
-    (select ggircs_portal_private.validate_current_user()),
+    (select ggircs_portal.validate_current_user()),
     true,
     'returns true if the user is in the database with a different uuid but the allow_uuid_update flag set to true'
 );
@@ -42,7 +42,7 @@ select is(
 -- returns false if the user is in the database, different uuid and allow_uuid_update set to false (new account with same email)
 update ggircs_portal.ciip_user set allow_uuid_update = false;
 select is(
-    (select ggircs_portal_private.validate_current_user()),
+    (select ggircs_portal.validate_current_user()),
     false,
     'returns false if the user is in the database, different uuid and allow_uuid_update set to false (new account with same email) '
 );
