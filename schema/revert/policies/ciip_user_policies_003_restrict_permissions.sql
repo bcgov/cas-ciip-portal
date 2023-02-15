97fb4c7e86272160b39ed7f1a8b814db3652285a
Dylan Leard <dylan@button.is>
2023-02-15 15:44:27 -0800

    chore: fix revert file

--- schema/revert/policies/ciip_user_policies_003_restrict_permissions.sql
+++ schema/revert/policies/ciip_user_policies_003_restrict_permissions.sql
@@ -9,6 +9,7 @@ $policy$
 begin
 
 perform ggircs_portal_private.upsert_policy('ciip_industry_user_select_ciip_user', 'ciip_user', 'select', 'ciip_industry_user', 'true');
+drop function ggircs_portal_private.get_valid_users;
 
 end
 $policy$;
