bdda572b30e1a82a6d57fb4b278de0ee874d85b6
Dylan Leard <dylan@button.is>
2023-03-15 14:33:19 -0700

    chore: fix return order in session function to match type

--- schema/deploy/function_session.sql
+++ schema/deploy/function_session.sql
@@ -35,8 +35,8 @@ begin
                    current_setting('jwt.claims.email', true),
                    current_setting('jwt.claims.broker_session_id', true),
                    current_setting('jwt.claims.priority_group', true),
-                   current_setting('jwt.claims.bceid_business_name', true),
-                   (select string_to_array(current_setting('jwt.claims.user_groups', true), ','))
+                   (select string_to_array(current_setting('jwt.claims.user_groups', true), ',')),
+                   current_setting('jwt.claims.bceid_business_name', true)
                    )::ggircs_portal.jwt_token
     );
 end
