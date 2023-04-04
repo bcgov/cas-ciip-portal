57241247b300631f3ac215c8e3aa61f2d2a12178
Dylan Leard <dylan@button.is>
2023-04-04 16:01:05 -0700

    test: fix test description

--- app/tests/unit/containers/User/ManageBceidUserTableRow.test.tsx
+++ app/tests/unit/containers/User/ManageBceidUserTableRow.test.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { ManageBceidUserTableRow_userOrganisation } from "__generated__/ManageBceidUserTableRow_userOrganisation.graphql";
+import { ManageBceidUserTableRow_user } from "__generated__/ManageBceidUserTableRow_user.graphql";
 import { ManageBceidUserTableRowComponent } from "containers/User/ManageBceidUserTableRow";
 
-describe("Organisation request rows", () => {
-  it("should render an organisation properly", async () => {
-    const user: ManageBceidUserTableRow_userOrganisation = {
-      " $refType": "ManageBceidUserTableRow_userOrganisation",
+describe("Manage BCeID Users table", () => {
+  it("should render a user properly", async () => {
+    const user: ManageBceidUserTableRow_user = {
+      " $refType": "ManageBceidUserTableRow_user",
       emailAddress: "a@a.a",
       firstName: "First",
       lastName: "Last",
