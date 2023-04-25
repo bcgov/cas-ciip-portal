63867c89c9f92d0f48ed8c86ea764c1efb5917c8
Brianna Cerkiewicz <brianna@button.is>
2023-04-25 14:08:45 -0700

    test: update VerificationStatement tests

--- app/tests/unit/containers/Applications/VerificationStatement.test.tsx
+++ app/tests/unit/containers/Applications/VerificationStatement.test.tsx
@@ -6,12 +6,13 @@ import { VerificationStatementComponent } from "containers/Forms/VerificationSta
 const getAttachmentDownloadRoute = require("routes");
 jest.mock("routes");
 
-describe("The Attachment Upload Component", () => {
+describe("The Verification Statement component", () => {
+  const onError = jest.fn();
   const application: VerificationStatement_application = {
     " $refType": "VerificationStatement_application",
     rowId: 1,
     latestDraftRevision: {
-      versionNumber: 1,
+      versionNumber: 2,
     },
     attachmentsByApplicationId: {
       __id: "abc",
@@ -21,7 +22,8 @@ describe("The Attachment Upload Component", () => {
             fileName: "test-filename",
             id: "test-id",
             rowId: 7,
-            createdAt: "date",
+            createdAt: "1111-01-11",
+            versionNumber: 1,
           },
         },
         {
@@ -29,13 +31,24 @@ describe("The Attachment Upload Component", () => {
             fileName: "test2-filename",
             id: "test-id-2",
             rowId: 8,
-            createdAt: "date2",
+            createdAt: "2222-02-22",
+            versionNumber: 2,
           },
         },
       ],
     },
   };
-  const onError = jest.fn();
+  const emptyApplication: VerificationStatement_application = {
+    " $refType": "VerificationStatement_application",
+    rowId: 1,
+    latestDraftRevision: {
+      versionNumber: 2,
+    },
+    attachmentsByApplicationId: {
+      __id: "abc",
+      edges: [],
+    },
+  };
 
   it("should match the snapshot", () => {
     const wrapper = shallow(
@@ -48,7 +61,20 @@ describe("The Attachment Upload Component", () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it("renders the component", () => {
+  it("renders the component without attachment table when there are no attachments", () => {
+    const wrapper = shallow(
+      <VerificationStatementComponent
+        application={emptyApplication}
+        relay={null}
+        onError={onError}
+      />
+    );
+    expect(wrapper.find("h1").text()).toEqual("Verification Statement");
+    expect(wrapper.find("FilePicker").exists()).toBe(true);
+    expect(wrapper.find("table.bc-table").exists()).toBe(false);
+  });
+
+  it("renders the component with attachment table", () => {
     const wrapper = shallow(
       <VerificationStatementComponent
         application={application}
@@ -58,8 +84,15 @@ describe("The Attachment Upload Component", () => {
     );
     expect(wrapper.find("h1").text()).toEqual("Verification Statement");
     expect(wrapper.find("FilePicker").exists()).toBe(true);
-    expect(wrapper.find("div.attachment-link").exists()).toBe(true);
-    expect(wrapper.find("div.uploaded-on").exists()).toBe(true);
+    expect(wrapper.find("table.bc-table").exists()).toBe(true);
+
+    // only current version's row should have delete icon (FontAwesomeIcon)
+    expect(wrapper.find("tr").at(1).text()).toEqual(
+      "test-filename January 11th, 11111"
+    );
+    expect(wrapper.find("tr").at(2).text()).toEqual(
+      "test-filename-2 <FontAwesomeIcon />February 22nd, 22222"
+    );
   });
 
   it("calls the download router", () => {
