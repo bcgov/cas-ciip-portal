630635ade5edac845d424cc2fe1bcc73209bf976
Brianna Cerkiewicz <brianna@button.is>
2023-04-26 14:24:14 -0700

    test: deleteFileMiddleware test

---
+++ app/tests/unit/server/middleware/deleteFileMiddleware.test.tsx
@@ -0,0 +1,36 @@
+/**
+ * @jest-environment node
+ */
+
+import {
+  attachmentDeleteRouter,
+  handleDelete,
+} from "server/middleware/deleteFileMiddleware";
+
+describe("The attachment delete router", () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it("is configured with the right route and handler", () => {
+    const routerUnderTest = attachmentDeleteRouter;
+
+    expect(routerUnderTest.stack).toHaveLength(1);
+    expect(routerUnderTest.stack[0].route.path).toBe("/delete/:attachmentId");
+    expect(attachmentDeleteRouter.stack[0].route.stack[0].handle).toBe(
+      handleDelete
+    );
+  });
+
+  it("calls next() with the error if an exception is thrown", async () => {
+    const error = new TypeError(
+      "Cannot read property 'attachmentId' of undefined"
+    );
+    const next = jest.fn();
+    const handlerUnderTest = handleDelete;
+
+    handlerUnderTest({ garbage: "garbage" }, {}, next);
+
+    expect(next).toHaveBeenCalledWith(error);
+  });
+});
