7536aa69faaeb61c6a64892a6955caba70d20d7e
Dylan Leard <dylan@button.is>
2023-05-26 14:01:15 -0700

    chore: fix table definition and foreign key columns

--- schema/deploy/tables/gwp.sql
+++ schema/deploy/tables/gwp.sql
@@ -9,19 +9,18 @@ create table ggircs_portal.gwp (
   gwp numeric not null,
   description varchar(10000),
   reporting_year_start integer not null,
-  reporting_year_end integer not null,
-  created_at timestamp with time zone not null default now(),
-  created_by int references ggircs_portal.ciip_user,
-  updated_at timestamp with time zone not null default now(),
-  updated_by int references ggircs_portal.ciip_user,
-  deleted_at timestamp with time zone,
-  deleted_by int references ggircs_portal.ciip_user
+  reporting_year_end integer not null
 );
 
-create trigger _100_timestamps
-  before insert or update on ggircs_portal.gwp
-  for each row
-  execute procedure ggircs_portal_private.update_timestamps();
+create index gwp_gas_id_idx
+  on ggircs_portal.gwp(gas_id);
+
+select ggircs_portal_private.upsert_timestamp_columns(
+  table_schema_name := 'ggircs_portal',
+  table_name := 'gwp',
+  add_create := true,
+  add_update := true,
+  add_delete := true);
 
 do
 $grant$
