6ffdfc518b6254840fb37de5ebaa5cd2dc6f79f5
Dylan Leard <dylan@button.is>
2023-06-06 11:47:51 -0700

    test: add pgTap tests for create_or_update_ciip_user()

--- schema/test/unit/mutations/create_or_update_ciip_user_test.sql
+++ schema/test/unit/mutations/create_or_update_ciip_user_test.sql
@@ -3,7 +3,7 @@ create extension if not exists pgtap;
 reset client_min_messages;
 
 begin;
-select plan(6);
+select plan(10);
 
 alter table ggircs_portal.ciip_user
   disable trigger _welcome_email;
@@ -102,6 +102,97 @@ select throws_like (
   'throws if the user has a new sub for an existing email address, and the allow_uuid_update flag is set to false'
 );
 
+-- Business BCeID tests
+
+set jwt.claims.sub to 'business@provider';
+set jwt.claims.email to 'bob.loblaw@gov.bc.ca';
+set jwt.claims.bceid_business_name to 'business';
+
+select ggircs_portal.create_or_update_ciip_user('Bob','Loblaw','tester','123-456-6780');
+
+select results_eq (
+  $$
+    select first_name, last_name, email_address, allow_uuid_update, bceid_business_name
+    from ggircs_portal.ciip_user
+    where uuid = 'business@provider'::varchar
+  $$,
+  $$
+  values (
+    'Bob'::varchar(1000),
+    'Loblaw'::varchar(1000),
+    'bob.loblaw@gov.bc.ca'::varchar(1000),
+    false,
+    'business'::varchar(1000)
+  )
+  $$,
+  'create_or_update_ciip_user successfully adds a new user if the email matches a basic bceid, but bceid_business_name is not null'
+);
+
+set jwt.claims.sub to 'different_business@provider';
+set jwt.claims.email to 'bob.loblaw@gov.bc.ca';
+set jwt.claims.bceid_business_name to 'different_business';
+
+select ggircs_portal.create_or_update_ciip_user('Bob','Loblaw','tester','123-456-6780');
+
+select results_eq (
+  $$
+    select first_name, last_name, email_address, allow_uuid_update, bceid_business_name
+    from ggircs_portal.ciip_user
+    where uuid = 'different_business@provider'::varchar
+  $$,
+  $$
+  values (
+    'Bob'::varchar(1000),
+    'Loblaw'::varchar(1000),
+    'bob.loblaw@gov.bc.ca'::varchar(1000),
+    false,
+    'different_business'::varchar(1000)
+  )
+  $$,
+  'create_or_update_ciip_user successfully adds a new user if the email matches a business bceid, but bceid_business_name is different from the existing row'
+);
+
+update ggircs_portal.ciip_user set allow_uuid_update = true where uuid='business@provider'::varchar;
+
+set jwt.claims.sub to 'new_business@provider';
+set jwt.claims.email to 'bob.loblaw@gov.bc.ca';
+set jwt.claims.bceid_business_name to 'business';
+
+select ggircs_portal.create_or_update_ciip_user('Bob','Loblaw','tester','123-456-6780');
+
+select results_eq (
+  $$
+    select uuid, first_name, last_name, email_address, allow_uuid_update, bceid_business_name
+    from ggircs_portal.ciip_user
+    where uuid = 'new_business@provider'::varchar
+  $$,
+  $$
+  values (
+    'new_business@provider'::varchar(1000),
+    'Bob'::varchar(1000),
+    'Loblaw'::varchar(1000),
+    'bob.loblaw@gov.bc.ca'::varchar(1000),
+    false,
+    'business'::varchar(1000)
+  )
+  $$,
+  'create_or_update_ciip_user successfully updates the uuid of a business bceid user if the allow_uuid_update flag is set'
+);
+
+update ggircs_portal.ciip_user set allow_uuid_update = false;
+
+-- Throws if we try to change the sub on a user that doesn't have the flag set and the bceid_business_name is the same
+set jwt.claims.sub to 'changed@new_provider';
+set jwt.claims.email to 'bob.loblaw@gov.bc.ca';
+set jwt.claims.bceid_business_name to 'business';
+
+select throws_like (
+  $$
+    select ggircs_portal.create_or_update_ciip_user('a name','a last name','worker','321-456-9876');
+  $$,
+  'uuid cannot be updated when allow_uuid_update is false',
+  'throws if the user has a new sub for an existing email address and bceid_business_name, and the allow_uuid_update flag is set to false'
+);
 
 select finish();
 rollback;
