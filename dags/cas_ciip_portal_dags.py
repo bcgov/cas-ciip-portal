baea0e2b25366559771564cf13e337aab0852341
Josh Gamache <jsgamache@gmail.com>
2025-09-09 11:58:04 -0600

    refactor: replace depricated params and functions

--- dags/cas_ciip_portal_dags.py
+++ dags/cas_ciip_portal_dags.py
@@ -26,7 +26,7 @@ DAG cas_ciip_portal_ciip_deploy_db.
 Initializes the portal database and deploys the schema/data.
 If we're in the test namespace, dag first restores data from prod and then imports data from swrs.
 """
-deploy_db_dag = DAG('cas_ciip_portal_deploy_db', schedule_interval=None,
+deploy_db_dag = DAG('cas_ciip_portal_deploy_db', schedule=None,
                     default_args=ciip_deploy_db_args)
 
 
@@ -90,7 +90,7 @@ DAG cas_ciip_portal_acme_issue
 Issues site certificates for the CIIP portal
 """
 acme_issue_dag = DAG('cas_ciip_portal_acme_issue',
-                     schedule_interval=None, default_args=acme_renewal_args)
+                     schedule=None, default_args=acme_renewal_args)
 
 cron_acme_issue_task = PythonOperator(
     python_callable=trigger_k8s_cronjob,
@@ -103,7 +103,7 @@ cron_acme_issue_task = PythonOperator(
 DAG cas_ciip_portal_acme_renewal
 Renews site certificates for the CIIP portal
 """
-acme_renewal_dag = DAG('cas_ciip_portal_acme_renewal', schedule_interval='0 8 * * *',
+acme_renewal_dag = DAG('cas_ciip_portal_acme_renewal', schedule='0 8 * * *',
                        default_args=acme_renewal_args)
 
 cert_renewal_task = PythonOperator(
@@ -136,7 +136,7 @@ default_args = {
 
 
 ciip_full_backup_dag = DAG('walg_backup_ciip_full', default_args=default_args,
-                           schedule_interval='0 8 * * *')
+                           schedule='0 8 * * *')
 
 create_backup_task(ciip_full_backup_dag,
                    namespace, 'cas-ciip-portal-patroni')
